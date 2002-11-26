5bc91faa921552228be50757160449f9fa302a4c
hstoerne <hstoerne@44351d6e-118b-4698-b696-ce33095ecaa4>
2002-11-26 11:59:34 +0000

    Changed copyright notice
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@72 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbdisplay/bbgen.c
+++ trunk/bbdisplay/bbgen.c
@@ -11,10 +11,12 @@
 /*                                                                            */
 /* Copyright (C) 2002 Henrik Storner <henrik@storner.dk>                      */
 /*                                                                            */
-/* This program is released under the GNU Public License (GPL), version 2.    */
-/* See the file "COPYING" for details.                                        */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: bbgen.c,v 1.47 2002-11-26 11:59:34 hstoerne Exp $";
 
 #include <stdio.h>
 #include <unistd.h>
