dd59ab25bc0956ccfd3950ad2909afe6666ab2a8
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-02-14 12:02:11 +0000

    Prelim. support - build a bb-hosts file with critical hosts only
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@199 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbdisplay/wmlgen.c
+++ trunk/bbdisplay/wmlgen.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #include "bbgen.h"
 #include "wmlgen.h"
 
-void do_wml_cards(void)
+void do_wml_cards(time_t startofrun)
 {
 	FILE		*fd;
 	char		fn[256];
 	hostlist_t	*h;
+	int		hostcount = 0;
+	time_t		timeleft;
 
 	sprintf(fn, "%s/bb-hosts-wml.tmp", getenv("BBTMP"));
 	fd = fopen(fn, "w");
@@ -19,6 +23,7 @@ void do_wml_cards(void)
 
 	for (h = hosthead; (h); h = h->next) {
 		if (h->hostentry->color == COL_RED) {
+			hostcount++;
 			fprintf(fd, "%s %s %s\n", 
 				h->hostentry->ip, h->hostentry->hostname,
 				h->hostentry->rawentry);
@@ -26,5 +31,27 @@ void do_wml_cards(void)
 	}
 
 	fclose(fd);
+
+	timeleft = atoi(getenv("BBSLEEP")) - (time(NULL) - startofrun); 
+	if ((timeleft >= 20) && (hostcount <= 10)) {
+		pid_t pid;
+		char mkbbwmlcmd[256];
+		char newbbhosts[256];
+
+		sprintf(mkbbwmlcmd, "%s/web/mkbbwml.sh", getenv("BBHOME"));
+		sprintf(newbbhosts, "BBHOSTS=%s", fn);
+		pid = fork();
+		if (pid == -1) {
+			printf("Fork error in forking %s\n", mkbbwmlcmd);
+			return;
+		}
+		else if (pid == 0) {
+			putenv(newbbhosts);
+			execl(mkbbwmlcmd, "mkbbwml.sh",NULL);
+		}
+		else {
+			wait(NULL);
+		}
+	}
 }
 
