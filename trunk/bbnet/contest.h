f4318c7cd68eb53f189aab5e3c5b36a7278f6af6
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-04-15 22:13:35 +0000

    Copyright, multi-include guard
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@289 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbnet/contest.h
+++ trunk/bbnet/contest.h
@@ -1,3 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Big Brother network test tool.                                             */
+/*                                                                            */
+/* This is used to implement the testing of a TCP service.                    */
+/*                                                                            */
+/* Copyright (C) 2003 Henrik Storner <henrik@hswn.dk>                         */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef __CONTEST_H_
+#define __CONTEST_H_
+
 typedef struct {
 	char *svcname;
 	char *sendtxt;
@@ -20,4 +35,6 @@ typedef struct {
 extern test_t *add_test(char *ip, int portnum, char *service, int silent);
 extern void do_conn(int timeout, int concurrency);
 extern void show_conn_res(void);
+
+#endif
 
