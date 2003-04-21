285c4584307e5c23c3c34899bfa977739c79b2a5
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-04-21 07:10:12 +0000

    Support reporting "clear" on tcp tests that fail when ping also fails. Improved status reporting. Comment some of the trickier code bits and structures
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@309 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbnet/bbtest-net.c
+++ trunk/bbnet/bbtest-net.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: bbtest-net.c,v 1.24 2003-04-20 22:02:44 henrik Exp $";
+static char rcsid[] = "$Id: bbtest-net.c,v 1.25 2003-04-21 07:10:12 henrik Exp $";
 
 #include <stdio.h>
 #include <unistd.h>
@@ -43,47 +43,54 @@ link_t  null_link = { "", "", "", NULL };
 #define IP_ONLY         2	/* IP only - dont do DNS lookups */
 
 typedef struct {
-	char *testname;
-	int toolid;
-	int namelen;
-	int portnum;
+	char *testname;		/* Name of the test = column name in BB report */
+	int namelen;		/* Length of name - "testname:port" has this as strlen(testname), others 0 */
+	int portnum;		/* Port number this service runs on */
+	int toolid;		/* Which tool to use */
+	void *items;		/* testitem_t linked list of tests for this service */
 	void *next;
-	void *items;
 } service_t;
 
 typedef struct {
 	char *hostname;
 	char ip[16];
-	int dialup;
-	int testip;
-	int dnserror;
-	int in_sla;
-	int noconn;
-	int noping;
-	int badconn[3];
-	int downcount;
-	time_t downstart;
+
+	int dialup;		/* dialup flag (if set, failed tests report as clear) */
+	int testip;		/* testip flag (dont do dns lookups on hostname) */
+	int dnserror;		/* set internally if we cannot find the host's IP */
+	int in_sla;		/* set internally if inside SLA period. If not, failed tests are blue */
+
+	/* The following are for the connectivity test */
+	int noconn;		/* noconn flag (dont report "conn" at all */
+	int noping;		/* noping flag (report "conn" as clear=disabled */
+	int badconn[3];		/* badconn:x:y:z flag */
+	int downcount;		/* number of successive failed conn tests */
+	time_t downstart;	/* time() of first conn failure */
+
 	void *next;
 } testedhost_t;
 
 typedef struct {
-	testedhost_t	*host;
-	service_t	*service;
-	int		reverse;
-	int		dialup;
-	int		silenttest;
-	int		open;
-	test_t		*testresult;
+	testedhost_t	*host;		/* Pointer to testedhost_t record for this host */
+	service_t	*service;	/* Pointer to service_t record for the service to test */
+	int		reverse;	/* "!testname" flag */
+	int		dialup;		/* "?testname" flag */
+	int		alwaystrue;	/* "~testname" flag */
+	int		silenttest;	/* "testname:s" flag */
+	int		open;		/* Is the service open ? NB: Shows true state of service, ignores flags */
+	test_t		*testresult;	/* Banner and duration of test */
 	char		*banner;
 	void		*next;
 } testitem_t;
 
-service_t	*svchead = NULL;
-service_t	*pingtest = NULL;
-testedhost_t	*testhosthead = NULL;
-testitem_t	*testhead = NULL;
-char		*nonetpage = NULL;
-int		dnsmethod = DNS_THEN_IP;
+
+service_t	*svchead = NULL;		/* Head of all known services */
+service_t	*pingtest = NULL;		/* Identifies the pingtest within svchead list */
+testedhost_t	*testhosthead = NULL;		/* Head of all hosts */
+testitem_t	*testhead = NULL;		/* Head of all tests */
+char		*nonetpage = NULL;		/* The "NONETPAGE" env. variable */
+int		dnsmethod = DNS_THEN_IP;	/* How to do DNS lookups */
+
 
 service_t *add_service(char *name, int port, int namelen, int toolid)
 {
@@ -125,6 +132,7 @@ void load_services(void)
 	}
 	free(netsvcs);
 
+	/* Save NONETPAGE env. var in ",test1,test2," format for easy and safe grepping */
 	nonetpage = malloc(strlen(getenv("NONETPAGE"))+3);
 	sprintf(nonetpage, ",%s,", getenv("NONETPAGE"));
 	for (p=nonetpage; (*p); p++) if (*p == ' ') *p = ',';
@@ -146,7 +154,7 @@ void load_tests(void)
 		return;
 	}
 
-	/* Local hack - each network test tagged with NET:locationname */
+	/* Each network test tagged with NET:locationname */
 	p = getenv("BBLOCATION");
 	if (p) {
 		netstring = malloc(strlen(p)+5);
@@ -182,7 +190,8 @@ void load_tests(void)
 				h->testip = (strstr(p, "testip") != NULL);
 				h->noconn = (strstr(p, "noconn") != NULL);
 				h->noping = (strstr(p, "noping") != NULL);
-				h->badconn[0] = h->badconn[1] = h->badconn[2] = h->downcount = 0;
+				h->badconn[0] = h->badconn[1] = h->badconn[2] = 0;
+				h->downcount = 0;
 				h->in_sla = within_sla(p);
 				h->ip[0] = '\0';
 				h->dnserror = 0;
@@ -195,6 +204,7 @@ void load_tests(void)
 					newtest->host = h;
 					newtest->dialup = 0;
 					newtest->reverse = 0;
+					newtest->alwaystrue = 1;
 					newtest->open = 0;
 					newtest->testresult = NULL;
 					newtest->service = pingtest;
@@ -206,8 +216,9 @@ void load_tests(void)
 				while (testspec) {
 
 					service_t *s;
-					int dialuptest = 0;
-					int reversetest = 0;
+					int dialuptest;
+					int reversetest;
+					int alwaystruetest;
 					char *option;
 
 					if (strncmp(testspec, "badconn:", 8) == 0) {
@@ -220,14 +231,18 @@ void load_tests(void)
 						*option = '\0'; 
 						option++; 
 					}
-					if (*testspec == '?') { dialuptest=1; testspec++; }
-					if (*testspec == '!') { reversetest=1; testspec++; }
-					/*
-					 * ~TEST is ignored. It is used to have failed test
-					 * display as red, if coupled with the conn test
-					 * (which would normally make the test clear on failure).
+
+					/* Test prefixes:
+					 * - '?' denotes dialup test, i.e. report failures as clear.
+					 * - '|' denotes reverse test, i.e. service should be DOWN.
+					 * - '~' denotes test that ignores ping result (normally,
+					 *       TCP tests are reported CLEAR if ping check fails;
+					 *       with this flag report their true status)
 					 */
-					if (*testspec == '~') { testspec++; }
+					dialuptest = reversetest = alwaystruetest = 0;
+					if (*testspec == '?') { dialuptest=1;     testspec++; }
+					if (*testspec == '!') { reversetest=1;    testspec++; }
+					if (*testspec == '~') { alwaystruetest=1; testspec++; }
 
 					/* Find the service */
 					for (s=svchead; (s && (strcmp(s->testname, testspec) != 0)); s = s->next) ;
@@ -261,6 +276,7 @@ void load_tests(void)
 						newtest->service = s;
 						newtest->dialup = dialuptest;
 						newtest->reverse = reversetest;
+						newtest->alwaystrue = alwaystruetest;
 						newtest->open = 0;
 						newtest->testresult = NULL;
 						newtest->next = s->items;
@@ -470,6 +486,7 @@ int run_fping_service(service_t *service)
 	int		rtt1, rtt2;
 	char		timespec[20];
 
+	/* Run "fping -Ae 2>/dev/null" and feed it all IP's to test */
 	p = getenv("FPING");
 	strcpy(cmdpath, (p ? p : "fping"));
 	sprintf(logfn, "%s/fping.%u", getenv("BBTMP"), getpid());
@@ -484,6 +501,7 @@ int run_fping_service(service_t *service)
 	}
 	pclose(cmdpipe);
 
+	/* Load status of previously failed tests */
 	load_fping_status();
 
 	logfd = fopen(logfn, "r");
@@ -500,21 +518,7 @@ int run_fping_service(service_t *service)
 			strcpy(t->banner, l);
 
 			if (t->open) {
-				p = strchr(l, '(');
-				t->testresult = malloc(sizeof(test_t));
-				t->testresult->banner = t->banner;
-				if (sscanf(p, "(%d.%d %s)", &rtt1, &rtt2, timespec) == 3) {
-					if (strncmp(timespec, "ms", 2) == 0) {
-						/* Milliseconds */
-						t->testresult->duration.tv_sec = 0;
-						t->testresult->duration.tv_usec = 1000*rtt1;
-					}
-					else {
-						/* Seconds */
-						t->testresult->duration.tv_sec = rtt1;
-						t->testresult->duration.tv_usec = 0;
-					}
-				}
+				t->host->downcount = 0;
 			}
 			else {
 				t->host->downcount++;
@@ -553,7 +557,6 @@ void send_results(service_t *service)
 	for (t=service->items; (t); t = t->next) {
 		if ((service == pingtest) && t->host->noping) {
 			color = COL_CLEAR;
-			t->banner = "Ping test disabled";
 		}
 		else {
 			color = COL_GREEN;
@@ -579,6 +582,14 @@ void send_results(service_t *service)
 
 		}
 
+		/* If host ping fails, report failed TCP tests as clear unless "alwaystrue" flag set */
+		if ( ((color == COL_RED) || (color == COL_YELLOW)) && /* Test failed */
+		     (service != pingtest)                         && /* It's not a ping test */
+		     (t->host->downcount > 0)                      && /* The ping check did fail */
+		     (!t->alwaystrue)                              )  /* No "~testname" flag */ {
+			color = COL_CLEAR;
+		}
+
 		/* NOPAGENET services that are down are reported as yellow */
 		if (nopage && (color == COL_RED)) color = COL_YELLOW;
 
@@ -589,29 +600,56 @@ void send_results(service_t *service)
 		addtostatus(msgline);
 
 		if (t->host->dnserror) {
-			sprintf(msgline, "\nUnable to resolve hostname %s\n", t->host->hostname);
+			sprintf(msgline, "\n<p>Unable to resolve hostname %s\n</p>\n", t->host->hostname);
 		}
 		else {
-			sprintf(msgline, "\n&%s Service %s on %s is %s\n",
-				colorname(color), svcname, t->host->hostname,
-				((color == COL_CLEAR) ? "disabled" : (t->open ? "UP" : "DOWN")));
-
+			sprintf(msgline, "\n<p>Service %s on %s is ", svcname, t->host->hostname);
+			switch (color) {
+			  case COL_GREEN: 
+				  strcat(msgline, "OK ");
+				  strcat(msgline, (t->reverse ? "(down)" : "(up)"));
+				  strcat(msgline, "<br>\n");
+				  break;
+			  case COL_RED:
+			  case COL_YELLOW:
+				  strcat(msgline, "not OK ");
+				  strcat(msgline, (t->reverse ? "(up)" : "(down)"));
+				  strcat(msgline, "<br>\n");
+				  break;
+			  case COL_CLEAR:
+				  strcat(msgline, "OK<br>\n");
+				  if (service == pingtest) {
+					  if (t->host->noping) {
+						  strcat(msgline, "Ping check disabled (noping)\n");
+					  }
+					  /* "clear" due to badconn: no extra text */
+				  }
+				  else {
+					  /* Non-ping test clear: Dialup test or failed ping */
+					  strcat(msgline, "Dialup host, or connectivitity check failed\n");
+				  }
+			  case COL_BLUE:
+				  strcat(msgline, "OK<br>\n");
+				  strcat(msgline, "Host currently not monitored due to SLA setting.\n");
+				  break;
+			}
+			strcat(msgline, "</p>\n");
 		}
 		addtostatus(msgline);
 
 		if ((service == pingtest) && t->host->downcount) {
-			sprintf(msgline, "\nSystem unreachable for %d poll periods (%lu seconds)\n",
+			sprintf(msgline, "\n<p>System unreachable for %d poll periods (%lu seconds)\n</p>",
 				t->host->downcount, (time(NULL) - t->host->downstart));
 			addtostatus(msgline);
 		}
 
 		if (t->banner) {
-			addtostatus("\n<pre>\n");
-			addtostatus(t->banner);
-			addtostatus("\n</pre>\n\n");
+			if (service != pingtest) addtostatus("\n<pre>");
+			addtostatus("\n"); addtostatus(t->banner); addtostatus("\n");
+			if (service != pingtest) addtostatus("</pre>\n\n");
 		}
 		if (t->testresult) {
-			sprintf(msgline, "Seconds: %ld.%03ld\n", 
+			sprintf(msgline, "\nSeconds: %ld.%03ld\n", 
 				t->testresult->duration.tv_sec, t->testresult->duration.tv_usec / 1000);
 			addtostatus(msgline);
 		}
@@ -686,6 +724,12 @@ int main(int argc, char *argv[])
 	combo_start();
 	dprintf("\nTest services\n");
 
+	/* Ping checks first */
+	if (pingtest && pingtest->items) {
+		run_fping_service(pingtest); 
+		send_results(pingtest);
+	}
+
 	/* First run the standard TCP/IP tests */
 	for (s = svchead; (s); s = s->next) {
 		if ((s->items) && (s->toolid == TOOL_CONTEST)) {
@@ -734,7 +778,7 @@ int main(int argc, char *argv[])
 					run_ntp_service(s); send_results(s);
 					break;
 				case TOOL_FPING:
-					run_fping_service(s); send_results(s);
+					/* Already done */
 					break;
 			}
 		}
