cba340e2a77efc69af79ee67d2109fb8f04fc856
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-06-20 12:29:15 +0000

    Parameter change for parse_historyfile Export replog_t datatype for use by CGI script
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@526 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/availability.h
+++ trunk/lib/availability.h
@@ -18,6 +18,17 @@
 
 #ifndef __REPORTDATA_H__
 #define __REPORTDATA_H__
-extern int parse_historyfile(FILE *fd, reportinfo_t *repinfo, char *hostname, char *servicename);
+
+typedef struct {
+        time_t starttime;
+        time_t duration;
+        int color;
+        char *cause;
+	char *timespec;
+        void *next;
+} replog_t;
+extern replog_t *reploghead;
+
+extern int parse_historyfile(FILE *fd, reportinfo_t *repinfo, char *hostname, char *servicename, time_t fromtime, time_t totime);
 #endif
 
