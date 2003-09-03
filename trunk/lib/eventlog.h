cdd03c37e313d05a01301b9656008399a23ac1ff
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-09-03 20:23:37 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@904 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/lib/eventlog.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Big Brother webpage generator tool.                                        */
+/*                                                                            */
+/* This is a replacement for the "mkbb.sh" and "mkbb2.sh" scripts from the    */
+/* "Big Brother" monitoring tool from BB4 Technologies.                       */
+/*                                                                            */
+/* Primary reason for doing this: Shell scripts perform badly, and with a     */
+/* medium-sized installation (~150 hosts) it takes several minutes to         */
+/* generate the webpages. This is a problem, when the pages are used for      */
+/* 24x7 monitoring of the system status.                                      */
+/*                                                                            */
+/* Copyright (C) 2002 Henrik Storner <henrik@storner.dk>                      */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef __EVENTLOG_H_
+#define __EVENTLOG_H_
+
+extern char *eventignorecolumns;
+
+extern void do_eventlog(FILE *output, int maxcount, int maxminutes, int allowallhosts);
+
+#endif
