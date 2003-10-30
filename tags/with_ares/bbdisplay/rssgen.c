18ff036dbf377aaa2145f39f64896adcdf5a5a80
(no author) <(no author)@44351d6e-118b-4698-b696-ce33095ecaa4>
2003-10-30 13:10:59 +0000

    This commit was manufactured by cvs2svn to create tag 'with_ares'.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1067 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ tags/with_ares/bbdisplay/rssgen.c
@@ -0,0 +1,260 @@
+/*----------------------------------------------------------------------------*/
+/* Big Brother webpage generator tool.                                        */
+/*                                                                            */
+/* This file contains code to generate RSS/RDF format output of alerts.       */
+/* It is heavily influenced by Jeff Stoner's bb_content-feed script.          */
+/*                                                                            */
+/* Copyright (C) 2003 Henrik Storner <henrik@storner.dk>                      */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: rssgen.c,v 1.3 2003-08-11 21:32:43 henrik Exp $";
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "bbgen.h"
+#include "util.h"
+#include "rssgen.h"
+
+char *rssfilename = NULL;
+char *rssversion = "0.91";
+char *nssidebarfilename = NULL;
+
+#define RSS091 0
+#define RSS092 1
+#define RSS10  2
+#define RSS20  3
+
+void do_rss_feed(void)
+{
+	FILE *fd;
+	char tmpfn[MAX_PATH];
+	char destfn[MAX_PATH];
+	int rssver = 0;
+	int ttlvalue;
+	hostlist_t *h;
+	int anyshown;
+
+	if (rssfilename == NULL) return;
+
+	if      (strcmp(rssversion, "0.91") == 0) rssver = RSS091;
+	else if (strcmp(rssversion, "0.92") == 0) rssver = RSS092;
+	else if (strcmp(rssversion, "1.0") == 0)  rssver = RSS10;
+	else if (strcmp(rssversion, "2.0") == 0)  rssver = RSS20;
+	else {
+		errprintf("Unknown RSS version requested (%s), using 0.91\n", rssversion);
+		rssver = RSS091;
+	}
+
+	ttlvalue = (getenv("BBSLEEP") ? (atoi(getenv("BBSLEEP")) / 60) : 5);
+
+	sprintf(tmpfn, "%s/www/%s.tmp", getenv("BBHOME"), rssfilename);
+	sprintf(destfn, "%s/www/%s", getenv("BBHOME"), rssfilename);
+	fd = fopen(tmpfn, "w");
+	if (fd == NULL) {
+		errprintf("Cannot create RSS/RDF outputfile %s\n", tmpfn);
+		return;
+	}
+
+	switch (rssver) {
+	  case RSS091:
+		fprintf(fd, "<?xml version=\"1.0\"?>\n");
+		fprintf(fd, "<rss version=\"0.91\">\n");
+		fprintf(fd, "<channel>\n");
+		fprintf(fd, "  <title>Big Brother Critical Alerts</title>\n");
+		fprintf(fd, "  <link>%s</link>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "  <description>Last updated on %s</description>\n", timestamp);
+		break;
+	  case RSS092:
+		fprintf(fd, "<?xml version=\"1.0\"?>\n");
+		fprintf(fd, "<rss version=\"0.92\">\n");
+		fprintf(fd, "<channel>\n");
+		fprintf(fd, "  <title>Big Brother Critical Alerts</title>\n");
+		fprintf(fd, "  <link>%s</link>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "  <description>Last updated on %s</description>\n", timestamp);
+		fprintf(fd, "  <image>\n");
+		fprintf(fd, "    <url>%s/gifs/bblogo.gif</url>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "    <title>Big Brother</title>\n");
+		fprintf(fd, "    <link>http://bb4.com</link>\n");
+		fprintf(fd, "  </image>\n");
+		break;
+	  case RSS10:
+		fprintf(fd, "<?xml version=\"1.0\"?>\n");
+		fprintf(fd, "<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns=\"http://purl.org/rss/1.0/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n");
+		fprintf(fd, "  <channel rdf:about=\"%s\">\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "    <title>Big Brother Critical Alerts</title>\n");
+		fprintf(fd, "    <link>%s</link>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "    <description>Last updated on %s</description>\n", timestamp);
+		fprintf(fd, "  </channel>\n");
+		break;
+	  case RSS20:
+		fprintf(fd, "<?xml version=\"1.0\"?>\n");
+		fprintf(fd, "<rss version=\"2.0\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n");
+		fprintf(fd, "  <channel>\n");
+		fprintf(fd, "    <title>Big Brother Critical Alerts</title>\n");
+		fprintf(fd, "    <link>%s</link>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "    <description>Last updated on %s</description>\n", timestamp);
+		fprintf(fd, "    <ttl>%d</ttl>\n", ttlvalue);
+		break;
+	}
+
+	for (h=hosthead, anyshown=0; (h); h=h->next) {
+		entry_t *e;
+
+		if (h->hostentry->color == COL_RED) {
+			for (e=h->hostentry->entries; (e); e=e->next) {
+				if (e->color == COL_RED) {
+					anyshown = 1;
+
+					switch (rssver) {
+					  case RSS091:
+					  case RSS092:
+					  case RSS20:
+						fprintf(fd, "  <item>\n");
+						break;
+
+					  case RSS10:
+						fprintf(fd, "  <item rdf:about=\"%s/bb2.html\">\n",
+							getenv("BBWEBHOSTURL"));
+						break;
+					}
+
+					fprintf(fd, "    <title>%s (%s)</title>\n",
+						h->hostentry->hostname, e->column->name);
+
+					fprintf(fd, "    <link>");
+					if (generate_static()) {
+						fprintf(fd, "%s/html/%s.%s.html",
+							getenv("BBWEBHOSTURL"), 
+							h->hostentry->hostname, 
+							e->column->name);
+					}
+					else {
+						fprintf(fd, "%s%s/bb-hostsvc.sh?HOSTSVC=%s.%s",
+							getenv("BBWEBHOST"),
+							getenv("CGIBINURL"),
+							commafy(h->hostentry->hostname), 
+							e->column->name);
+					}
+					fprintf(fd, "</link>\n");
+
+					fprintf(fd, "  </item>\n");
+				}
+			}
+		}
+	}
+
+	if (!anyshown) {
+		fprintf(fd, "  <item>\n");
+		fprintf(fd, "    <title>No Critical Alerts</title>\n");
+		fprintf(fd, "    <link>%s</link>\n", getenv("BBWEBHOSTURL"));
+		fprintf(fd, "  </item>\n");
+	}
+
+	switch (rssver) {
+	  case RSS091:
+	  case RSS092:
+	  case RSS20:
+		fprintf(fd, "  </channel>\n");
+		fprintf(fd, "</rss>\n");
+		break;
+	  case RSS10:
+		fprintf(fd, "</rdf:RDF>\n");
+		break;
+	}
+
+	fclose(fd);
+	if (rename(tmpfn, destfn) != 0) {
+		errprintf("Cannot move file %s to destination %s\n", tmpfn, destfn);
+	}
+
+	return;
+}
+
+void do_netscape_sidebar(void)
+{
+	FILE *fd;
+	char tmpfn[MAX_PATH];
+	char destfn[MAX_PATH];
+	int ttlvalue;
+	hostlist_t *h;
+	int anyshown;
+
+	if (nssidebarfilename == NULL) return;
+
+	ttlvalue = (getenv("BBSLEEP") ? atoi(getenv("BBSLEEP")) : 300);
+
+	sprintf(tmpfn, "%s/www/%s.tmp", getenv("BBHOME"), nssidebarfilename);
+	sprintf(destfn, "%s/www/%s", getenv("BBHOME"), nssidebarfilename);
+	fd = fopen(tmpfn, "w");
+	if (fd == NULL) {
+		errprintf("Cannot create Netscape sidebar outputfile %s\n", tmpfn);
+		return;
+	}
+
+	fprintf(fd, "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">\n");
+	fprintf(fd, "<HTML>\n");
+	fprintf(fd, "  <HEAD>\n");
+	fprintf(fd, "    <TITLE>Big Brother Critical Alerts</TITLE>\n");
+	fprintf(fd, "    <META NAME=\"Generator\" CONTENT=\"bbgen - generator for Big Brother\">\n");
+	fprintf(fd, "    <META HTTP-EQUIV=\"Refresh\" CONTENT=\"%d; URL=%s/%s\">\n",
+		ttlvalue, getenv("BBWEBHOSTURL"), nssidebarfilename);
+	fprintf(fd, "  </HEAD>\n");
+	fprintf(fd, "  <BODY>\n");
+	fprintf(fd, "    <FONT SIZE=\"-2\">Last updated:<BR>%s<BR></FONT>\n", timestamp);
+	fprintf(fd, "    <UL>\n");
+
+	for (h=hosthead, anyshown=0; (h); h=h->next) {
+		entry_t *e;
+
+		if (h->hostentry->color == COL_RED) {
+			for (e=h->hostentry->entries; (e); e=e->next) {
+				if (e->color == COL_RED) {
+					anyshown = 1;
+
+					fprintf(fd, "      <LI>\n");
+					if (generate_static()) {
+						fprintf(fd, "\t<A TARGET=\"_content\" HREF=\"%s/html/%s.%s.html\">",
+							getenv("BBWEBHOSTURL"), 
+							h->hostentry->hostname, 
+							e->column->name);
+					}
+					else {
+						fprintf(fd, "\t<A TARGET=\"_content\" HREF=\"%s%s/bb-hostsvc.sh?HOSTSVC=%s.%s\">",
+							getenv("BBWEBHOST"),
+							getenv("CGIBINURL"),
+							commafy(h->hostentry->hostname), 
+							e->column->name);
+					}
+					fprintf(fd, "%s (%s)</A>\n",
+						h->hostentry->hostname, e->column->name);
+					fprintf(fd, "      </LI>\n");
+				}
+			}
+		}
+	}
+
+	if (!anyshown) {
+		fprintf(fd, "      <LI>\n");
+		fprintf(fd, "        <A TARGET=\"_content\" HREF=\"%s\">No Critical Alerts</A>\n",
+			getenv("BBWEBHOSTURL"));
+		fprintf(fd, "      </LI>\n");
+	}
+
+	fprintf(fd, "    </UL>\n");
+	fprintf(fd, "  </BODY>\n"),
+	fprintf(fd, "</HTML>\n");
+
+	fclose(fd);
+	if (rename(tmpfn, destfn) != 0) {
+		errprintf("Cannot move file %s to destination %s\n", tmpfn, destfn);
+	}
+
+	return;
+}
+
---
+++ tags/with_ares/web/hobbitsvc-info.c
@@ -0,0 +1,440 @@
+/*----------------------------------------------------------------------------*/
+/* Big Brother webpage generator tool.                                        */
+/*                                                                            */
+/* This is a replacement for the "mkbb.sh" and "mkbb2.sh" scripts from the    */
+/* "Big Brother" monitoring tool from BB4 Technologies.                       */
+/*                                                                            */
+/* Primary reason for doing this: Shell scripts perform badly, and with a     */
+/* medium-sized installation (~150 hosts) it takes several minutes to         */
+/* generate the webpages. This is a problem, when the pages are used for      */
+/* 24x7 monitoring of the system status.                                      */
+/*                                                                            */
+/* Copyright (C) 2002 Henrik Storner <henrik@storner.dk>                      */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: hobbitsvc-info.c,v 1.37 2003-10-14 21:12:37 henrik Exp $";
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <utime.h>
+
+#include "bbgen.h"
+#include "util.h"
+#include "pagegen.h"		/* for documentationurl variable */
+#include "infogen.h"
+#include "alert.h"
+
+char *infocol = "info";
+int enable_infogen = 0;
+int info_update_interval = 300; /* Update INFO pages every N seconds */
+
+static char *service_text(char *svc)
+{
+	if (strlen(svc) == 0) return "&nbsp;";
+
+	if (strcmp(svc, "*") == 0) return "All"; else return svc;
+}
+
+int generate_info(char *infocolumn)
+{
+	hostlist_t *hostwalk, *clonewalk;
+	struct utimbuf logfiletime;
+	int infobuflen = 0;
+	char *infobuf = NULL;
+	char l[MAX_LINE_LEN];
+	int ping, testip, dialup;
+	alertrec_t *alerts;
+
+	if (!run_columngen("info", info_update_interval, enable_infogen))
+		return 1;
+
+	/* Load the alert setup */
+	load_alerts();
+
+	logfiletime.actime = logfiletime.modtime = (time(NULL) + atoi(getenv("PURPLEDELAY"))*60);
+	infobuflen = 4096; infobuf = (char *)malloc(infobuflen); *infobuf = '\0';
+
+	for (hostwalk=hosthead; (hostwalk); hostwalk = hostwalk->next) {
+		char logfn[MAX_PATH], htmlfn[MAX_PATH];
+		FILE *fd;
+		char *p, *alertspec, *slaspec, *noprop, *rawcopy;
+		int firstcontent;
+
+		if (hostwalk->hostentry->banksize > 0) continue; /* No info for modem-banks */
+
+		sprintf(logfn, "%s/%s.%s", getenv("BBLOGS"), 
+			commafy(hostwalk->hostentry->hostname), infocolumn);
+		if (getenv("BBHTML")) {
+			sprintf(htmlfn,"%s/%s.%s.html", getenv("BBHTML"), 
+				hostwalk->hostentry->hostname, infocolumn);
+		}
+		else {
+			sprintf(htmlfn,"%s/www/html/%s.%s.html", getenv("BBHOME"), 
+				hostwalk->hostentry->hostname, infocolumn);
+		}
+
+		*infobuf = '\0';
+		addtobuffer(&infobuf, &infobuflen, "<table width=\"100%%\">\n");
+
+		if (hostwalk->hostentry->displayname && (strcmp(hostwalk->hostentry->displayname, hostwalk->hostentry->hostname) != 0)) {
+			sprintf(l, "<tr><th align=left>Hostname:</th><td align=left>%s (%s)</td></tr>\n", 
+				hostwalk->hostentry->displayname, hostwalk->hostentry->hostname);
+		}
+		else {
+			sprintf(l, "<tr><th align=left>Hostname:</th><td align=left>%s</td></tr>\n", hostwalk->hostentry->hostname);
+		}
+		addtobuffer(&infobuf, &infobuflen, l);
+
+		sprintf(l, "<tr><th align=left>IP:</th><td align=left>%s</td></tr>\n", hostwalk->hostentry->ip);
+		addtobuffer(&infobuf, &infobuflen, l);
+		if (documentationurl) {
+			sprintf(l, "<tr><th align=left>Documentation:</th><td align=left><a href=\"%s\">%s</a>\n", 
+				urldoclink(documentationurl, hostwalk->hostentry->hostname),
+				urldoclink(documentationurl, hostwalk->hostentry->hostname));
+			addtobuffer(&infobuf, &infobuflen, l);
+		}
+		if (hostwalk->hostentry->link != &null_link) {
+			sprintf(l, "<tr><th align=left>Notes:</th><td align=left><a href=\"%s\">%s%s</a>\n", 
+				hostlink(hostwalk->hostentry->link),
+				getenv("BBWEBHOST"),
+				hostlink(hostwalk->hostentry->link));
+			addtobuffer(&infobuf, &infobuflen, l);
+		}
+		sprintf(l, "<tr><th align=left>Page/subpage:</th><td align=left><a href=\"%s/%s\">%s</a>\n", 
+			getenv("BBWEB"), hostpage_link(hostwalk->hostentry), hostpage_name(hostwalk->hostentry));
+		addtobuffer(&infobuf, &infobuflen, l);
+		for (clonewalk = hostwalk->clones; (clonewalk); clonewalk = clonewalk->next) {
+			sprintf(l, "<br><a href=\"%s/%s\">%s</a>\n", 
+				getenv("BBWEB"), hostpage_link(clonewalk->hostentry), hostpage_name(clonewalk->hostentry));
+			addtobuffer(&infobuf, &infobuflen, l);
+		}
+		addtobuffer(&infobuf, &infobuflen, "</td></tr>\n");
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		if (hostwalk->hostentry->description) {
+			char *delim = strchr(hostwalk->hostentry->description, ':');
+
+			if (delim) *delim = '\0';
+			sprintf(l, "<tr><th align=left>Host type:</th><td align=left>%s</td></tr>\n",
+				hostwalk->hostentry->description);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (delim) { 
+				*delim = ':'; 
+				delim++;
+				sprintf(l, "<tr><th align=left>Description:</th><td align=left>%s</td></tr>\n", delim);
+				addtobuffer(&infobuf, &infobuflen, l);
+			}
+		}
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		p = hostwalk->hostentry->alerts;
+		if (p) {
+			alertspec = (p+1); /* Skip leading comma */
+			p = alertspec + strlen(alertspec) - 1; /* Point to trailing comma */
+			if (*p == ',') *p = '\0'; else p = NULL;
+
+			sprintf(l, "<tr><th align=left>NK Alerts:</th><td align=left>%s", alertspec); 
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ',';
+
+			slaspec = strstr(hostwalk->hostentry->rawentry, "NKTIME=");
+			if (slaspec) {
+				slaspec +=7;
+				p = strchr(slaspec, ' ');
+				if (p) *p = '\0';
+				sprintf(l, " (%s)", slaspec);
+				addtobuffer(&infobuf, &infobuflen, l);
+				if (p) *p = ' ';
+			}
+			else addtobuffer(&infobuf, &infobuflen, " (24x7)");
+
+			addtobuffer(&infobuf, &infobuflen, "</td></tr>\n");
+		}
+		else {
+			addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>NK alerts:</th><td align=left>None</td></tr>\n");
+		}
+		slaspec = strstr(hostwalk->hostentry->rawentry, "NKTIME=");
+		if (slaspec) {
+			slaspec +=7;
+			p = strchr(slaspec, ' ');
+			if (p) *p = '\0';
+			sprintf(l, "<tr><th align=left>NK alerts shown:</th><td align=left>%s</td></tr>\n", slaspec);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ' ';
+		}
+		slaspec = strstr(hostwalk->hostentry->rawentry, "SLA=");
+		if (slaspec) {
+			slaspec +=4;
+			p = strchr(slaspec, ' ');
+			if (p) *p = '\0';
+			sprintf(l, "<tr><th align=left>Alert times:</th><td align=left>%s</td></tr>\n", slaspec);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ' ';
+		}
+		slaspec = strstr(hostwalk->hostentry->rawentry, "DOWNTIME=");
+		if (slaspec) {
+			slaspec +=9;
+			p = strchr(slaspec, ' ');
+			if (p) *p = '\0';
+			sprintf(l, "<tr><th align=left>Planned downtime:</th><td align=left>%s</td></tr>\n", slaspec);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ' ';
+		}
+		slaspec = strstr(hostwalk->hostentry->rawentry, "REPORTTIME=");
+		if (slaspec) {
+			slaspec +=11;
+			p = strchr(slaspec, ' ');
+			if (p) *p = '\0';
+			sprintf(l, "<tr><th align=left>SLA Report period:</th><td align=left>%s</td></tr>\n", slaspec);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ' ';
+			sprintf(l, "<tr><th align=left>SLA Availability:</th><td align=left>%.2f</td></tr>\n", hostwalk->hostentry->reportwarnlevel); 
+			addtobuffer(&infobuf, &infobuflen, l);
+		}
+		if (hostwalk->hostentry->nopropyellowtests) {
+			noprop = (hostwalk->hostentry->nopropyellowtests+1);
+			p = noprop + strlen(noprop) - 1; /* Point to trailing comma */
+			if (*p == ',') *p = '\0'; else p = NULL;
+
+			sprintf(l, "<tr><th align=left>Suppressed warnings (yellow):</th><td align=left>%s</td></tr>\n", noprop);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ',';
+		}
+		if (hostwalk->hostentry->nopropredtests) {
+			noprop = (hostwalk->hostentry->nopropredtests+1);
+			p = noprop + strlen(noprop) - 1; /* Point to trailing comma */
+			if (*p == ',') *p = '\0'; else p = NULL;
+
+			sprintf(l, "<tr><th align=left>Suppressed alarms (red):</th><td align=left>%s</td></tr>\n", noprop);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ',';
+		}
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		p = strstr(hostwalk->hostentry->rawentry, "NET:");
+		if (p) {
+			char *location;
+			p += 4; location = p;
+			p = strchr(location, ' ');
+			if (p) *p = '\0';
+			sprintf(l, "<tr><th align=left>Tested from network:</th><td align=left>%s</td></tr>\n", location);
+			addtobuffer(&infobuf, &infobuflen, l);
+			if (p) *p = ' ';
+		}
+
+		dialup = 0;
+		if (strstr(hostwalk->hostentry->rawentry, "dialup")) dialup = 1;
+		if (dialup) addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2 align=left>Host downtime does not trigger alarms (dialup host)</td></tr>\n");
+
+		testip = 0;
+		if (strstr(hostwalk->hostentry->rawentry, "testip")) testip = 1;
+		sprintf(l, "<tr><th align=left>Network tests use:</th><td align=left>%s</td></tr>\n", 
+			(testip ? "IP-address" : "Hostname"));
+		addtobuffer(&infobuf, &infobuflen, l);
+
+		ping = 1;
+		if (strstr(hostwalk->hostentry->rawentry, "noping")) ping = 0;
+		if (strstr(hostwalk->hostentry->rawentry, "noconn")) ping = 0;
+		sprintf(l, "<tr><th align=left>Checked with ping:</th><td align=left>%s</td></tr>\n", (ping ? "Yes" : "No"));
+		addtobuffer(&infobuf, &infobuflen, l);
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		rawcopy = malcop(hostwalk->hostentry->rawentry);
+		firstcontent = 1;
+		p = strtok(rawcopy, " \t");
+		while (p) {
+			if (strncmp(p, "http", 4) == 0) {
+				if (firstcontent) {
+					addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>URL checks:</th><td align=left>\n");
+					firstcontent = 0;
+				}
+
+				sprintf(l, "<a href=\"%s\">%s</a><br>\n", 
+					realurl(p, NULL, NULL, NULL, NULL), 
+					realurl(p, NULL, NULL, NULL, NULL)); 
+				addtobuffer(&infobuf, &infobuflen, l);
+			}
+			p = strtok(NULL, " \t");
+		}
+		if (!firstcontent) addtobuffer(&infobuf, &infobuflen, "</td></tr>\n");
+
+		strcpy(rawcopy, hostwalk->hostentry->rawentry);
+		firstcontent = 1;
+		p = strtok(rawcopy, " \t");
+		while (p) {
+			if ( (strncmp(p, "content=", 8) == 0) ||
+			     (strncmp(p, "cont;", 5) == 0)    ||
+			     (strncmp(p, "post;", 5) == 0)       ) {
+
+				if (firstcontent) {
+					addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>Content checks:</th><td align=left>\n");
+					firstcontent = 0;
+				}
+
+				sprintf(l, "<a href=\"%s\">%s</a>", 
+					realurl(p, NULL, NULL, NULL, NULL), 
+					realurl(p, NULL, NULL, NULL, NULL)); 
+				addtobuffer(&infobuf, &infobuflen, l);
+				if ((strncmp(p, "cont;", 5) == 0) || (strncmp(p, "post;", 5) == 0)) {
+					char *wanted = strrchr(p, ';');
+
+					if (wanted) {
+						wanted++;
+						sprintf(l, "&nbsp; must return '%s'", wanted);
+						addtobuffer(&infobuf, &infobuflen, l);
+					}
+				}
+				addtobuffer(&infobuf, &infobuflen, "<br>\n");
+			}
+			p = strtok(NULL, " \t");
+		}
+		if (!firstcontent) addtobuffer(&infobuf, &infobuflen, "</td></tr>\n");
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		alerts = find_alert(hostwalk->hostentry->hostname, 0, 0);
+		if (!dialup) {
+			if (alerts) {
+				addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>E-mail/SMS alerting:</th><td align=left>\n");
+				addtobuffer(&infobuf, &infobuflen, "<table width=\"100%%\" border=1>\n");
+				addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>Services</th><th align=left>Ex.Services</th><th align=left>Weekdays</th><th align=left>Time</th><th align=left>Recipients</th></tr>\n");
+				while (alerts) {
+					char *recips = malcop(alerts->items[6]);
+					char *onercpt;
+
+					addtobuffer(&infobuf, &infobuflen, "<tr>\n");
+
+					sprintf(l, "<td align=left>%s</td>\n", service_text(alerts->items[2]));
+					addtobuffer(&infobuf, &infobuflen, l);
+					sprintf(l, "<td align=left>%s</td>\n", service_text(alerts->items[3]));
+					addtobuffer(&infobuf, &infobuflen, l);
+					sprintf(l, "<td align=left>%s</td>\n", weekday_text(alerts->items[4]));
+					addtobuffer(&infobuf, &infobuflen, l);
+					sprintf(l, "<td align=left>%s</td>\n", time_text(alerts->items[5]));
+					addtobuffer(&infobuf, &infobuflen, l);
+
+					addtobuffer(&infobuf, &infobuflen, "<td align=left>");
+					onercpt = strtok(recips, " \t");
+					while (onercpt) {
+						addtobuffer(&infobuf, &infobuflen, onercpt);
+						onercpt = strtok(NULL, " \t");
+						if (onercpt) addtobuffer(&infobuf, &infobuflen, "<br>");
+					}
+					addtobuffer(&infobuf, &infobuflen, "</td>\n");
+
+					addtobuffer(&infobuf, &infobuflen, "</tr>\n");
+
+					alerts = find_alert(hostwalk->hostentry->hostname, 0, 1);
+				}
+				addtobuffer(&infobuf, &infobuflen, "</table>\n");
+
+				addtobuffer(&infobuf, &infobuflen, "</td></tr>\n");
+
+				sprintf(l, "<tr><th align=left>Default time between each alert:</th><td align=left>%d minutes</td></tr>\n", 
+					pagedelay);
+				addtobuffer(&infobuf, &infobuflen, l);
+			}
+			else {
+				addtobuffer(&infobuf, &infobuflen, "<tr><th colspan=2 align=left>No e-mail/SMS alerting defined</th></tr>\n");
+			}
+		}
+		addtobuffer(&infobuf, &infobuflen, "<tr><td colspan=2>&nbsp;</td></tr>\n");
+
+		addtobuffer(&infobuf, &infobuflen, "<tr><th align=left>Other tags:</th><td align=left>");
+		strcpy(rawcopy, hostwalk->hostentry->rawentry); /* Already allocated */
+		p = strtok(rawcopy, " \t");
+		while (p) {
+			if ((strncmp(p, "NAME:", 5) == 0) || (strncmp(p, "COMMENT:", 8) == 0)) {
+				char *p2 = strchr(p, ':');
+
+				p2++;
+				if (*p2 == '"') {
+					/* See if the end '"' is already in the token */
+					p2++;
+					if (strchr(p2, '"') == NULL) {
+						/* Skip to the next '"' or end-of-line */
+						p = strtok(NULL, "\"\r\n");
+					}
+				}
+			}
+			else if (
+					(strncmp(p, "#", 1) != 0)
+				&&	(strncmp(p, "NK:", 3) != 0)
+				&&	(strncmp(p, "NET:", 4) != 0)
+				&&	(strncmp(p, "NOPROP:", 7) != 0)
+				&&	(strncmp(p, "NOPROPRED:", 10) != 0)
+				&&	(strncmp(p, "NOPROPYELLOW:", 13) != 0)
+				&&	(strncmp(p, "SLA=", 4) != 0)
+				&&	(strncmp(p, "NKTIME=", 7) != 0)
+				&&	(strncmp(p, "DOWNTIME=", 9) != 0)
+				&&	(strncmp(p, "REPORTTIME=", 11) != 0)
+				&&	(strncmp(p, "WARNPCT:", 8) != 0)
+				&&	(strncmp(p, "http", 4) != 0)
+				&&	(strncmp(p, "content=", 8) != 0)
+				&&	(strncmp(p, "cont;", 5)  != 0)
+				&&	(strncmp(p, "testip", 6) != 0)
+				&&	(strncmp(p, "dialup", 6) != 0)
+				&&	(strncmp(p, "noconn", 6) != 0)
+				&&	(strncmp(p, "noping", 6) != 0)
+			   )  {
+				sprintf(l, "%s ", p);
+				addtobuffer(&infobuf, &infobuflen, l);
+			}
+
+			p = strtok(NULL, " \t");
+		}
+		free(rawcopy);
+		addtobuffer(&infobuf, &infobuflen, "</td></tr>\n</table>\n");
+
+		fd = fopen(logfn, "w");
+		if (!fd) {
+			errprintf("Cannot open info logfile %s\n", logfn);
+			return 1;
+		}
+
+		fprintf(fd, "green %s info\n\n", timestamp);
+		fprintf(fd, "%s", infobuf);
+		fclose(fd);
+		utime(logfn, &logfiletime);
+
+		/* HTML files generated only if we use BBLOGSTATUS=STATIC */
+		if (generate_static()) {
+			sethostenv(hostwalk->hostentry->hostname, hostwalk->hostentry->ip, infocolumn, "green");
+			fd = fopen(htmlfn, "w");
+			if (!fd) {
+				errprintf("Cannot open info html logfile %s\n", htmlfn);
+				return 1;
+			}
+ 			headfoot(fd, "hostsvc", "", "header", COL_GREEN);
+
+			fprintf(fd, "<!-- Start of code generated by Big Brother Systems and Network Monitor -->\n");
+			fprintf(fd, "\n");
+			fprintf(fd, "<A NAME=begindata>&nbsp;</A>\n");
+			fprintf(fd, "\n");
+			fprintf(fd, "<CENTER><TABLE ALIGN=CENTER BORDER=0 WIDTH=\"80%%\">\n");
+			fprintf(fd, "<TR><TH ALIGN=CENTER><FONT %s>", getenv("MKBBROWFONT"));
+			fprintf(fd, "%s - %s</FONT><BR><HR WIDTH=\"100%%\"></TH></TR>\n", hostwalk->hostentry->hostname, infocolumn);
+			fprintf(fd, "<TR><TD>\n%s\n</TD></TR>\n", infobuf);
+			fprintf(fd, "</TABLE>\n");
+			fprintf(fd, "\n");
+			fprintf(fd, "</CENTER>\n");
+			fprintf(fd, "\n");
+			fprintf(fd, "<!-- End of code generated by Big Brother Systems and Network Monitor -->\n");
+
+			headfoot(fd, "hostsvc", "", "footer", COL_GREEN);
+			fclose(fd);
+		}
+	}
+
+	free(infobuf);
+	return 0;
+}
+
