fd5d0ff288182dcf822fabc13a1a10e8ec4b7ec2
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-08-07 11:11:38 +0000

    Load tcp test definitions from $BBHOME/etc/services.txt
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1228 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbnet/contest.c
+++ trunk/bbnet/contest.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: contest.c,v 1.42 2004-08-05 22:15:54 henrik Exp $";
+static char rcsid[] = "$Id: contest.c,v 1.43 2004-08-07 11:11:38 henrik Exp $";
 
 #include <sys/time.h>
 #include <sys/types.h>
@@ -49,50 +49,307 @@ static char rcsid[] = "$Id: contest.c,v 1.42 2004-08-05 22:15:54 henrik Exp $";
 static test_t *thead = NULL;
 
 /*
- * tnsping data taken from http://www.mail-archive.com/whatsup_forum@list.ipswitch.com/msg06678.html:
- * "%0%87%0%0%1%0%0%0%1%54%1%44%0%0%8%0%127%255%163%10%0%0%1%0%0%29%0%58%0%0%0% 0%0%0%0%0%0%0%0%0%0%0%0%0%2%8%0%0%0%1%0%0%0%0%0%0%0%0(CONNECT_DATA=(COMMAND=ping))"
- */
-
-static unsigned char tnspingcmd[] = "\00\x57\x00\x00\x01\x00\x00\x00\x01\x36\x01\x2C\x00\x00\x08\x00\x7F\xFF\xA3\x0A\x00\x00\x01\x00\x00\x1D\x00\x3A\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x08\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00(CONNECT_DATA=(COMMAND=ping))";
-
-/*
  * Services we know how to handle:
  * This defines what to send to them to shut down the 
  * session nicely, and whether we want to grab the
  * banner or not.
  */
-static svcinfo_t svcinfo[] = {
-	{ "ftp",     "quit\r\n",          0,                  "220",	0, 0,	(TCP_GET_BANNER) },
-	{ "ssh",     NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER) },
-	{ "ssh1",    NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER) },
-	{ "ssh2",    NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER) },
-	{ "telnet",  NULL,                0,                  NULL,	0, 0,	(TCP_GET_BANNER|TCP_TELNET) },
-	{ "smtp",    "mail\r\nquit\r\n",  0,                  "220",	0, 0,	(TCP_GET_BANNER) }, /* Send "MAIL" to avoid sendmail NOQUEUE logs */
-	{ "pop",     "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "pop2",    "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "pop-2",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "pop3",    "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "pop-3",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "imap",    "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "imap2",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "imap3",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "imap4",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER) },
-	{ "nntp",    "quit\r\n",          0,                  "200",	0, 0,	(TCP_GET_BANNER) },
-	{ "ldap",    NULL,                0,                  NULL,     0, 0,	(0) },
-	{ "rsync",   NULL,                0,                  "@RSYNCD",0, 0,	(TCP_GET_BANNER) },
-	{ "bbd",     "dummy",             0,                  NULL,	0, 0,	(0) },
-	{ "ftps",    "quit\r\n",          0,                  "220",	0, 0,	(TCP_GET_BANNER|TCP_SSL) },
-	{ "telnets", NULL,                0,                  NULL, 	0, 0,	(TCP_GET_BANNER|TCP_TELNET|TCP_SSL) },
-	{ "smtps",   "mail\r\nquit\r\n",  0,                  "220",	0, 0,	(TCP_GET_BANNER|TCP_SSL) }, /* Non-standard - IANA */
-	{ "pop3s",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER|TCP_SSL) },
-	{ "imaps",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER|TCP_SSL) },
-	{ "nntps",   "quit\r\n",          0,                  "200",	0, 0,	(TCP_GET_BANNER|TCP_SSL) },
-	{ "ldaps",   NULL,                0,                  NULL,     0, 0,	(TCP_SSL) },
-	{ "clamd",   "PING\r\n",          0,                  "PONG",   0, 0,	(0) },
-	{ "oratns",  tnspingcmd,          sizeof(tnspingcmd), NULL,     0, 0,	(TCP_GET_BANNER) },
-	{ NULL,      NULL,                0,                  NULL,	0, 0,	(0) }	/* Default behaviour: Just try a connect */
+static svcinfo_t default_svcinfo[] = {
+	/*           ------------- data to send ------   ---- green data ------ flags */
+	/* name      databytes            length          databytes offset len        */
+	{ "ftp",     "quit\r\n",          0,                  "220",	0, 0,	(TCP_GET_BANNER), 21 },
+	{ "ssh",     NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER), 22 },
+	{ "ssh1",    NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER), 22 },
+	{ "ssh2",    NULL,                0,                  "SSH",	0, 0,	(TCP_GET_BANNER), 22 },
+	{ "telnet",  NULL,                0,                  NULL,	0, 0,	(TCP_GET_BANNER|TCP_TELNET), 23 },
+	{ "smtp",    "mail\r\nquit\r\n",  0,                  "220",	0, 0,	(TCP_GET_BANNER), 25 }, /* Send "MAIL" to avoid sendmail NOQUEUE logs */
+	{ "pop",     "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER), 110 },
+	{ "pop2",    "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER), 109 },
+	{ "pop-2",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER), 109 },
+	{ "pop3",    "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER), 110 },
+	{ "pop-3",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER), 110 },
+	{ "imap",    "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER), 143 },
+	{ "imap2",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER), 143 },
+	{ "imap3",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER), 220 },
+	{ "imap4",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER), 143 },
+	{ "nntp",    "quit\r\n",          0,                  "200",	0, 0,	(TCP_GET_BANNER), 119 },
+	{ "ldap",    NULL,                0,                  NULL,     0, 0,	(0), 389 },
+	{ "rsync",   NULL,                0,                  "@RSYNCD",0, 0,	(TCP_GET_BANNER), 873 },
+	{ "bbd",     "dummy",             0,                  NULL,	0, 0,	(0), 1984 },
+	{ "ftps",    "quit\r\n",          0,                  "220",	0, 0,	(TCP_GET_BANNER|TCP_SSL), 990 },
+	{ "telnets", NULL,                0,                  NULL, 	0, 0,	(TCP_GET_BANNER|TCP_TELNET|TCP_SSL), 992 },
+	{ "smtps",   "mail\r\nquit\r\n",  0,                  "220",	0, 0,	(TCP_GET_BANNER|TCP_SSL), 0 }, /* Non-standard - IANA */
+	{ "pop3s",   "quit\r\n",          0,                  "+OK",	0, 0,	(TCP_GET_BANNER|TCP_SSL), 995 },
+	{ "imaps",   "ABC123 LOGOUT\r\n", 0,                  "* OK",	0, 0,	(TCP_GET_BANNER|TCP_SSL), 993 },
+	{ "nntps",   "quit\r\n",          0,                  "200",	0, 0,	(TCP_GET_BANNER|TCP_SSL), 563 },
+	{ "ldaps",   NULL,                0,                  NULL,     0, 0,	(TCP_SSL), 636 },
+	{ "clamd",   "PING\r\n",          0,                  "PONG",   0, 0,	(0), 3310 },
+	{ NULL,      NULL,                0,                  NULL,	0, 0,	(0), 0 }	/* Default behaviour: Just try a connect */
 };
 
+static svcinfo_t *svcinfo = default_svcinfo;
+
+typedef struct svclist_t {
+	struct svcinfo_t *rec;
+	struct svclist_t *next;
+} svclist_t;
+
+
+static char *binview(unsigned char *buf, int buflen)
+{
+	static char hexchars[16] = "0123456789ABCDEF";
+	static char result[MAX_LINE_LEN];
+	unsigned char *inp, *outp;
+	int i;
+
+	if (buf && (buflen == 0)) buflen = strlen(buf);
+	for (inp=buf, i=0, outp=result; (i<buflen); i++,inp++) {
+		if (isprint(*inp)) {
+			*outp = *inp;
+			outp++;
+		}
+		else if (*inp == '\r') {
+			*outp = '\\'; outp++;
+			*outp = 'r'; outp++;
+		}
+		else if (*inp == '\n') {
+			*outp = '\\'; outp++;
+			*outp = 'n'; outp++;
+		}
+		else if (*inp == '\t') {
+			*outp = '\\'; outp++;
+			*outp = 't'; outp++;
+		}
+		else {
+			*outp = '\\'; outp++;
+			*outp = 'x'; outp++;
+			*outp = hexchars[*inp / 16]; outp++;
+			*outp = hexchars[*inp % 16]; outp++;
+		}
+	}
+	*outp = '\0';
+
+	return result;
+}
+
+static void getescapestring(char *msg, unsigned char **buf, int *buflen, int *offset)
+{
+	char *inp, *outp;
+	int outlen = 0;
+
+	inp = msg;
+	if (*inp != '\"') {
+		errprintf("services.txt error - strings must be in quotes\n");
+		return;
+	}
+	inp++; /* Skip the quote */
+
+	outp = *buf = malloc(strlen(msg)+1);
+	while (*inp && (*inp != '\"')) {
+		if (*inp == '\\') {
+			inp++;
+			if (*inp == 'r') {
+				*outp = '\r'; outlen++; inp++; outp++;
+			}
+			else if (*inp == 'n') {
+				*outp = '\n'; outlen++; inp++; outp++;
+			}
+			else if (*inp == 't') {
+				*outp = '\t'; outlen++; inp++; outp++;
+			}
+			else if (*inp == 'x') {
+				inp++;
+				if (isxdigit((int) *inp)) {
+					*outp = hexvalue(*inp);
+					inp++;
+
+					if (isxdigit((int) *inp)) {
+						*outp *= 16;
+						*outp += hexvalue(*inp);
+						inp++;
+					}
+				}
+				else {
+					errprintf("Invalid hex escape in service.txt\n");
+				}
+				outlen++; outp++;
+			}
+			else {
+				errprintf("Unknown escape sequence in service.txt at: %s\n", inp);
+			}
+		}
+		else {
+			*outp = *inp;
+			outlen++;
+			inp++; outp++;
+		}
+	}
+	*outp = '\0';
+	*buflen = outlen;
+}
+
+char *init_tcp_services(void)
+{
+	char filename[MAX_PATH];
+	FILE *fd;
+	char buf[MAX_LINE_LEN];
+	svclist_t *head = NULL;
+	svclist_t *item = NULL;
+	svclist_t *first, *walk;
+	char *bbnetsvcs = NULL;
+	char *searchstring;
+	int svcnamebytes = 0;
+	int svccount = 1;
+	int i;
+
+	sprintf(filename, "%s/etc/services.txt", getenv("BBHOME"));
+	fd = fopen(filename, "r");
+	if (fd == NULL) {
+		errprintf("Cannot open TCP service-definitions file %s - using defaults\n", filename);
+		return malcop(getenv("BBNETSVCS"));
+	}
+
+	head = (svclist_t *)malloc(sizeof(svclist_t));
+	head->rec = (svcinfo_t *)calloc(1, sizeof(svcinfo_t));
+	head->next = NULL;
+
+	while (fd && fgets(buf, sizeof(buf), fd)) {
+		char *l;
+
+		l = strchr(buf, '\n'); if (l) *l = '\0';
+		l = skipwhitespace(buf);
+
+		if (strncmp(l, "service ", 8) == 0) {
+			char *svcname;
+
+			l = skipwhitespace(l+7);
+			svcname = strtok(l, "|");
+			first = NULL;
+			while (svcname) {
+				item = (svclist_t *) malloc(sizeof(svclist_t));
+				item->rec = (svcinfo_t *)calloc(1, sizeof(svcinfo_t));
+				item->rec->svcname = malcop(svcname);
+				svcnamebytes += (strlen(svcname) + 1);
+				item->next = head;
+				head = item;
+				svcname = strtok(NULL, "|");
+				if (first == NULL) first = item;
+				svccount++;
+			}
+		}
+		else if (strncmp(l, "send ", 5) == 0) {
+			if (item) {
+				getescapestring(skipwhitespace(l+4), &item->rec->sendtxt, &item->rec->sendlen, NULL);
+				for (walk = item; (walk != first); walk = walk->next) {
+					walk->next->rec->sendtxt = item->rec->sendtxt;
+					walk->next->rec->sendlen = item->rec->sendlen;
+				}
+			}
+		}
+		else if (strncmp(l, "expect ", 7) == 0) {
+			if (item) {
+				getescapestring(skipwhitespace(l+7), &item->rec->exptext, &item->rec->explen, &item->rec->expofs);
+				for (walk = item; (walk != first); walk = walk->next) {
+					walk->next->rec->exptext = item->rec->exptext;
+					walk->next->rec->explen = item->rec->explen;
+					walk->next->rec->expofs = item->rec->expofs;
+				}
+			}
+		}
+		else if (strncmp(l, "options ", 8) == 0) {
+			if (item) {
+				char *opt;
+
+				item->rec->flags = 0;
+				l = skipwhitespace(l+7);
+				opt = strtok(l, ",");
+				while (opt) {
+					if      (strcmp(opt, "ssl") == 0)    item->rec->flags += TCP_SSL;
+					else if (strcmp(opt, "banner") == 0) item->rec->flags += TCP_GET_BANNER;
+					else if (strcmp(opt, "telnet") == 0) item->rec->flags += TCP_TELNET;
+					else errprintf("Unknown option: %s\n", opt);
+
+					opt = strtok(NULL, ",");
+				}
+			}
+		}
+		else if (strncmp(l, "port ", 5) == 0) {
+			if (item) {
+				item->rec->port = atoi(skipwhitespace(l+4));
+				for (walk = item; (walk != first); walk = walk->next) {
+					walk->next->rec->port = item->rec->port;
+				}
+			}
+		}
+	}
+
+	if (fd) fclose(fd);
+
+	svcinfo = (svcinfo_t *) malloc(svccount * sizeof(svcinfo_t));
+	for (walk=head, i=0; (walk); walk = walk->next, i++) {
+		svcinfo[i].svcname = walk->rec->svcname;
+		svcinfo[i].sendtxt = walk->rec->sendtxt;
+		svcinfo[i].sendlen = walk->rec->sendlen;
+		svcinfo[i].exptext = walk->rec->exptext;
+		svcinfo[i].explen  = walk->rec->explen;
+		svcinfo[i].expofs  = walk->rec->expofs;
+		svcinfo[i].flags   = walk->rec->flags;
+		svcinfo[i].port    = walk->rec->port;
+	}
+
+	searchstring = malcop(getenv("BBNETSVCS"));
+	bbnetsvcs = (char *) malloc(strlen(getenv("BBNETSVCS")) + svcnamebytes + 1);
+	strcpy(bbnetsvcs, getenv("BBNETSVCS"));
+	for (i=0; (svcinfo[i].svcname); i++) {
+		char *p;
+
+		strcpy(searchstring, getenv("BBNETSVCS"));
+		p = strtok(searchstring, " ");
+		while (p && (strcmp(p, svcinfo[i].svcname) != 0)) p = strtok(NULL, " ");
+
+		if (p == NULL) {
+			strcat(bbnetsvcs, " ");
+			strcat(bbnetsvcs, svcinfo[i].svcname);
+		}
+	}
+	free(searchstring);
+
+	if (debug) {
+		dump_tcp_services();
+		printf("BBNETSVCS set to : %s\n", bbnetsvcs);
+	}
+
+	return bbnetsvcs;
+}
+
+int default_tcp_port(char *svcname)
+{
+	int svcidx;
+	int result = 0;
+
+	for (svcidx=0; (svcinfo[svcidx].svcname && (strcmp(svcname, svcinfo[svcidx].svcname) != 0)); svcidx++) ;
+	if (svcinfo[svcidx].svcname) result = svcinfo[svcidx].port;
+
+	return result;
+}
+
+void dump_tcp_services(void)
+{
+	int i;
+
+	printf("Service list dump\n");
+	for (i=0; (svcinfo[i].svcname); i++) {
+		printf(" Name      : %s\n", svcinfo[i].svcname);
+		printf("   Sendtext: %s\n", binview(svcinfo[i].sendtxt, svcinfo[i].sendlen));
+		printf("   Sendlen : %d\n", svcinfo[i].sendlen);
+		printf("   Exp.text: %s\n", binview(svcinfo[i].exptext, svcinfo[i].explen));
+		printf("   Exp.len : %d\n", svcinfo[i].explen);
+		printf("   Exp.ofs : %d\n", svcinfo[i].expofs);
+		printf("   Flags   : %d\n", svcinfo[i].flags);
+		printf("   Port    : %d\n", svcinfo[i].port);
+	}
+	printf("\n");
+}
 
 test_t *add_tcp_test(char *ip, int port, char *service, int silent)
 {
