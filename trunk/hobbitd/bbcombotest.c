b5b81dd528f47d6225577afe4be26fdc6b07ae71
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-07 21:14:45 +0000

    bbgenlogd now includes "sender" in message
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1498 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/bbcombotest.c
+++ trunk/hobbitd/bbcombotest.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: bbcombotest.c,v 1.20 2004-10-07 16:30:21 henrik Exp $";
+static char rcsid[] = "$Id: bbcombotest.c,v 1.21 2004-10-07 21:14:45 henrik Exp $";
 
 #include <stdio.h>
 #include <string.h>
@@ -206,7 +206,7 @@ static int getbbgendvalue(char *hostname, char *testname, char **errptr)
 	}
 
 	if (found) {
-		/* hostname|testname|color|testflags|lastchange|expires|1st line of message */
+		/* hostname|testname|color|testflags|lastchange|expires|sender|1st line of message */
 		colstr = found + strlen(pattern);
 		p = strchr(colstr, '|');
 		if (p) {
