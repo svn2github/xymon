b50f6fda3601e44627cfacd3aa1984d919191b45
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-08 12:20:43 +0000

    Add LARRD and HISTORY support. Use sane defaults for most of the environments taken from BB.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1503 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/web/hobbitsvc.c
+++ trunk/web/hobbitsvc.c
@@ -2,13 +2,62 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #include "bbgen.h"
 #include "debug.h"
 #include "util.h"
 #include "sendmsg.h"
 
-#ifdef CGI
+typedef struct {
+   char *bbsvcname;
+   char *larrdsvcname;
+   char *larrdpartname;
+} larrdsvc_t;
+
+static larrdsvc_t *larrdsvcs = NULL;
+
+larrdsvc_t *find_larrd(char *service, char *flags)
+{
+	larrdsvc_t *result;
+	larrdsvc_t *lrec;
+
+	if (strchr(flags, 'R')) {
+		/* Dont do LARRD for reverse tests, since they have no data */
+		return NULL;
+	}
+
+	if (larrdsvcs == NULL) {
+		char *lenv = getenv("LARRDS");
+		int lcount = 0;
+		char *ldef, *p;
+
+		p = lenv; do { lcount++; p = strchr(p+1, ','); } while (p);
+		larrdsvcs = (larrdsvc_t *)calloc(sizeof(larrdsvc_t), (lcount+1));
+
+		lrec = larrdsvcs; ldef = strtok(lenv, ",");
+		while (ldef) {
+			p = strchr(ldef, '=');
+			if (p) {
+				*p = '\0'; 
+				lrec->bbsvcname = strdup(ldef);
+				lrec->larrdsvcname = strdup(p+1);
+			}
+			else {
+				lrec->bbsvcname = lrec->larrdsvcname = strdup(ldef);
+			}
+
+			if (strcmp(ldef, "disk") == 0) lrec->larrdpartname = "disk_part";
+
+			ldef = strtok(NULL, ",");
+			lrec++;
+		}
+	}
+
+	lrec = larrdsvcs; while (lrec->bbsvcname && strcmp(lrec->bbsvcname, service)) lrec++;
+	return (lrec->bbsvcname ? lrec : NULL);
+}
+
 /*
  * This program is invoked via CGI with QUERY_STRING containing:
  *
@@ -22,14 +71,14 @@ link_t  null_link = { "", "", "", NULL };
 
 char *hostname = "";
 char *service = "";
+char *ip = "";
+char *displayname = "";
 
 char *reqenv[] = {
 	"BBDISP",
-	"BBSKIN",
-	"BBWEB",
-	"MKBBROWFONT",
-	"MKBBCOLFONT",
-	NULL };
+	"BBHOME",
+	NULL 
+};
 
 
 static void errormsg(char *msg)
@@ -66,11 +115,19 @@ static void parse_query(void)
 			hostname = malcop(val);
 			while ((p = strchr(hostname, ','))) *p = '.';
 		}
+		else if (argnmatch(token, "IP")) {
+			ip = malcop(val);
+		}
+		else if (argnmatch(token, "DISPLAYNAME")) {
+			displayname = malcop(val);
+		}
 
 		token = strtok(NULL, "&");
 	}
 
         free(query);
+
+	if (strcmp(displayname, "") == 0) displayname = hostname;
 }
 
 
@@ -82,11 +139,30 @@ int main(int argc, char *argv[])
 	char *msg;
 	char *sumline, *firstline, *restofmsg, *p;
 	char *items[20];
-	int icount;
+	int icount, linecount;
 	int color;
 	time_t logage;
 	char timesincechange[100];
 	char *sender;
+	char *flags;
+	larrdsvc_t *larrd = NULL;
+
+	char *cgibinurl, *colfont, *rowfont;
+
+	getenv_default("LARRDS", "cpu=la,content=http,http,conn,fping=conn,ftp,ssh,telnet,nntp,pop,pop-2,pop-3,pop2,pop3,smtp,imap,disk,vmstat,memory,iostat,netstat,citrix,bbgen,bbtest,bbproxy,time=ntpstat,vmio,temperature", NULL);
+	getenv_default("NONHISTS", "info,larrd,trends,graphs", NULL);
+	getenv_default("BBREL", "bbgen", NULL);
+	getenv_default("BBRELDATE", VERSION, NULL);
+	getenv_default("CGIBINURL", "/cgi-bin", &cgibinurl);
+	getenv_default("MKBBCOLFONT", "COLOR=teal SIZE=-1\"", &colfont);
+	getenv_default("MKBBROWFONT", "SIZE=+1 COLOR=\"#FFFFCC\" FACE=\"Tahoma, Arial, Helvetica\"", &rowfont);
+	getenv_default("BBWEB", "/bb", NULL);
+	{
+		char *dbuf = malloc(strlen(getenv("BBWEB")) + 6);
+		sprintf(dbuf, "%s/gifs", getenv("BBWEB"));
+		getenv_default("BBSKIN", dbuf, NULL);
+		free(dbuf);
+	}
 
 	envcheck(reqenv);
 	parse_query();
@@ -118,13 +194,27 @@ int main(int argc, char *argv[])
 	}
 
 	color = parse_color(items[2]);
+	flags = items[3];
 	logage = time(NULL) - atoi(items[4]);
 	timesincechange[0] = '\0'; p = timesincechange;
 	if (logage > 86400) p += sprintf(p, "%d days,", (int) (logage / 86400));
 	p += sprintf(p, "%d hours, %d minutes", (int) ((logage % 86400) / 3600), (int) ((logage % 3600) / 60));
 	sender = items[6];
 
-	sethostenv(hostname, "", service, colorname(color));
+	/* Count how many lines are in the status message. This is needed by LARRD later */
+	linecount = 0; p = restofmsg;
+	do {
+		/* First skip all whitespace and blank lines */
+		while ((*p) && (isspace((int)*p) || iscntrl((int)*p))) p++;
+		if (*p) {
+			/* We found something that is not blank, so one more line */
+			linecount++;
+			/* Then skip forward to the EOLN */
+			p = strchr(p, '\n');
+		}
+	} while (p && (*p));
+
+	sethostenv(displayname, ip, service, colorname(color));
 	fprintf(stdout, "Content-type: text/html\n\n");
 
 	headfoot(stdout, "hostsvc", "", "header", color);
@@ -132,24 +222,66 @@ int main(int argc, char *argv[])
 	fprintf(stdout, "<br><br><a name=\"begindata\">&nbsp;</a>\n");
 
 	fprintf(stdout, "<CENTER><TABLE ALIGN=CENTER BORDER=0>\n");
-	fprintf(stdout, "<TR><TH><FONT %s>%s - %s</FONT><BR><HR WIDTH=\"60%%\"></TH></TR>\n", 
-		getenv("MKBBROWFONT"), hostname, service);
+	fprintf(stdout, "<TR><TH><FONT %s>%s - %s</FONT><BR><HR WIDTH=\"60%%\"></TH></TR>\n", rowfont, displayname, service);
 	fprintf(stdout, "<TR><TD><H3>%s</H3>\n", firstline);
 	fprintf(stdout, "<PRE>\n%s\n</PRE>\n", restofmsg);
 	fprintf(stdout, "</TD></TR></TABLE>\n");
 
 	fprintf(stdout, "<br><br>\n");
 	fprintf(stdout, "<table align=\"center\" border=0>\n");
-	fprintf(stdout, "<tr><td align=\"center\"><font %s>", getenv("MKBBCOLFONT"));
+	fprintf(stdout, "<tr><td align=\"center\"><font %s>", colfont);
 	fprintf(stdout, "Status unchanged in %s<br>\n", timesincechange);
 	fprintf(stdout, "Status message received from %s\n", sender);
 	fprintf(stdout, "</font></td></tr>\n");
 	fprintf(stdout, "</table>\n");
 
 	/* larrd stuff here */
+	larrd = find_larrd(service, flags);
+	if (larrd) {
+		/* 
+		 * If this service uses part-names (currently, only disk does),
+		 * then setup a link for each of the part graphs.
+		 */
+		if (larrd->larrdpartname) {
+			int start;
+
+			fprintf(stdout, "<!-- linecount=%d -->\n", linecount);
+			for (start=0; (start < linecount); start += 6) {
+				fprintf(stdout,"<BR><BR><CENTER><A HREF=\"%s/larrd-grapher.cgi?host=%s&amp;service=%s&%s=%d..%d&amp;disp=%s\"><IMG SRC=\"%s/larrd-grapher.cgi?host=%s&amp;service=%s&amp;%s=%d..%d&amp;graph=hourly&ampdisp=%s\" ALT=\"&nbsp;\" BORDER=0></A><BR></CENTER>\n",
+					cgibinurl, hostname, larrd->larrdsvcname, larrd->larrdpartname,
+					start, (((start+5) < linecount) ? start+5 : linecount-1), displayname,
+					cgibinurl, hostname, larrd->larrdsvcname, larrd->larrdpartname,
+					start, (((start+5) < linecount) ? start+5 : linecount-1), displayname);
+			}
+		}
+		else {
+				fprintf(stdout,"<BR><BR><CENTER><A HREF=\"%s/larrd-grapher.cgi?host=%s&amp;service=%s&amp;disp=%s\"><IMG SRC=\"%s/larrd-grapher.cgi?host=%s&amp;service=%s&amp;disp=%s&amp;graph=hourly\"ALT=\"&nbsp;\" BORDER=0></A><BR></CENTER>\n",
+					cgibinurl, hostname, larrd->larrdsvcname, displayname,
+					cgibinurl, hostname, larrd->larrdsvcname, displayname);
+		}
+	}
+
+	{
+		char *tmp1 = (char *)malloc(strlen(getenv("NONHISTS"))+3);
+		char *tmp2 = (char *)malloc(strlen(service)+3);
+
+		sprintf(tmp1, ",%s,", getenv("NONHISTS"));
+		sprintf(tmp2, ",%s,", service);
+		if (strstr(tmp1, tmp2) == NULL) {
+			fprintf(stdout,"<BR><BR><CENTER><FORM ACTION=\"%s/bb-hist.sh\"> \
+				<INPUT TYPE=SUBMIT VALUE=\"HISTORY\"> \
+				<INPUT TYPE=HIDDEN NAME=\"HISTFILE\" VALUE=\"%s.%s\"> \
+				<INPUT TYPE=HIDDEN NAME=\"ENTRIES\" VALUE=\"50\"> \
+				<INPUT TYPE=HIDDEN NAME=\"IP\" VALUE=\"%s\"> \
+				</FORM></CENTER>\n",
+				cgibinurl, hostname, service, ip);
+		}
+
+		free(tmp2);
+		free(tmp1);
+	}
 
 	headfoot(stdout, "hostsvc", "", "footer", color);
 	return 0;
 }
-#endif
 
