052e2295dc5f13896854a37973fd91652c66a090
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-09 20:42:55 +0000

    New nlencode/nldecode routines
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1513 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd_worker.h
+++ trunk/hobbitd/hobbitd_worker.h
@@ -4,9 +4,12 @@
 #include "bbgen.h"
 #include "util.h"
 #include "debug.h"
+#include "bbd_net.h"
+#include "bbdutil.h"
 
 extern unsigned char *get_bbgend_message(void);
-
+extern unsigned char *nlencode(unsigned char *msg);
+extern void nldecode(unsigned char *msg);
 
 #endif
 
