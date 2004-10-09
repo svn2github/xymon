73f860fba26842794cc65310dc634a073935dbf1
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-09 20:41:26 +0000

    Define the memory size of the shared memory region
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1510 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd_ipc.h
+++ trunk/hobbitd/hobbitd_ipc.h
@@ -1,5 +1,11 @@
+#ifndef __BBDUTIL_H__
+#define __BBDUTIL_H__
+
+#define SHAREDBUFSZ (3*MAXMSG+4096)
+
 extern char *channelnames[];
 
 extern bbd_channel_t *setup_channel(enum msgchannels_t chnname, int flags);
 extern void close_channel(bbd_channel_t *chn, int flags);
+#endif
 
