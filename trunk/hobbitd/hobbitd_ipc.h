d17f79e3c312221a7e4c958ec823c9232df0363a
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-11 11:43:38 +0000

    Extra semaphore, CLIENTCOUNT. Define roles for setup_channel/close_channel
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1531 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd_ipc.h
+++ trunk/hobbitd/hobbitd_ipc.h
@@ -4,12 +4,16 @@
 #define SHAREDBUFSZ (3*MAXMSG+4096)
 
 /* Semaphore numbers */
-#define BOARDBUSY 0
-#define GOCLIENT 1
+#define BOARDBUSY   0
+#define GOCLIENT    1
+#define CLIENTCOUNT 2
+
+#define CHAN_MASTER 0
+#define CHAN_CLIENT 1
 
 extern char *channelnames[];
 
-extern bbd_channel_t *setup_channel(enum msgchannels_t chnname, int flags);
-extern void close_channel(bbd_channel_t *chn, int flags);
+extern bbd_channel_t *setup_channel(enum msgchannels_t chnname, int role);
+extern void close_channel(bbd_channel_t *chn, int role);
 #endif
 
