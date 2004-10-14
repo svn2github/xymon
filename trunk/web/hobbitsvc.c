19d21a35988b478de24f78879ceb48ce7db5f7c1
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-14 14:02:58 +0000

    Support for bb-histlog functionality.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1558 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/web/hobbitsvc.c
+++ trunk/web/hobbitsvc.c
@@ -1,3 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Big Brother webpage generator tool.                                        */
+/*                                                                            */
+/* This is a replacement for the "bb-hostsvc.sh" script                       */
+/*                                                                            */
+/* Copyright (C) 2004 Henrik Storner <henrik@storner.dk>                      */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: hobbitsvc.c,v 1.5 2004-10-14 14:02:58 henrik Exp $";
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -76,6 +90,7 @@ char *hostname = "";
 char *service = "";
 char *ip = "";
 char *displayname = "";
+char *tstamp = "";
 
 char *reqenv[] = {
 	"BBDISP",
@@ -124,6 +139,15 @@ static void parse_query(void)
 		else if (argnmatch(token, "DISPLAYNAME")) {
 			displayname = malcop(val);
 		}
+		else if (argnmatch(token, "HOST")) {
+			hostname = malcop(val);
+		}
+		else if (argnmatch(token, "SERVICE")) {
+			service = malcop(val);
+		}
+		else if (argnmatch(token, "TIMEBUF")) {
+			tstamp = malcop(val);
+		}
 
 		token = strtok(NULL, "&");
 	}
@@ -134,24 +158,24 @@ static void parse_query(void)
 }
 
 
+enum source_t { SRC_BBLOGS, SRC_BBGEND, SRC_HISTLOGS };
+
 int main(int argc, char *argv[])
 {
-	int use_bbgend = 0;
 	char bbgendreq[200];
 	char *log = NULL;
 	int bbgendresult;
 	char *msg;
 	char *sumline, *firstline, *restofmsg, *p;
 	char *items[20];
-	int icount, linecount;
+	int argi, icount, linecount;
 	int color;
-	time_t logage;
 	char timesincechange[100];
 	char *sender;
 	char *flags;
 	larrdsvc_t *larrd = NULL;
-
 	char *cgibinurl, *colfont, *rowfont;
+	enum source_t source = SRC_BBLOGS;
 
 	getenv_default("LARRDS", "cpu=la,content=http,http,conn,fping=conn,ftp,ssh,telnet,nntp,pop,pop-2,pop-3,pop2,pop3,smtp,imap,disk,vmstat,memory,iostat,netstat,citrix,bbgen,bbtest,bbproxy,time=ntpstat,vmio,temperature", NULL);
 	getenv_default("NONHISTS", "info,larrd,trends,graphs", NULL);
@@ -169,13 +193,24 @@ int main(int argc, char *argv[])
 	}
 	{
 		getenv_default("USEBBGEND", "FALSE", NULL);
-		if (strcmp(getenv("USEBBGEND"), "TRUE") == 0) use_bbgend = 1;
+		if (strcmp(getenv("USEBBGEND"), "TRUE") == 0) source = SRC_BBGEND;
+	}
+
+	for (argi = 1; (argi < argc); argi++) {
+		if (strcmp(argv[argi], "--historical") == 0) {
+			source = SRC_HISTLOGS;
+		}
+		else if (strcmp(argv[argi], "--bbgend") == 0) {
+			source = SRC_BBGEND;
+		}
 	}
 
 	envcheck(reqenv);
 	parse_query();
 
-	if (use_bbgend) {
+	if (source == SRC_BBGEND) {
+		time_t logage;
+
 		sprintf(bbgendreq, "bbgendlog %s.%s", hostname, service);
 		bbgendresult = sendmessage(bbgendreq, NULL, NULL, &log, 1);
 		if ((bbgendresult != BB_OK) || (log == NULL) || (strlen(log) == 0)) {
@@ -205,6 +240,9 @@ int main(int argc, char *argv[])
 		color = parse_color(items[2]);
 		flags = items[3];
 		logage = time(NULL) - atoi(items[4]);
+		timesincechange[0] = '\0'; p = timesincechange;
+		if (logage > 86400) p += sprintf(p, "%d days,", (int) (logage / 86400));
+		p += sprintf(p, "%d hours, %d minutes", (int) ((logage % 86400) / 3600), (int) ((logage % 3600) / 60));
 		sender = items[6];
 	}
 	else {
@@ -216,7 +254,17 @@ int main(int argc, char *argv[])
 		char *p, *unchangedstr, *receivedfromstr;
 		int n;
 
-		sprintf(logfn, "%s/%s.%s", getenv("BBLOGS"), commafy(hostname), service);
+		if (source == SRC_BBLOGS) {
+			sprintf(logfn, "%s/%s.%s", getenv("BBLOGS"), commafy(hostname), service);
+		}
+		else if (source == SRC_HISTLOGS) {
+			p = hostname; while ((p = strchr(p, '.')) != NULL) *p = '_';
+			p = hostname; while ((p = strchr(p, ',')) != NULL) *p = '_';
+			sprintf(logfn, "%s/%s/%s/%s", getenv("BBHISTLOGS"), hostname, service, tstamp);
+			p = tstamp; while ((p = strchr(p, '_')) != NULL) *p = ' ';
+			sethostenv_histlog(tstamp);
+		}
+
 		if (stat(logfn, &st) == -1) {
 			errormsg("No such host/service\n");
 			return 1;
@@ -250,15 +298,13 @@ int main(int argc, char *argv[])
 			flags = "";
 		}
 
-		sprintf(logfn, "%s/%s.%s", getenv("BBHIST"), commafy(hostname), service);
-		if (stat(logfn, &st) == 0) {
-			logage = time(NULL) - st.st_mtime;
-		}
-		else {
-			logage = 0;
-		}
-
+		timesincechange[0] = '\0';
 		unchangedstr = strstr(restofmsg, statusunchangedtext);
+		if (p) {
+			p += strlen(statusunchangedtext);
+			n = strcspn(p, "\n");
+			strncpy(timesincechange, p, n);
+		}
 
 		p = receivedfromstr = strstr(restofmsg, receivedfromtext); 
 		if (p) {
@@ -277,12 +323,6 @@ int main(int argc, char *argv[])
 		if (receivedfromstr) *receivedfromstr = '\0';
 	}
 
-	if (logage) {
-		timesincechange[0] = '\0'; p = timesincechange;
-		if (logage > 86400) p += sprintf(p, "%d days,", (int) (logage / 86400));
-		p += sprintf(p, "%d hours, %d minutes", (int) ((logage % 86400) / 3600), (int) ((logage % 3600) / 60));
-	}
-
 	/* Count how many lines are in the status message. This is needed by LARRD later */
 	linecount = 0; p = restofmsg;
 	do {
@@ -299,7 +339,7 @@ int main(int argc, char *argv[])
 	sethostenv(displayname, ip, service, colorname(color));
 	fprintf(stdout, "Content-type: text/html\n\n");
 
-	headfoot(stdout, "hostsvc", "", "header", color);
+	headfoot(stdout, ((source == SRC_HISTLOGS) ? "histlog" : "hostsvc"), "", "header", color);
 
 	fprintf(stdout, "<br><br><a name=\"begindata\">&nbsp;</a>\n");
 
@@ -342,7 +382,7 @@ int main(int argc, char *argv[])
 	fprintf(stdout, "<br><br>\n");
 	fprintf(stdout, "<table align=\"center\" border=0>\n");
 	fprintf(stdout, "<tr><td align=\"center\"><font %s>", colfont);
-	if (timesincechange) fprintf(stdout, "Status unchanged in %s<br>\n", timesincechange);
+	if (strlen(timesincechange)) fprintf(stdout, "Status unchanged in %s<br>\n", timesincechange);
 	if (sender) fprintf(stdout, "Status message received from %s\n", sender);
 	fprintf(stdout, "</font></td></tr>\n");
 	fprintf(stdout, "</table>\n");
@@ -393,7 +433,7 @@ int main(int argc, char *argv[])
 		free(tmp1);
 	}
 
-	headfoot(stdout, "hostsvc", "", "footer", color);
+	headfoot(stdout, ((source == SRC_HISTLOGS) ? "histlog" : "hostsvc"), "", "footer", color);
 	return 0;
 }
 
