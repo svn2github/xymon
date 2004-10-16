04bdeaba926b528d18611be28570e06c4967f651
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-10-16 12:36:45 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1564 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/hobbitd/do_alert.h
@@ -0,0 +1,29 @@
+#ifndef __BBD_ALERT_H__
+#define __BBD_ALERT_H__
+
+#include <sys/time.h>
+
+typedef struct htnames_t {
+	char *name;
+	struct htnames_t *next;
+} htnames_t;
+
+enum astate_t { A_PAGING, A_ACKED, A_RECOVERED, A_DEAD };
+
+typedef struct activealerts_t {
+	/* Identification of the alert */
+	htnames_t *hostname;
+	htnames_t *testname;
+
+	/* Alert status */
+	int color;
+	char *pagemessage;
+	char *ackmessage;
+	time_t eventstart;
+	time_t nextalerttime;
+	enum astate_t state;
+
+	struct activealerts_t *next;
+} activealerts_t;
+#endif
+
