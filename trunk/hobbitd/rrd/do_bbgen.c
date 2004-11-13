8a53671082cf0979e0697a0c1cb385c94ea9fee7
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-11-13 22:06:24 +0000

    If testname is the same as standard prefix, drop testname from rrd filename
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1807 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/rrd/do_bbgen.c
+++ trunk/hobbitd/rrd/do_bbgen.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char bbgen_rcsid[] = "$Id: do_bbgen.c,v 1.5 2004-11-13 17:37:36 henrik Exp $";
+static char bbgen_rcsid[] = "$Id: do_bbgen.c,v 1.6 2004-11-13 22:06:24 henrik Exp $";
 
 static char *bbgen_params[] = { "rrdcreate", rrdfn, "DS:runtime:GAUGE:600:0:U", rra1, rra2, rra3, rra4, NULL };
 
@@ -19,7 +19,12 @@ int do_bbgen_larrd(char *hostname, char *testname, char *msg, time_t tstamp)
 
 	p = strstr(msg, "TIME TOTAL");
 	if (p && (sscanf(p, "TIME TOTAL %f", &runtime) == 1)) {
-		sprintf(rrdfn, "bbgen.%s.rrd", testname);
+		if (strcmp("bbgen", testname) != 0) {
+			sprintf(rrdfn, "bbgen.%s.rrd", testname);
+		}
+		else {
+			strcpy(rrdfn, "bbgen.rrd");
+		}
 		sprintf(rrdvalues, "%d:%.2f", (int)tstamp, runtime);
 		return create_and_update_rrd(hostname, rrdfn, bbgen_params, update_params);
 	}
--- trunk/hobbitd/rrd/do_bbproxy.c
+++ trunk/hobbitd/rrd/do_bbproxy.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char bbproxy_rcsid[] = "$Id: do_bbproxy.c,v 1.5 2004-11-13 17:37:36 henrik Exp $";
+static char bbproxy_rcsid[] = "$Id: do_bbproxy.c,v 1.6 2004-11-13 22:06:24 henrik Exp $";
 
 static char *bbproxy_params[]       = { "rrdcreate", rrdfn, "DS:runtime:GAUGE:600:0:U", rra1, rra2, rra3, rra4, NULL };
 
@@ -19,7 +19,12 @@ int do_bbproxy_larrd(char *hostname, char *testname, char *msg, time_t tstamp)
 
 	p = strstr(msg, "Average queue time");
 	if (p && (sscanf(p, "Average queue time : %f", &runtime) == 1)) {
-		sprintf(rrdfn, "bbproxy.%s.rrd", testname);
+		if (strcmp("bbproxy", testname) != 0) {
+			sprintf(rrdfn, "bbproxy.%s.rrd", testname);
+		}
+		else {
+			strcpy(rrdfn, "bbproxy.rrd");
+		}
 		sprintf(rrdvalues, "%d:%.2f", (int) tstamp, runtime);
 		return create_and_update_rrd(hostname, rrdfn, bbproxy_params, update_params);
 	}
