9f1822551875a9bb454a7c31f5e6a7371bf1acbd
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-11-14 16:57:16 +0000

    New include_configid global var.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@1818 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/do_alert.h
+++ trunk/hobbitd/do_alert.h
@@ -33,6 +33,8 @@ typedef struct activealerts_t {
 	struct activealerts_t *next;
 } activealerts_t;
 
+extern int include_configid;
+
 extern void load_alertconfig(char *configfn, int alertcolors);
 extern void dump_alertconfig(void);
 extern time_t next_alert(activealerts_t *alert);
