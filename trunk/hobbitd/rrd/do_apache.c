6a5035991e6575acbb3933ec2db9e8a3c19aa6ee
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-12-11 15:49:46 +0000

    Handle Apache data from bbtest-net
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2053 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/rrd/do_apache.c
+++ trunk/hobbitd/rrd/do_apache.c
@@ -8,11 +8,45 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char apache_rcsid[] = "$Id: do_apache.c,v 1.1 2004-11-07 18:24:24 henrik Exp $";
+static char apache_rcsid[] = "$Id: do_apache.c,v 1.2 2004-12-11 15:49:46 henrik Exp $";
+
+static char *apache_params[] = { "rrdcreate", rrdfn, 
+				 "DS:TA:DERIVE:600:0:U",
+				 "DS:TKB:DERIVE:600:0:U",
+				 "DS:BW:GAUGE:600:1:U",
+				 "DS:IW:GAUGE:600:1:U",
+				 "DS:CPU:GAUGE:600:0:U",
+				 "DS:REQPERSEC:GAUGE:600:0:U",
+				 rra1, rra2, rra3, rra4, NULL };
 
 int do_apache_larrd(char *hostname, char *testname, char *msg, time_t tstamp)
 {
-	errprintf("Apache larrd not implemented\n");
-	return -1;
+	char *vals[] = { "U", "U", "U", "U", "U", "U", NULL };
+	char *markers[] = { "Total Accesses:", "Total kBytes:", 
+			    "BusyWorkers:", "IdleWorkers:", "CPULoad:", "ReqPerSec:", NULL };
+	int i;
+	char *p, *eoln, *val;
+
+	strcpy(rrdfn, "apache.rrd");
+	sprintf(rrdvalues, "%d", (int)tstamp);
+	i = 0;
+	while (markers[i]) {
+		strcat(rrdvalues, ":"); 
+		p = strstr(msg, markers[i]);
+		if (p) {
+			eoln = strchr(p, '\n');
+			if (eoln) *eoln = '\0';
+			p = strchr(p, ':')+1;
+			p += strspn(p, " ");
+			strcat(rrdvalues, p);
+			if (eoln) *eoln = '\n';
+		}
+		else {
+			strcat(rrdvalues, "U");
+		}
+		i++;
+	}
+
+	return create_and_update_rrd(hostname, rrdfn, apache_params, update_params);
 }
 
