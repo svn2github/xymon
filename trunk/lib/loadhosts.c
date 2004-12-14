472e88fb904986c11637cd63822461ef92b49471
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2004-12-14 23:08:26 +0000

    Add a couple more BBH items. Fix broken prefer handling. Save page titles as well as their names, using the full path. Make tag-matching case insensitive.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2095 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/loadhosts.c
+++ trunk/lib/loadhosts.c
@@ -12,7 +12,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: loadhosts.c,v 1.11 2004-12-13 23:07:49 henrik Exp $";
+static char rcsid[] = "$Id: loadhosts.c,v 1.12 2004-12-14 23:08:26 henrik Exp $";
 
 #include <stdio.h>
 #include <string.h>
@@ -26,8 +26,8 @@ static char rcsid[] = "$Id: loadhosts.c,v 1.11 2004-12-13 23:07:49 henrik Exp $"
 
 static pagelist_t *pghead = NULL;
 static namelist_t *namehead = NULL;
-
 static const char *bbh_item_key[BBH_LAST];
+static char *documentation_url = NULL;
 
 static void bbh_item_list_setup(void)
 {
@@ -80,13 +80,13 @@ static void bbh_item_list_setup(void)
 
 static int pagematch(pagelist_t *pg, char *name)
 {
-	char *p = strrchr(pg->pagename, '/');
+	char *p = strrchr(pg->pagepath, '/');
 
 	if (p) {
 		return (strcmp(p+1, name) == 0);
 	}
 	else {
-		return (strcmp(pg->pagename, name) == 0);
+		return (strcmp(pg->pagepath, name) == 0);
 	}
 }
 
@@ -96,17 +96,38 @@ static char *bbh_find_item(namelist_t *host, enum bbh_item_t item)
 
 	bbh_item_list_setup();
 	i = 0;
-	while (host->elems[i] && strncmp(host->elems[i], bbh_item_key[item], strlen(bbh_item_key[item]))) i++;
+	while (host->elems[i] && strncasecmp(host->elems[i], bbh_item_key[item], strlen(bbh_item_key[item]))) i++;
 	return (host->elems[i] ? (host->elems[i] + strlen(bbh_item_key[item])) : NULL);
 }
 
-namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
+static int get_page_name_title(char *buf, char *key, char **name, char **title)
+{
+	*name = *title = NULL;
+
+	*name = buf + strlen(key); *name += strspn(*name, " \t\r\n");
+	if (strlen(*name) > 0) {
+		/* (*name) now points at the start of the name. Find end of name */
+		*title = *name;
+		*title += strcspn(*title, " \t\r\n");
+		/* Null-terminate the name */
+		**title = '\0'; (*title)++;
+
+		*title += strspn(*title, " \t\r\n");
+		return 0;
+	}
+
+	return 1;
+}
+
+namelist_t *load_hostnames(char *bbhostsfn, int fqdn, char *docurl)
 {
 	FILE *bbhosts;
 	int ip1, ip2, ip3, ip4;
 	char hostname[4096];
 	char l[4096];
 	pagelist_t *curtoppage, *curpage;
+
+	if (documentation_url) free(documentation_url); documentation_url = NULL;
 
 	while (namehead) {
 		namelist_t *walk = namehead;
@@ -124,26 +145,34 @@ namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
 		pagelist_t *walk = pghead;
 
 		pghead = pghead->next;
-		if (walk->pagename) free(walk->pagename);
+		if (walk->pagepath) free(walk->pagepath);
+		if (walk->pagetitle) free(walk->pagetitle);
 		free(walk);
 	}
 
+	if (docurl) documentation_url = strdup(docurl);
+
 	/* Setup the top-level page */
 	pghead = (pagelist_t *) malloc(sizeof(pagelist_t));
-	pghead->pagename = strdup("");
+	pghead->pagepath = strdup("");
+	pghead->pagetitle = strdup("");
 	pghead->next = NULL;
 	curpage = curtoppage = pghead;
 
 	bbhosts = stackfopen(bbhostsfn, "r");
 	while (stackfgets(l, sizeof(l), "include", NULL)) {
+		char *eoln;
+		
+		eoln = strchr(l, '\n'); if (eoln) *eoln = '\0';
+
 		if (strncmp(l, "page ", 5) == 0) {
 			pagelist_t *newp;
-			char *tok;
+			char *name, *title;
 
-			tok = strtok(l+5, " \n\t\r");
-			if (tok) {
+			if (get_page_name_title(l, "page", &name, &title) == 0) {
 				newp = (pagelist_t *)malloc(sizeof(pagelist_t));
-				newp->pagename = strdup(tok);
+				newp->pagepath = strdup(name);
+				newp->pagetitle = (title ? strdup(title) : NULL);
 				newp->next = pghead;
 				curtoppage = pghead = newp;
 				curpage = newp;
@@ -151,13 +180,14 @@ namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
 		}
 		else if (strncmp(l, "subpage ", 8) == 0) {
 			pagelist_t *newp;
-			char *tok;
+			char *name, *title;
 
-			tok = strtok(l+8, " \n\t\r");
-			if (tok) {
+			if (get_page_name_title(l, "subpage", &name, &title) == 0) {
 				newp = (pagelist_t *)malloc(sizeof(pagelist_t));
-				newp->pagename = malloc(strlen(curtoppage->pagename) + strlen(tok) + 2);
-				sprintf(newp->pagename, "%s/%s", curtoppage->pagename, tok);
+				newp->pagepath = malloc(strlen(curtoppage->pagepath) + strlen(name) + 2);
+				sprintf(newp->pagepath, "%s/%s", curtoppage->pagepath, name);
+				newp->pagetitle = malloc(strlen(curtoppage->pagetitle) + strlen(title) + 2);
+				sprintf(newp->pagetitle, "%s/%s", curtoppage->pagetitle, title);
 				newp->next = pghead;
 				pghead = newp;
 				curpage = newp;
@@ -165,19 +195,19 @@ namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
 		}
 		else if (strncmp(l, "subparent ", 10) == 0) {
 			pagelist_t *newp, *parent;
-			char *partok, *tok;
+			char *pname, *name, *title;
 
-			parent = NULL; tok = NULL;
-
-			partok = strtok(l+10, " \n\t\r");
-			if (partok) {
-				tok = strtok(NULL, " \n\t\r");
-				for (parent = pghead; (parent && !pagematch(parent, partok)); parent = parent->next);
+			parent = NULL;
+			if (get_page_name_title(l, "subparent", &pname, &title) == 0) {
+				for (parent = pghead; (parent && !pagematch(parent, pname)); parent = parent->next);
 			}
-			if (parent) {
+
+			if (parent && (get_page_name_title(title, "", &name, &title) == 0)) {
 				newp = (pagelist_t *)malloc(sizeof(pagelist_t));
-				newp->pagename = malloc(strlen(parent->pagename) + strlen(tok) + 2);
-				sprintf(newp->pagename, "%s/%s", parent->pagename, tok);
+				newp->pagepath = malloc(strlen(parent->pagepath) + strlen(name) + 2);
+				sprintf(newp->pagepath, "%s/%s", parent->pagepath, name);
+				newp->pagetitle = malloc(strlen(parent->pagetitle) + strlen(title) + 2);
+				sprintf(newp->pagetitle, "%s/%s", parent->pagetitle, title);
 				newp->next = pghead;
 				pghead = newp;
 				curpage = newp;
@@ -190,7 +220,7 @@ namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
 			char downtime[4096];
 
 			namelist_t *newitem = malloc(sizeof(namelist_t));
-			namelist_t *iwalk;
+			namelist_t *iwalk, *iprev;
 
 			if (!fqdn) {
 				/* Strip any domain from the hostname */
@@ -265,16 +295,27 @@ namelist_t *load_hostnames(char *bbhostsfn, int fqdn)
 			newitem->downtime = bbh_find_item(newitem, BBH_DOWNTIME);
 
 			/* See if this host is defined before */
-			for (iwalk = namehead; (iwalk && strcmp(iwalk->bbhostname, newitem->bbhostname)); iwalk = iwalk->next) ;
-			if ((iwalk == NULL) || (newitem->preference > iwalk->preference)) {
-				/* New item, or one with higher preference, so add to beginning of list */
+			for (iwalk = namehead, iprev = NULL; (iwalk && strcmp(iwalk->bbhostname, newitem->bbhostname)); iprev = iwalk, iwalk = iwalk->next) ;
+			if (iwalk == NULL) {
+				/* New item, so add to beginning of list */
 				newitem->next = namehead;
 				namehead = newitem;
 			}
-			else {
+ 			else if (newitem->preference <= iwalk->preference) {
 				/* Add after the existing (more preferred) entry */
 				newitem->next = iwalk->next;
 				iwalk->next = newitem;
+			}
+			else {
+				/* New item has higher preference, so add before the iwalk item (i.e. after iprev) */
+				if (iprev == NULL) {
+					newitem->next = namehead;
+					namehead = newitem;
+				}
+				else {
+					newitem->next = iprev->next;
+					iprev->next = newitem;
+				}
 			}
 		}
 	}
@@ -331,6 +372,9 @@ namelist_t *hostinfo(char *hostname)
 
 char *bbh_item(namelist_t *host, enum bbh_item_t item)
 {
+	static char *result;
+	char *p;
+
 	switch (item) {
 	  case BBH_CLIENTALIAS: 
 		  return host->clientname;
@@ -348,7 +392,30 @@ char *bbh_item(namelist_t *host, enum bbh_item_t item)
 		  return host->bbhostname;
 
 	  case BBH_PAGENAME:
-		  return host->page->pagename;
+		  p = strrchr(host->page->pagepath, '/');
+		  if (p) return (p+1); else return host->page->pagepath;
+
+	  case BBH_PAGEPATH:
+		  return host->page->pagepath;
+
+	  case BBH_PAGETITLE:
+		  p = strrchr(host->page->pagetitle, '/');
+		  if (p) return (p+1);
+		  /* else: Fall through */
+
+	  case BBH_PAGEPATHTITLE:
+		  if (strlen(host->page->pagetitle)) return host->page->pagetitle;
+		  return "Top Page";
+
+	  case BBH_DOCURL:
+		  if (documentation_url) {
+			if (result) free(result);
+			result = (char *)malloc(strlen(documentation_url) + strlen(host->bbhostname) + 1);
+			sprintf(result, documentation_url, host->bbhostname);
+		  	return result;
+		  }
+		  else
+			return NULL;
 
 	  default:
 		  return bbh_find_item(host, item);
@@ -401,7 +468,7 @@ int main(int argc, char *argv[])
 	namelist_t *hosts, *h;
 	char *val;
 
-	hosts = load_hostnames(argv[1], 1);
+	hosts = load_hostnames(argv[1], 1, NULL);
 
 	for (argi = 2; (argi < argc); argi++) {
 		h = hostinfo(argv[argi]);
