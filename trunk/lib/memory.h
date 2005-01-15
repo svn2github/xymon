43bc72a245f9cb159774c8ffc16725855d8642ee
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-01-15 22:13:08 +0000

    Added copyright notice
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2330 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/memory.h
+++ trunk/lib/memory.h
@@ -1,3 +1,15 @@
+/*----------------------------------------------------------------------------*/
+/* bbgen toolkit                                                              */
+/*                                                                            */
+/* Copyright (C) 2002-2004 Henrik Storner <henrik@storner.dk>                 */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
 
 extern void *xcalloc(size_t nmemb, size_t size);
 extern void *xmalloc(size_t size);
@@ -6,3 +18,6 @@ extern char *xstrdup(const char *s);
 extern const char *xfreenullstr;
 
 #define xfree(P) { if ((P) == NULL) { errprintf(xfreenullstr); abort(); } free((P)); (P) = NULL; }
+
+#endif
+
