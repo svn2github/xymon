434a770307a5c9ed5e1c1cf85b43353f8eb2f3ff
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-01-20 10:37:36 +0000

    Enable memory debugging by a pre-processor #define
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2377 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/memory.h
+++ trunk/lib/memory.h
@@ -11,11 +11,15 @@
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
+#undef MEMORY_DEBUG
+
 typedef struct xmemory_t {
 	char *sdata;
 	size_t ssize;
 	struct xmemory_t *next;
 } xmemory_t;
+
+extern const char *xfreenullstr;
 
 extern void add_to_memlist(void *ptr, size_t memsize);
 extern void remove_from_memlist(void *ptr);
@@ -34,17 +38,51 @@ extern int   xsprintf(char *dest, const char *fmt, ...);
 
 // strlen|strcmp|strncmp|strcasecmp|strncasecmp|strstr|strchr|strrchr|strspn|strcspn|strtok|strftime|strerror
 
-#define calloc(N,S) xcalloc((N), (S))
-#define malloc(N) xmalloc((N))
+
+/*
+ * This arranges for all memory-allocation routines to
+ * go via a wrapper that checks for bogus input data
+ * and malloc() returning NULL when running out of memory.
+ * Errors caught here are fatal.
+ * Overhead is small, so this is turned on always.
+ */
+#define calloc(N,S)  xcalloc((N), (S))
+#define malloc(N)    xmalloc((N))
 #define realloc(P,S) xrealloc((P), (S))
+#define strdup(P)    xstrdup((P))
+
+
+#ifdef MEMORY_DEBUG
+
+/*
+ * This arranges for all calls to potentially memory-overwriting routines
+ * to do strict allocation checks and overwrite checks. The performance
+ * overhead is significant, so it should only be turned on in debugging
+ * situations.
+ */
+
 #define xfree(P) { remove_from_memlist((P)); free((P)); (P) = NULL; }
 
-#define strdup(P) xstrdup((P))
 #define strcat(D,S) xstrcat((D), (S))
 #define strncat(D,S,L) xstrncat((D), (S), (L))
 
 #define strcpy(D,S) xstrcpy((D), (S))
 #define strncpy(D,S,L) xstrncpy((D), (S), (L))
 #define sprintf xsprintf
+
+#else
+
+/*
+ * This defines an "xfree()" macro, which checks for freeing of
+ * a NULL ptr and complains if that happens. It does not check if
+ * the pointer is valid.
+ * After being freed, the pointer is set to NULL to catch double-free's.
+ */
+
+#define xfree(P) { if ((P) == NULL) { errprintf(xfreenullstr); abort(); } free((P)); (P) = NULL; }
+
+#endif
+
+
 #endif
 
