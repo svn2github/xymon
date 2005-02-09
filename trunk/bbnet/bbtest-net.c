23830d5b784361369356445ed2ed89c1b51670b6
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-09 20:56:42 +0000

    Fix the init_testitem() testspec handling correctly, this time.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2535 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbnet/bbtest-net.c
+++ trunk/bbnet/bbtest-net.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: bbtest-net.c,v 1.195 2005-02-09 08:17:14 henrik Exp $";
+static char rcsid[] = "$Id: bbtest-net.c,v 1.196 2005-02-09 20:56:42 henrik Exp $";
 
 #include <limits.h>
 #include <stdio.h>
@@ -368,7 +368,7 @@ testitem_t *init_testitem(testedhost_t *host, service_t *service, char *testspec
 	newtest->alwaystrue = alwaystruetest;
 	newtest->silenttest = silenttest;
 	newtest->senddata = sendasdata;
-	newtest->testspec = strdup(testspec);
+	newtest->testspec = (testspec ? strdup(testspec) : NULL);
 	newtest->privdata = NULL;
 	newtest->open = 0;
 	newtest->banner = NULL;
