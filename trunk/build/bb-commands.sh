231d73d62e64645fc8c8985782ae2059e4fc2057
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-16 21:17:12 +0000

    Added setup for the BB client package
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2566 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/bb-commands.sh
+++ trunk/build/bb-commands.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# $Id: bb-commands.sh,v 1.2 2005-02-16 21:17:12 henrik Exp $
+
 # Script to pick up most of the commands used by BB extension scripts.
 # This is used during installation, to build a hobbitserver.cfg that
 # includes these commands so that extension scripts can run from
@@ -42,4 +44,54 @@ echo "DFCMD=${PGM} -Pk"
 # PS probably needs tweaking
 echo "# Adjust ps output to your liking"
 echo "PS=ps ax"
+
+echo ""
+echo "# Miscellaneous definitions used by the BB client"
+echo "MAXLINE=32768"
+
+echo ""
+echo "# For the cpu check"
+echo "DOCPU=TRUE"
+echo "NOCPUCOLOR=clear"
+echo "BBCPUTAB=\$BBHOME/etc/bb-cputab"
+echo "BBTOOMANYDAYSUP=9999"
+echo "CPUPANIC=300"
+echo "CPUWARN=150"
+echo "DISPREALLOADAVG=FALSE"
+echo "TOPARGS=-b -n 1"
+echo "WARNCOLORONREBOOT=yellow"
+echo "WARNMINSONREBOOT=60"
+
+echo ""
+echo "# For the disk check"
+echo "DODISK=TRUE"
+echo "NODISKCOLOR=clear"
+echo "BBDFTAB=\$BBHOME/etc/bb-dftab"
+echo "DFEXCLUDE=cdrom"
+echo "DFPANIC=95"
+echo "DFSORT=4"
+echo "DFUSE=^/dev"
+echo "DFWARN=90"
+
+echo ""
+echo "# For the msgs check"
+echo "DOMSGS=TRUE"
+echo "NOMSGSCOLOR=clear"
+echo "BBMSGSTAB=\$BBHOME/etc/bb-msgstab"
+echo "CHKMSGLEN=TRUE"
+echo "IGNMSGS="
+echo "MSGEXPIRE=30:60"
+echo "MSGFILE=/var/log/messages"
+echo "MSGS=NOTICE WARNING"
+echo "PAGEMSG=NOTICE"
+echo "REDMSGSLINES=20"
+echo "YELLOWMSGSLINES=10"
+
+echo ""
+echo "# For the procs check"
+echo "DOPROCS=TRUE"
+echo "NOPROCSCOLOR=clear"
+echo "BBPROCTAB=\$BBHOME/etc/bb-proctab"
+echo "PAGEPROC=cron"
+echo "PROCS=bbrun"
 
