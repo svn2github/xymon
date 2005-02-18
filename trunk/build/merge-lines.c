c9ee14ea853507117ec8acee677449e3e17332b2
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-18 11:57:07 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2582 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/build/merge-lines.c
@@ -0,0 +1,106 @@
+/*
+ * Merge the current hobbitserver.cfg file with a new template. New entries are added,
+ * and existing ones are copied over from the current setup.
+ */
+#include <stdio.h>
+#include <string.h>
+
+typedef struct entry_t {
+	char *name;
+	char *val;
+	int copied;
+	struct entry_t *next;
+} entry_t;
+
+entry_t *head = NULL;
+entry_t *tail = NULL;
+
+int main(int argc, char *argv[])
+{
+	char *curfn, *curbckfn, *srcfn;
+	FILE *curfd, *curbckfd, *srcfd;
+	char delim = '=';
+	char l[32768];
+	entry_t *ewalk;
+
+	srcfn = strdup(argv[1]);
+	curfn = strdup(argv[2]);
+	curbckfn = (char *)malloc(strlen(curfn) + 5);
+	sprintf(curbckfn, "%s.bak", curfn);
+
+	curfd = fopen(curfn, "r");
+	curbckfd = fopen(curbckfn, "w");
+
+	while (fgets(l, sizeof(l), curfd)) {
+		char *bol, *p;
+
+		fprintf(curbckfd, "%s", l);
+
+		bol = l + strspn(l, " \t\r\n");
+		if ((*bol == '#') || (*bol == '\0')) continue;
+
+		p = strchr(bol, '=');
+		if (p) {
+			entry_t *newent;
+
+			*p = '\0';
+			newent = (entry_t *)malloc(sizeof(entry_t));
+			newent->name = strdup(bol);
+			*p = '=';
+			newent->val = strdup(l);
+			newent->copied = 0;
+
+			if (tail == NULL) {
+				tail = head = newent;
+			}
+			else {
+				tail->next = newent;
+				tail = newent;
+			}
+		}
+	}
+	fclose(curfd);
+	fclose(curbckfd);
+
+	srcfd = fopen(srcfn, "r");
+	curfd = fopen(curfn, "w");
+	while (fgets(l, sizeof(l), srcfd)) {
+		char *bol, *p;
+
+		bol = l + strspn(l, " \t\r\n");
+		if ((*bol == '#') || (*bol == '\0')) {
+			fprintf(curfd, "%s", l);
+			continue;
+		}
+
+		p = strchr(bol, '=');
+		if (p) {
+			*p = '\0';
+			for (ewalk = head; (ewalk && strcmp(ewalk->name, bol)); ewalk = ewalk->next) ;
+			*p = '=';
+
+			if (ewalk) {
+				fprintf(curfd, "%s", ewalk->val);
+				ewalk->copied = 1;
+			}
+			else {
+				printf("Adding new entry %s", l);
+				fprintf(curfd, "%s", l);
+			}
+		}
+		else {
+			fprintf(curfd, "%s", l);
+		}
+	}
+
+	/* Copy over any local settings that have been added */
+	for (ewalk = head; (ewalk); ewalk = ewalk->next) {
+		if (!ewalk->copied) {
+			fprintf(curfd, "%s", ewalk->val);
+		}
+	}
+
+	fclose(curfd);
+	fclose(srcfd);
+}
+
