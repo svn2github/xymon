56877e1763419c0d273f1c34f879b835ff76340f
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-18 14:23:38 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2587 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/build/merge-sects.c
@@ -0,0 +1,110 @@
+/*
+ * Merge the current "[...]" sectioned file with a new template. New entries are added,
+ * and existing ones are copied over from the current setup.
+ */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct entry_t {
+	char *name;
+	char *val;
+	int copied;
+	struct entry_t *next;
+} entry_t;
+
+entry_t *head = NULL;
+entry_t *tail = NULL;
+
+int main(int argc, char *argv[])
+{
+	char *curfn, *curbckfn, *srcfn;
+	FILE *curfd, *curbckfd, *srcfd;
+	char delim = '=';
+	char l[32768];
+	entry_t *ewalk;
+	entry_t *newent = NULL;
+	int adding = 0;
+
+	srcfn = strdup(argv[1]);
+	curfn = strdup(argv[2]);
+	curbckfn = (char *)malloc(strlen(curfn) + 5);
+	sprintf(curbckfn, "%s.bak", curfn);
+
+	curfd = fopen(curfn, "r");
+	curbckfd = fopen(curbckfn, "w");
+
+	while (fgets(l, sizeof(l), curfd)) {
+		char *bol, *p;
+
+		fprintf(curbckfd, "%s", l);
+
+		bol = l + strspn(l, " \t\r\n");
+		if ((*bol == '#') || (*bol == '\0')) continue;
+
+		if ((*bol == '[') && strchr(bol, ']')) {
+			newent = (entry_t *)malloc(sizeof(entry_t));
+			p = strchr(bol, ']'); *p = '\0';
+			newent->name = strdup(bol+1);
+			*p = ']';
+			newent->val = strdup(l);
+			newent->copied = 0;
+
+			if (tail == NULL) {
+				tail = head = newent;
+			}
+			else {
+				tail->next = newent;
+				tail = newent;
+			}
+		}
+		else if (newent) {
+			newent->val = (char *)realloc(newent->val, strlen(newent->val) + strlen(l) + 1);
+			strcat(newent->val, l);
+		}
+	}
+	fclose(curfd);
+	fclose(curbckfd);
+
+	srcfd = fopen(srcfn, "r");
+	curfd = fopen(curfn, "w");
+	while (fgets(l, sizeof(l), srcfd)) {
+		char *bol, *p;
+
+		bol = l + strspn(l, " \t\r\n");
+
+		if ((*bol == '[') && strchr(bol, ']')) {
+			p = strchr(bol, ']'); *p = '\0';
+			for (ewalk = head; (ewalk && strcmp(ewalk->name, bol+1)); ewalk = ewalk->next) ;
+			*p = ']';
+
+			if (ewalk) {
+				fprintf(curfd, "%s", ewalk->val);
+				ewalk->copied = 1;
+				adding = 0;
+			}
+			else {
+				printf("Adding new entry %s", l);
+				fprintf(curfd, "%s", l);
+				adding = 1;
+			}
+		}
+		else if (adding || (*bol == '#') || (*bol == '\0')) {
+			fprintf(curfd, "%s", l);
+		}
+	}
+
+	/* Copy over any local settings that have been added */
+	for (ewalk = head; (ewalk); ewalk = ewalk->next) {
+		if (!ewalk->copied) {
+			fprintf(curfd, "%s", ewalk->val);
+		}
+	}
+
+	fclose(curfd);
+	fclose(srcfd);
+
+	return 0;
+}
+
