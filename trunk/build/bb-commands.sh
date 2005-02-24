ce0fcf58f16399156a1b28a916b167c29705dbd2
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-24 16:24:11 +0000

    All commands must be quoted.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2692 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/bb-commands.sh
+++ trunk/build/bb-commands.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: bb-commands.sh,v 1.2 2005-02-16 21:17:12 henrik Exp $
+# $Id: bb-commands.sh,v 1.3 2005-02-24 16:24:11 henrik Exp $
 
 # Script to pick up most of the commands used by BB extension scripts.
 # This is used during installation, to build a hobbitserver.cfg that
@@ -27,71 +27,71 @@ for CMD in top uptime awk cat cp cut date egrep expr find grep head id ln ls mv 
 do
 	ENVNAME=`echo $CMD | tr "[a-z]" "[A-Z]"`
 	PGM=`findbin $CMD | head -1`
-	echo "${ENVNAME}=${PGM}"
+	echo "${ENVNAME}=\"${PGM}\""
 done
 
 # WC is special
 PGM=`findbin wc | head -1`
-echo "WC=${PGM} -l"
-echo "WCC=${PGM}"
+echo "WC=\"${PGM} -l\""
+echo "WCC=\"${PGM}\""
 
 # DFCMD is an alias for DF
 echo "# You should check that this returns diskstats in KB (Posix output)"
 PGM=`findbin df | head -1`
-echo "DF=${PGM} -Pk"
-echo "DFCMD=${PGM} -Pk"
+echo "DF=\"${PGM} -Pk\""
+echo "DFCMD=\"${PGM} -Pk\""
 
 # PS probably needs tweaking
 echo "# Adjust ps output to your liking"
-echo "PS=ps ax"
+echo "PS=\"ps ax\""
 
 echo ""
 echo "# Miscellaneous definitions used by the BB client"
-echo "MAXLINE=32768"
+echo "MAXLINE=\"32768\""
 
 echo ""
 echo "# For the cpu check"
-echo "DOCPU=TRUE"
-echo "NOCPUCOLOR=clear"
-echo "BBCPUTAB=\$BBHOME/etc/bb-cputab"
-echo "BBTOOMANYDAYSUP=9999"
-echo "CPUPANIC=300"
-echo "CPUWARN=150"
-echo "DISPREALLOADAVG=FALSE"
-echo "TOPARGS=-b -n 1"
-echo "WARNCOLORONREBOOT=yellow"
-echo "WARNMINSONREBOOT=60"
+echo "DOCPU=\"TRUE\""
+echo "NOCPUCOLOR=\"clear\""
+echo "BBCPUTAB=\"\$BBHOME/etc/bb-cputab\""
+echo "BBTOOMANYDAYSUP=\"9999\""
+echo "CPUPANIC=\"300\""
+echo "CPUWARN=\"150\""
+echo "DISPREALLOADAVG=\"FALSE\""
+echo "TOPARGS=\"-b -n 1\""
+echo "WARNCOLORONREBOOT=\"yellow\""
+echo "WARNMINSONREBOOT=\"60\""
 
 echo ""
 echo "# For the disk check"
-echo "DODISK=TRUE"
-echo "NODISKCOLOR=clear"
-echo "BBDFTAB=\$BBHOME/etc/bb-dftab"
-echo "DFEXCLUDE=cdrom"
-echo "DFPANIC=95"
-echo "DFSORT=4"
-echo "DFUSE=^/dev"
-echo "DFWARN=90"
+echo "DODISK=\"TRUE\""
+echo "NODISKCOLOR=\"clear\""
+echo "BBDFTAB=\"\$BBHOME/etc/bb-dftab\""
+echo "DFEXCLUDE=\"cdrom\""
+echo "DFPANIC=\"95\""
+echo "DFSORT=\"4\""
+echo "DFUSE=\"^/dev\""
+echo "DFWARN=\"90\""
 
 echo ""
 echo "# For the msgs check"
-echo "DOMSGS=TRUE"
-echo "NOMSGSCOLOR=clear"
-echo "BBMSGSTAB=\$BBHOME/etc/bb-msgstab"
-echo "CHKMSGLEN=TRUE"
-echo "IGNMSGS="
-echo "MSGEXPIRE=30:60"
-echo "MSGFILE=/var/log/messages"
-echo "MSGS=NOTICE WARNING"
-echo "PAGEMSG=NOTICE"
-echo "REDMSGSLINES=20"
-echo "YELLOWMSGSLINES=10"
+echo "DOMSGS=\"TRUE\""
+echo "NOMSGSCOLOR=\"clear\""
+echo "BBMSGSTAB=\"\$BBHOME/etc/bb-msgstab\""
+echo "CHKMSGLEN=\"TRUE\""
+echo "IGNMSGS=\"\""
+echo "MSGEXPIRE=\"30:60\""
+echo "MSGFILE=\"/var/log/messages\""
+echo "MSGS=\"NOTICE WARNING\""
+echo "PAGEMSG=\"NOTICE\""
+echo "REDMSGSLINES=\"20\""
+echo "YELLOWMSGSLINES=\"10\""
 
 echo ""
 echo "# For the procs check"
-echo "DOPROCS=TRUE"
-echo "NOPROCSCOLOR=clear"
-echo "BBPROCTAB=\$BBHOME/etc/bb-proctab"
-echo "PAGEPROC=cron"
-echo "PROCS=bbrun"
+echo "DOPROCS=\"TRUE\""
+echo "NOPROCSCOLOR=\"clear\""
+echo "BBPROCTAB=\"\$BBHOME/etc/bb-proctab\""
+echo "PAGEPROC=\"cron\""
+echo "PROCS=\"bbrun\""
 
