cc30351bcbc12d687d9612000bf45f6a62e1655b
(no author) <(no author)@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-02-27 16:39:32 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_4_0-RC4'.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2727 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ tags/rel_4_0-RC4/build/ldap.sh
@@ -0,0 +1,63 @@
+	echo "Checking for LDAP ..."
+
+	LDAPINC=""
+	LDAPLIB=""
+	for DIR in /opt/openldap* /opt/ldap* /usr/local/openldap* /usr/local/ldap* /usr/local /usr /usr/pkg /opt/csw
+	do
+		if test -f $DIR/include/ldap.h
+		then
+			LDAPINC=$DIR/include
+		fi
+
+		if test -f $DIR/lib/libldap.so
+		then
+			LDAPLIB=$DIR/lib
+		fi
+		if test -f $DIR/lib/libldap.a
+		then
+			LDAPLIB=$DIR/lib
+		fi
+	done
+
+	#
+	# Some systems require liblber also
+	#
+	if test -f $LDAPLIB/liblber.a
+	then
+		LDAPLBER=-llber
+	fi
+	if test -f $LDAPLIB/liblber.so
+	then
+		LDAPLBER=-llber
+	fi
+
+	if test -z "$LDAPINC" -o -z "$LDAPLIB"; then
+		echo "(Open)LDAP include- or library-files not found."
+		echo "If you want to perform detailed LDAP tests (queries), you need to"
+		echo "install LDAP an LDAP client library that bbgen can use."
+		echo "OpenLDAP can be found at http://www.openldap.org/"
+		echo "Continuing with LDAP support disabled."
+	else
+		cd build
+		OS=`uname -s` $MAKE -f Makefile.test-ldap clean
+		OS=`uname -s` LDAPINC="-I$LDAPINC" $MAKE -f Makefile.test-ldap test-compile
+		if [ $? -eq 0 ]; then
+			echo "Found LDAP include files in $LDAPINC"
+		else
+			echo "WARNING: LDAP include files found in $LDAPINC, but compile fails."
+		fi
+
+		OS=`uname -s` LDAPLIB="-L$LDAPLIB" LDAPLBER="$LDAPLBER" $MAKE -f Makefile.test-ldap test-link
+		if [ $? -eq 0 ]; then
+			echo "Found LDAP libraries in $LDAPLIB"
+		else
+			echo "WARNING: LDAP library files found in $LDAPLIB, but link fails."
+		fi
+
+		LDAPVENDOR=`./test-ldap`
+
+		OS=`uname -s` $MAKE -f Makefile.test-ldap clean
+		cd ..
+	fi
+
+
---
+++ tags/rel_4_0-RC4/build/ssl.sh
@@ -0,0 +1,59 @@
+	echo "Checking for OpenSSL ..."
+
+	OSSLINC=""
+	OSSLLIB=""
+	for DIR in /opt/openssl* /opt/ssl* /usr/local/openssl* /usr/local/ssl* /usr/local /usr /usr/pkg /opt/csw/ssl*
+	do
+		if test -d $DIR/include/openssl
+		then
+			OSSLINC=$DIR/include
+		fi
+
+		if test -f $DIR/lib/libcrypto.so
+		then
+			OSSLLIB=$DIR/lib
+		fi
+		if test -f $DIR/lib/libcrypto.a
+		then
+			OSSLLIB=$DIR/lib
+		fi
+	done
+
+	if test -z "$OSSLINC" -o -z "$OSSLLIB"; then
+		echo "OpenSSL include- or library-files not found."
+		echo "Although you can use Hobbit and bbgen without OpenSSL, you will not"
+		echo "be able to run network tests of SSL-enabled services, e.g. https."
+		echo "So installing OpenSSL is recommended."
+		echo "OpenSSL can be found at http://www.openssl.org/"
+		echo "Continuing with SSL support disabled."
+	else
+		# Red Hat in their wisdom ships an openssl that depends on Kerberos,
+		# and then puts the Kerberos includes where they are not found automatically.
+		if test "`uname -s`" = "Linux" -a -d /usr/kerberos/include
+		then
+			OSSLINC2="-I/usr/kerberos/include"
+		else
+			OSSLINC2=""
+		fi
+
+		cd build
+		OS=`uname -s` $MAKE -f Makefile.test-ssl clean
+		OS=`uname -s` OSSLINC="-I$OSSLINC $OSSLINC2" $MAKE -f Makefile.test-ssl test-compile
+		if [ $? -eq 0 ]; then
+			echo "Found OpenSSL include files in $OSSLINC"
+		else
+			echo "WARNING: OpenSSL include files found in $OSSLINC, but compile fails."
+		fi
+	
+		OS=`uname -s` OSSLLIB="-L$OSSLLIB" $MAKE -f Makefile.test-ssl test-link
+		if [ $? -eq 0 ]; then
+			echo "Found OpenSSL libraries in $OSSLLIB"
+		else
+			echo "WARNING: OpenSSL library files found in $OSSLLIB, but link fails."
+		fi
+		OS=`uname -s` $MAKE -f Makefile.test-ssl clean
+		cd ..
+
+		OSSLINC="$OSSLINC $OSSLINC2"
+	fi
+
