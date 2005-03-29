c7df91dd3d2be237c3f368adb8901e1e586aaa9f
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-03-29 15:16:16 +0000

    Fix broken code.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2942 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/rrd/do_citrix.c
+++ trunk/hobbitd/rrd/do_citrix.c
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char citrix_rcsid[] = "$Id: do_citrix.c,v 1.7 2005-03-25 21:15:26 henrik Exp $";
+static char citrix_rcsid[] = "$Id: do_citrix.c,v 1.8 2005-03-29 15:16:16 henrik Exp $";
 
 static char *citrix_params[] = { "rrdcreate", rrdfn, "DS:users:GAUGE:600:0:U", rra1, rra2, rra3, rra4, NULL };
 
@@ -18,8 +18,8 @@ int do_citrix_larrd(char *hostname, char *testname, char *msg, time_t tstamp)
 	int users;
 
 	p = strstr(msg, " users active\n");
-	if (p) p = strrchr(p, '\n');
-	if (p && (sscanf(p+1, "%d users active\n", &users) == 1)) {
+	while (p && (p > msg) && (*p != '\n')) p--;
+	if (p && (sscanf(p+1, "\n%d users active\n", &users) == 1)) {
 		sprintf(rrdfn, "citrix.rrd");
 		sprintf(rrdvalues, "%d:%d", (int)tstamp, users);
 		return create_and_update_rrd(hostname, rrdfn, citrix_params, update_params);
