2f18fba5b83114cb3572a36910516c5555f4ebe7
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-03-30 19:53:04 +0000

    added RC6
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@2958 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/generate-md5.sh
+++ trunk/build/generate-md5.sh
@@ -2,7 +2,7 @@
 
 cd ~/hobbit/beta
 
-VERLIST="4.0-beta4 4.0-beta5 4.0-beta6 4.0-RC1 4.0-RC2 4.0-RC3 4.0-RC4 4.0-RC5"
+VERLIST="4.0-beta4 4.0-beta5 4.0-beta6 4.0-RC1 4.0-RC2 4.0-RC3 4.0-RC4 4.0-RC5 4.0-RC6"
 
 WEBLIST=`(cd ~/hobbit/hobbitd; find webfiles -type f) | grep -v RCS | xargs echo`
 WWWLIST=`(cd ~/hobbit/hobbitd; find wwwfiles -type f) | grep -v RCS | xargs echo`
