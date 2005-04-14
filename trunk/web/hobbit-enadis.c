8aa79898394e5e1ae89ac31b24d22ddf6a779295
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-04-14 08:24:42 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3069 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/web/hobbit-enadis.c
@@ -0,0 +1,168 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit backend script for disabling/enabling tests.                        */
+/*                                                                            */
+/* Copyright (C) 2003-2005 Henrik Storner <henrik@storner.dk>                 */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: hobbit-enadis.c,v 1.1 2005-04-14 08:24:42 henrik Exp $";
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "libbbgen.h"
+
+enum { ACT_NONE, ACT_ENABLE, ACT_DISABLE } action = ACT_NONE;
+char *hostname = NULL;
+char *enabletest = NULL;
+int duration = 0;
+int scale = 1;
+int disablecount = 0;
+char **disabletest = NULL;
+char *disablemsg = NULL;
+
+void errormsg(char *msg)
+{
+        printf("Content-type: text/html\n\n");
+        printf("<html><head><title>Invalid request</title></head>\n");
+        printf("<body>%s</body></html>\n", msg);
+        exit(1);
+}
+
+void parse_query(void)
+{
+	char l[4096];
+        char *token;
+
+	while (fgets(l, sizeof(l), stdin)) {
+		l[sizeof(l)-1] = '\0';
+		token = strtok(l, "&");
+
+		while (token) {
+			char *val;
+
+			val = strchr(token, '='); if (val) { *val = '\0'; val++; }
+			if (val) val = urlunescape(val);
+
+			if (strcmp(token, "go") == 0) {
+				if (strcasecmp(val, "enable") == 0) action = ACT_ENABLE;
+				else if (strcasecmp(val, "disable") == 0) action = ACT_DISABLE;
+			}
+			else if (strcmp(token, "duration") == 0) {
+				duration = atoi(val);
+			}
+			else if (strcmp(token, "scale") == 0) {
+				scale = atoi(val);
+			}
+			else if (strcmp(token, "cause") == 0) {
+				disablemsg = strdup(val);
+			}
+			else if (strcmp(token, "hostname") == 0) {
+				hostname = strdup(val);
+			}
+			else if (strcmp(token, "enabletest") == 0) {
+				enabletest = strdup(val);
+			}
+			else if (strcmp(token, "disabletest") == 0) {
+				if (disabletest == NULL) {
+					disabletest = (char **)malloc(2 * sizeof(char *));
+					disabletest[0] = strdup(val);
+					disabletest[1] = NULL;
+					disablecount = 1;
+				}
+				else {
+					disabletest = (char **)realloc(disabletest, (disablecount + 2) * sizeof(char *));
+					disabletest[disablecount] = strdup(val);
+					disabletest[disablecount+1] = NULL;
+					disablecount++;
+				}
+			}
+
+			token = strtok(NULL, "&");
+		}
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+	int argi, i, result;
+	char hobbitcmd[4096];
+
+	for (argi=1; (argi < argc); argi++) {
+		if (argnmatch(argv[argi], "--env=")) {
+			char *p = strchr(argv[argi], '=');
+			loadenv(p+1);
+		}
+		else if (strcmp(argv[argi], "--debug") == 0) {
+			debug = 1;
+		}
+	}
+
+	parse_query();
+
+	/*
+	 * Ready ... go build the webpage.
+	 */
+	printf("Content-Type: text/html\n\n");
+	printf("<html>\n");
+	printf("<head>\n<meta http-equiv=\"refresh\" content=\"%d; URL=%s\"></head>\n", 
+		(debug ? 15 : 5), xgetenv("HTTP_REFERER"));
+
+        /* It's ok with these hardcoded values, as they are not used for this page */
+	sethostenv("", "", "", colorname(COL_BLUE));
+	headfoot(stdout, "maint", "", "header", COL_BLUE);
+
+	if (debug) {
+		printf("<pre>\n");
+		dprintf("Hostname = %s\n", hostname);
+		switch (action) {
+		  case ACT_NONE   : dprintf("Action = none\n"); break;
+
+		  case ACT_ENABLE : dprintf("Action = enable, Test = %s\n", textornull(enabletest)); 
+				    break;
+
+		  case ACT_DISABLE: dprintf("Action = disable\n"); 
+				    dprintf("Tests = ");
+				    for (i=0; (i < disablecount); i++) printf("%s ", disabletest[i]);
+				    printf("\n");
+				    dprintf("Duration = %d, scale = %d\n", duration, scale);
+				    dprintf("Cause = %s\n", textornull(disablemsg));
+				    break;
+		}
+	}
+
+	switch (action) {
+	  case ACT_NONE:
+		break;
+
+	  case ACT_ENABLE:
+		sprintf(hobbitcmd, "enable %s.%s", commafy(hostname), enabletest);
+		result = sendmessage(hobbitcmd, NULL, NULL, NULL, 0, BBTALK_TIMEOUT);
+		printf("Enabling host %s test %s : %s\n", hostname, enabletest, ((result == BB_OK) ? "OK" : "Failed"));
+		break;
+
+	  case ACT_DISABLE:
+		for (i=0; (i < disablecount); i++) {
+			sprintf(hobbitcmd, "disable %s.%s %d %s", 
+				commafy(hostname), disabletest[i], duration*scale, disablemsg);
+			result = sendmessage(hobbitcmd, NULL, NULL, NULL, 0, BBTALK_TIMEOUT);
+			printf("Disabling host %s test %s: %s\n", 
+				hostname, disabletest[i], ((result == BB_OK) ? "OK" : "Failed"));
+		}
+		break;
+	}
+
+	printf("</pre>\n");
+	headfoot(stdout, "maint", "", "footer", COL_BLUE);
+
+	return 0;
+}
+
