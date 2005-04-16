13829f434f460d2bece7c78b3fe5e5b6ee5cd709
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-04-16 07:20:35 +0000

    New control function to apply filter
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3076 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/headfoot.h
+++ trunk/lib/headfoot.h
@@ -21,6 +21,7 @@ extern void sethostenv_snapshot(time_t snapshot);
 extern void sethostenv_histlog(char *histtime);
 extern void sethostenv_template(char *dir);
 extern void sethostenv_refresh(int n);
+extern void sethostenv_filter(char *hostptn, char *pageptn, char *ipptn);
 extern void output_parsed(FILE *output, char *templatedata, int bgcolor, char *pagetype, time_t selectedtime);
 extern void headfoot(FILE *output, char *pagetype, char *pagepath, char *head_or_foot, int bgcolor);
 
