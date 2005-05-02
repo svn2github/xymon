4c3c0d1ef552e2cccb33507b9e280bfcfb7f1c79
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-05-02 20:07:30 +0000

    Some cleanup and new features.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3163 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/dorelease.sh
+++ trunk/build/dorelease.sh
@@ -4,55 +4,66 @@ case "$1" in
 	"tag"|"untag"|"release")
 		CMD="$1"
 		REL="$2"
+		RELDIR=~/hobbit/release/hobbit-$REL
+		;;
+
+	"daily")
+		CMD="daily"
+		REL="snapshot"
+		RELDIR=~/hobbit/beta/snapshot
+		if [ -d $RELDIR ]; then
+			(cd $RELDIR && rm -rf *)
+		fi
 		;;
 
 	*)
-		CMD="release"
-		REL="$1"
-		;;
+		echo "$0 [tag|untag|release|daily] version"
+		exit
 esac
 
-if [ "$REL" = "" -o "$CMD" = "" ]; then
-	echo "$0 [tag|untag] version"
-	exit
-fi
-
 cd ~/hobbit
-if [ "$CMD" = "release" ]; then make distclean; fi
-
 RCSTAG=`echo $REL | sed 's/\./_/g'`
 DIRLIST=`find . -name RCS | sed -e 's/\/RCS//'|grep -v private|xargs echo`
 
-if [ "$CMD" = "untag" ]
-then
-	for f in . $DIRLIST
-	do
-		rcs -nrel_$RCSTAG ~/hobbit/$f/RCS/*
-	done
+case "$CMD" in
+	"release")
+		make distclean
+		for f in $DIRLIST
+		do
+			rcs -nrel_$RCSTAG: ~/hobbit/$f/RCS/*
+		done
+		;;
 
-	exit 0
-fi
+	"untag")
+		for f in . $DIRLIST
+		do
+			rcs -nrel_$RCSTAG ~/hobbit/$f/RCS/*
+		done
+		exit 0
+		;;
 
-# It's tag or release - so tag the files.
-for f in $DIRLIST
-do
-	rcs -nrel_$RCSTAG: ~/hobbit/$f/RCS/*
-done
+	"tag")
+		for f in $DIRLIST
+		do
+			rcs -nrel_$RCSTAG: ~/hobbit/$f/RCS/*
+		done
+		exit 0
+		;;
 
-if [ "$CMD" = "tag" ]
-then
-	exit 0
-fi
+	*)
+		;;
+esac
 
 # It's a release - copy the files
 cd ~/hobbit
-mkdir ~/hobbit/release/hobbit-$REL
+mkdir $RELDIR
 for f in bbdisplay bbnet bbpatches bbproxy build common docs hobbitd include lib scripts
 do
-	find $f/ | grep -v RCS | cpio -pdvmu ~/hobbit/release/hobbit-$REL/
+	find $f/ | grep -v RCS | cpio -pdvmu $RELDIR/
 done
-cp -p Changes configure COPYING CREDITS README ~/hobbit/release/hobbit-$REL/
-find ~/hobbit/release/hobbit-$REL -type d|xargs chmod 755
+cp -p Changes configure COPYING CREDITS README $RELDIR/
+find $RELDIR -type d|xargs chmod 755
+cd $RELDIR/../ && tar zcf hobbit-$REL.tar.gz `basename $RELDIR`
 
 exit 0
 
