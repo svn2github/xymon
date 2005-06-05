a61c325bdb870b5affe8c84ef5dd5e16d2a4b6b7
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-06-05 09:40:25 +0000

    Global "larrd" -> "hobbit" change. Drop old compatibility code for LARRD and BB.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3331 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/hobbitrrd.c
+++ trunk/lib/hobbitrrd.c
@@ -2,7 +2,7 @@
 /* Hobbit monitor library.                                                    */
 /*                                                                            */
 /* This is a library module, part of libbbgen.                                */
-/* It contains routines for working with LARRD graphs.                        */
+/* It contains routines for working with RRD graphs.                          */
 /*                                                                            */
 /* Copyright (C) 2002-2005 Henrik Storner <henrik@storner.dk>                 */
 /*                                                                            */
@@ -11,7 +11,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: hobbitrrd.c,v 1.31 2005-04-01 21:39:31 henrik Exp $";
+static char rcsid[] = "$Id: hobbitrrd.c,v 1.32 2005-06-05 09:40:25 henrik Exp $";
 
 #include <ctype.h>
 #include <stdlib.h>
@@ -23,31 +23,29 @@ static char rcsid[] = "$Id: hobbitrrd.c,v 1.31 2005-04-01 21:39:31 henrik Exp $"
 #include "libbbgen.h"
 #include "version.h"
 
-#include "bblarrd.h"
-
 /* This is for mapping a status-name -> RRD file */
-larrdrrd_t *larrdrrds = NULL;
+hobbitrrd_t *hobbitrrds = NULL;
 
-/* This is the information needed to generate links to larrd-grapher.cgi */
-larrdgraph_t *larrdgraphs = NULL;
+/* This is the information needed to generate links on the trends column page  */
+hobbitgraph_t *hobbitgraphs = NULL;
 
-static const char *bblinkfmt = "<br><A HREF=\"%s\"><IMG BORDER=0 SRC=\"%s&amp;graph=hourly\" ALT=\"larrd is accumulating %s\"></A>\n";
+static const char *bblinkfmt = "<br><A HREF=\"%s\"><IMG BORDER=0 SRC=\"%s&amp;graph=hourly\" ALT=\"Hobbit is accumulating %s\"></A>\n";
 
 static const char *hobbitlinkfmt = "<table summary=\"%s Graph\"><tr><td><A HREF=\"%s&amp;action=menu\"><IMG BORDER=0 SRC=\"%s&amp;graph=hourly&amp;action=view\" ALT=\"hobbit graph %s\"></A></td><td> <td align=\"left\" valign=\"top\"> <a href=\"%s&amp;graph=hourly&amp;action=selzoom\"> <img src=\"%s/zoom.gif\" border=0 alt=\"Zoom graph\" style='padding: 3px'> </a> </td></tr></table>\n";
 
 static const char *metafmt = "<RRDGraph>\n  <GraphType>%s</GraphType>\n  <GraphLink><![CDATA[%s]]></GraphLink>\n  <GraphImage><![CDATA[%s&amp;graph=hourly]]></GraphImage>\n</RRDGraph>\n";
 
 /*
- * Define the mapping between BB columns and LARRD graphs.
+ * Define the mapping between BB columns and RRD graphs.
  * Normally they are identical, but some RRD's use different names.
  */
-static void larrd_setup(void)
+static void rrd_setup(void)
 {
 	static int setup_done = 0;
 	char *lenv, *ldef, *p, *tcptests, *services;
 	int count;
-	larrdrrd_t *lrec;
-	larrdgraph_t *grec;
+	hobbitrrd_t *lrec;
+	hobbitgraph_t *grec;
 
 	/* Do nothing if we have been called within the past 5 minutes */
 	if ((setup_done + 300) >= time(NULL)) return;
@@ -58,21 +56,21 @@ static void larrd_setup(void)
 	 * NB: These lists are NOT null-terminated ! 
 	 *     Stop when bbsvcname becomes a NULL.
 	 */
-	lrec = larrdrrds;
+	lrec = hobbitrrds;
 	while (lrec && lrec->bbsvcname) {
-		if (lrec->larrdrrdname != lrec->bbsvcname) xfree(lrec->larrdrrdname);
+		if (lrec->hobbitrrdname != lrec->bbsvcname) xfree(lrec->hobbitrrdname);
 		xfree(lrec->bbsvcname);
 		lrec++;
 	}
-	if (larrdrrds) xfree(larrdrrds);
+	if (hobbitrrds) xfree(hobbitrrds);
 
-	grec = larrdgraphs;
-	while (grec && grec->larrdrrdname) {
-		if (grec->larrdpartname) xfree(grec->larrdpartname);
-		xfree(grec->larrdrrdname);
+	grec = hobbitgraphs;
+	while (grec && grec->hobbitrrdname) {
+		if (grec->hobbitpartname) xfree(grec->hobbitpartname);
+		xfree(grec->hobbitrrdname);
 		grec++;
 	}
-	if (larrdgraphs) xfree(larrdgraphs);
+	if (hobbitgraphs) xfree(hobbitgraphs);
 
 
 	/* Get the tcp services, and count how many there are */
@@ -81,27 +79,27 @@ static void larrd_setup(void)
 	count = 0; p = strtok(tcptests, " "); while (p) { count++; p = strtok(NULL, " "); }
 	strcpy(tcptests, services);
 
-	/* Setup the larrdrrds table, mapping test-names to RRD files */
-	lenv = (char *)malloc(strlen(xgetenv("LARRDS")) + strlen(tcptests) + count*strlen(",=tcp") + 1);
-	strcpy(lenv, xgetenv("LARRDS")); 
+	/* Setup the hobbitrrds table, mapping test-names to RRD files */
+	lenv = (char *)malloc(strlen(xgetenv("TEST2RRD")) + strlen(tcptests) + count*strlen(",=tcp") + 1);
+	strcpy(lenv, xgetenv("TEST2RRD")); 
 	p = lenv+strlen(lenv)-1; if (*p == ',') *p = '\0';	/* Drop a trailing comma */
 	p = strtok(tcptests, " "); while (p) { sprintf(lenv+strlen(lenv), ",%s=tcp", p); p = strtok(NULL, " "); }
 	xfree(tcptests);
 	xfree(services);
 
 	count = 0; p = lenv; do { count++; p = strchr(p+1, ','); } while (p);
-	larrdrrds = (larrdrrd_t *)calloc(sizeof(larrdrrd_t), (count+1));
+	hobbitrrds = (hobbitrrd_t *)calloc(sizeof(hobbitrrd_t), (count+1));
 
-	lrec = larrdrrds; ldef = strtok(lenv, ",");
+	lrec = hobbitrrds; ldef = strtok(lenv, ",");
 	while (ldef) {
 		p = strchr(ldef, '=');
 		if (p) {
 			*p = '\0'; 
 			lrec->bbsvcname = strdup(ldef);
-			lrec->larrdrrdname = strdup(p+1);
+			lrec->hobbitrrdname = strdup(p+1);
 		}
 		else {
-			lrec->bbsvcname = lrec->larrdrrdname = strdup(ldef);
+			lrec->bbsvcname = lrec->hobbitrrdname = strdup(ldef);
 		}
 
 		ldef = strtok(NULL, ",");
@@ -109,31 +107,31 @@ static void larrd_setup(void)
 	}
 	xfree(lenv);
 
-	/* Setup the larrdgraphs table, describing how to make graphs from an RRD */
+	/* Setup the hobbitgraphs table, describing how to make graphs from an RRD */
 	lenv = strdup(xgetenv("GRAPHS"));
 	p = lenv+strlen(lenv)-1; if (*p == ',') *p = '\0';	/* Drop a trailing comma */
 	count = 0; p = lenv; do { count++; p = strchr(p+1, ','); } while (p);
-	larrdgraphs = (larrdgraph_t *)calloc(sizeof(larrdgraph_t), (count+1));
+	hobbitgraphs = (hobbitgraph_t *)calloc(sizeof(hobbitgraph_t), (count+1));
 
-	grec = larrdgraphs; ldef = strtok(lenv, ",");
+	grec = hobbitgraphs; ldef = strtok(lenv, ",");
 	while (ldef) {
 		p = strchr(ldef, ':');
 		if (p) {
 			*p = '\0'; 
-			grec->larrdrrdname = strdup(ldef);
-			grec->larrdpartname = strdup(p+1);
-			p = strchr(grec->larrdpartname, ':');
+			grec->hobbitrrdname = strdup(ldef);
+			grec->hobbitpartname = strdup(p+1);
+			p = strchr(grec->hobbitpartname, ':');
 			if (p) {
 				*p = '\0';
 				grec->maxgraphs = atoi(p+1);
-				if (strlen(grec->larrdpartname) == 0) {
-					xfree(grec->larrdpartname);
-					grec->larrdpartname = NULL;
+				if (strlen(grec->hobbitpartname) == 0) {
+					xfree(grec->hobbitpartname);
+					grec->hobbitpartname = NULL;
 				}
 			}
 		}
 		else {
-			grec->larrdrrdname = strdup(ldef);
+			grec->hobbitrrdname = strdup(ldef);
 		}
 
 		ldef = strtok(NULL, ",");
@@ -145,36 +143,36 @@ static void larrd_setup(void)
 }
 
 
-larrdrrd_t *find_larrd_rrd(char *service, char *flags)
+hobbitrrd_t *find_hobbit_rrd(char *service, char *flags)
 {
-	/* Lookup an entry in the larrdrrds table */
-	larrdrrd_t *lrec;
+	/* Lookup an entry in the hobbitrrds table */
+	hobbitrrd_t *lrec;
 
-	larrd_setup();
+	rrd_setup();
 
 	if (flags && (strchr(flags, 'R') != NULL)) {
-		/* Dont do LARRD for reverse tests, since they have no data */
+		/* Dont do RRD's for reverse tests, since they have no data */
 		return NULL;
 	}
 
-	lrec = larrdrrds; while (lrec->bbsvcname && strcmp(lrec->bbsvcname, service)) lrec++;
+	lrec = hobbitrrds; while (lrec->bbsvcname && strcmp(lrec->bbsvcname, service)) lrec++;
 	return (lrec->bbsvcname ? lrec : NULL);
 }
 
-larrdgraph_t *find_larrd_graph(char *rrdname)
+hobbitgraph_t *find_hobbit_graph(char *rrdname)
 {
-	/* Lookup an entry in the larrdgraphs table */
-	larrdgraph_t *grec;
+	/* Lookup an entry in the hobbitgraphs table */
+	hobbitgraph_t *grec;
 	int found = 0;
 	char *dchar;
 
-	larrd_setup();
-	grec = larrdgraphs; 
-	while (!found && (grec->larrdrrdname != NULL)) {
-		found = (strncmp(grec->larrdrrdname, rrdname, strlen(grec->larrdrrdname)) == 0);
+	rrd_setup();
+	grec = hobbitgraphs; 
+	while (!found && (grec->hobbitrrdname != NULL)) {
+		found = (strncmp(grec->hobbitrrdname, rrdname, strlen(grec->hobbitrrdname)) == 0);
 		if (found) {
 			/* Check that it's not a partial match, e.g. "ftp" matches "ftps" */
-			dchar = rrdname + strlen(grec->larrdrrdname);
+			dchar = rrdname + strlen(grec->hobbitrrdname);
 			if ( (*dchar != '.') && (*dchar != ',') && (*dchar != '\0') ) found = 0;
 		}
 
@@ -185,9 +183,8 @@ larrdgraph_t *find_larrd_graph(char *rrdname)
 }
 
 
-static char *larrd_graph_text(char *hostname, char *dispname, char *service, 
-			      larrdgraph_t *graphdef, int itemcount, int larrd043, int hobbitd,
-			      const char *fmt)
+static char *hobbit_graph_text(char *hostname, char *dispname, char *service, 
+			      hobbitgraph_t *graphdef, int itemcount, const char *fmt)
 {
 	static char *rrdurl = NULL;
 	static int rrdurlsize = 0;
@@ -197,19 +194,18 @@ static char *larrd_graph_text(char *hostname, char *dispname, char *service,
 
 	MEMDEFINE(rrdservicename);
 
-	dprintf("rrdlink_url: host %s, rrd %s (partname:%s, maxgraphs:%d, count=%d), larrd043=%d\n", 
+	dprintf("rrdlink_url: host %s, rrd %s (partname:%s, maxgraphs:%d, count=%d)\n", 
 		hostname, 
-		graphdef->larrdrrdname, textornull(graphdef->larrdpartname), graphdef->maxgraphs, itemcount, 
-		larrd043);
+		graphdef->hobbitrrdname, textornull(graphdef->hobbitpartname), graphdef->maxgraphs, itemcount);
 
-	if ((service != NULL) && (strcmp(graphdef->larrdrrdname, "tcp") == 0)) {
+	if ((service != NULL) && (strcmp(graphdef->hobbitrrdname, "tcp") == 0)) {
 		sprintf(rrdservicename, "tcp:%s", service);
 	}
-	else if ((service != NULL) && (strcmp(graphdef->larrdrrdname, "ncv") == 0)) {
+	else if ((service != NULL) && (strcmp(graphdef->hobbitrrdname, "ncv") == 0)) {
 		sprintf(rrdservicename, "ncv:%s", service);
 	}
 	else {
-		strcpy(rrdservicename, graphdef->larrdrrdname);
+		strcpy(rrdservicename, graphdef->hobbitrrdname);
 	}
 
 	svcurllen = 2048                        + 
@@ -231,7 +227,7 @@ static char *larrd_graph_text(char *hostname, char *dispname, char *service,
 	}
 	*rrdurl = '\0';
 
-	if (hobbitd) {
+	{
 		char *rrdparturl;
 		int first = 1;
 		int step;
@@ -266,38 +262,6 @@ static char *larrd_graph_text(char *hostname, char *dispname, char *service,
 		} while (first <= itemcount);
 		xfree(rrdparturl);
 	}
-	else if (larrd043 && graphdef->larrdpartname && (itemcount > 0)) {
-		char *rrdparturl;
-		int first = 0;
-
-		rrdparturl = (char *) malloc(rrdparturlsize);
-		do {
-			int last;
-			
-			last = (first-1)+graphdef->maxgraphs; if (last > itemcount) last = itemcount;
-
-			sprintf(svcurl, "%s/larrd-grapher.cgi?host=%s&amp;service=%s&amp;%s=%d..%d", 
-				xgetenv("CGIBINURL"), hostname, rrdservicename,
-				graphdef->larrdpartname, first, last);
-			strcat(svcurl, "&amp;disp=");
-			strcat(svcurl, urlencode(dispname ? dispname : hostname));
-			sprintf(rrdparturl, fmt, svcurl, svcurl, rrdservicename, svcurl, xgetenv("BBSKIN"));
-			if ((strlen(rrdparturl) + strlen(rrdurl) + 1) >= rrdurlsize) {
-				rrdurlsize += (4096 + rrdparturlsize);
-				rrdurl = (char *) realloc(rrdurl, rrdurlsize);
-			}
-			strcat(rrdurl, rrdparturl);
-			first = last+1;
-		} while (first < itemcount);
-		xfree(rrdparturl);
-	}
-	else {
-		sprintf(svcurl, "%s/larrd-grapher.cgi?host=%s&amp;service=%s", 
-			xgetenv("CGIBINURL"), hostname, rrdservicename);
-		strcat(svcurl, "&amp;disp=");
-		strcat(svcurl, urlencode(dispname ? dispname : hostname));
-		sprintf(rrdurl, fmt, svcurl, svcurl, rrdservicename, svcurl, xgetenv("BBSKIN"));
-	}
 
 	dprintf("URLtext: %s\n", rrdurl);
 
@@ -309,15 +273,9 @@ static char *larrd_graph_text(char *hostname, char *dispname, char *service,
 }
 
 
-char *larrd_graph_data(char *hostname, char *dispname, char *service, 
-		      larrdgraph_t *graphdef, int itemcount, int larrd043, int hobbitd,
-		      int wantmeta)
+char *hobbit_graph_data(char *hostname, char *dispname, char *service, 
+		        hobbitgraph_t *graphdef, int itemcount, int wantmeta)
 {
-	if (wantmeta)
-		return larrd_graph_text(hostname, dispname, service, graphdef, itemcount, 1, 1, metafmt);
-	else if (hobbitd)
-		return larrd_graph_text(hostname, dispname, service, graphdef, itemcount, larrd043, hobbitd, hobbitlinkfmt);
-	else
-		return larrd_graph_text(hostname, dispname, service, graphdef, itemcount, larrd043, hobbitd, bblinkfmt);
+	return hobbit_graph_text(hostname, dispname, service, graphdef, itemcount, (wantmeta ? metafmt : hobbitlinkfmt));
 }
 
