cb04b8f4dedae227bff5bc4344449a59a2b5a054
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-07-22 11:01:05 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3536 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/client/hobbitclient-darwin.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Experimental Darwin (Mac OS X) client for Hobbit
+
+echo "[date]"
+date
+echo "[uname]"
+uname -a
+echo "[uptime]"
+uptime
+echo "[who]"
+who
+echo "[df]"
+df -H -tnonfs,nullfs,cd9660,procfs
+echo "[meminfo]"
+$BBHOME/bin/darwin-meminfo
+echo "[swapinfo]"
+echo "Swap data not supported on Darwin"
+echo "[netstat]"
+netstat -s
+echo "[ps]"
+ps -axw
+echo "[top]"
+top -n 20
+# vmstat
+nohup sh -c "vmstat 300 2 1>$BBTMP/hobbit_vmstat.$$ 2>&1; mv $BBTMP/hobbit_vmstat.$$ $BBTMP/hobbit_vmstat" </dev/null >/dev/null 2>&1 &
+sleep 5
+if test -f $BBTMP/hobbit_vmstat; then echo "[vmstat]"; cat $BBTMP/hobbit_vmstat; rm -f $BBTMP/hobbit_vmstat; fi
+
+exit
+
