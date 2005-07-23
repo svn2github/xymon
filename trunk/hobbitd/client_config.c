b33a233c4c7a8b3bad5ecadfbb263cab02326c63
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-07-23 08:05:12 +0000

    Think we have a working configuration tool now .
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3553 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/client_config.c
+++ trunk/hobbitd/client_config.c
@@ -2,6 +2,8 @@
 /* Hobbit message daemon.                                                     */
 /*                                                                            */
 /* Client backend module                                                      */
+/* This file has routines that load the hobbitd_client configuration and      */
+/* finds the rules relevant for a particular test when applied.               */
 /*                                                                            */
 /* Copyright (C) 2005 Henrik Storner <henrik@hswn.dk>                         */
 /*                                                                            */
@@ -10,7 +12,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: client_config.c,v 1.1 2005-07-22 16:14:06 henrik Exp $";
+static char rcsid[] = "$Id: client_config.c,v 1.2 2005-07-23 08:05:12 henrik Exp $";
 
 #include <stdio.h>
 #include <string.h>
@@ -20,11 +22,21 @@ static char rcsid[] = "$Id: client_config.c,v 1.1 2005-07-22 16:14:06 henrik Exp
 #include <signal.h>
 #include <time.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <errno.h>
 
 #include <pcre.h>
 
 #include "libbbgen.h"
 #include "client_config.h"
+
+typedef struct exprlist_t {
+	char *pattern;
+	pcre *exp;
+	struct exprlist_t *next;
+} exprlist_t;
 
 typedef struct c_load_t {
 	float warnlevel, paniclevel;
@@ -35,8 +47,7 @@ typedef struct c_uptime_t {
 } c_uptime_t;
 
 typedef struct c_disk_t {
-	char *fspattern;
-	pcre *fspcre;
+	exprlist_t *fsexp;
 	int warnlevel, paniclevel;
 } c_disk_t;
 
@@ -46,8 +57,7 @@ typedef struct c_mem_t {
 } c_mem_t;
 
 typedef struct c_proc_t {
-	char *procpattern;
-	pcre *procpcre;
+	exprlist_t *procexp;
 	int pmin, pmax, pcount;
 	int color;
 } c_proc_t;
@@ -55,14 +65,10 @@ typedef struct c_proc_t {
 typedef enum { C_LOAD, C_UPTIME, C_DISK, C_MEM, C_PROC } ruletype_t;
 
 typedef struct c_rule_t {
-	char *hostpattern;
-	pcre *hostpcre;
-	char *exhostpattern;
-	pcre *exhostpcre;
-	char *pagepattern;
-	pcre *pagepcre;
-	char *expagepattern;
-	pcre *expagepcre;
+	exprlist_t *hostexp;
+	exprlist_t *exhostexp;
+	exprlist_t *pageexp;
+	exprlist_t *expageexp;
 	char *timespec;
 	ruletype_t ruletype;
 	struct c_rule_t *next;
@@ -76,33 +82,294 @@ typedef struct c_rule_t {
 } c_rule_t;
 
 static c_rule_t *rulehead = NULL;
+static c_rule_t *ruletail = NULL;
 
-void load_client_config(char *fn)
+static exprlist_t *exprhead = NULL;
+
+static char *mytok(char *s, char *delims)
 {
+	static int atend = 0;
+	char *result = NULL;
+
+	if (s) atend = 0;
+	if (!atend) {
+		result = strtok(s, delims);
+		if (!result) atend = 1;
+	}
+
+	return result;
+}
+
+static exprlist_t *setup_expr(char *ptn)
+{
+	exprlist_t *newitem = (exprlist_t *)calloc(1, sizeof(exprlist_t));
+
+	newitem->pattern = strdup(ptn);
+	if (*ptn == '%') newitem->exp = compileregex(ptn+1);
+	newitem->next = exprhead;
+	exprhead = newitem;
+
+	return newitem;
+}
+
+static c_rule_t *setup_rule(ruletype_t ruletype, 
+			    exprlist_t *curhost, exprlist_t *curexhost, 
+			    exprlist_t *curpage, exprlist_t *curexpage, 
+			    char *curtime)
+{
+	c_rule_t *newitem = (c_rule_t *)calloc(1, sizeof(c_rule_t));
+	if (ruletail) { ruletail->next = newitem; ruletail = newitem; }
+	else rulehead = ruletail = newitem;
+
+	newitem->ruletype = ruletype;
+	newitem->hostexp = curhost;
+	newitem->exhostexp = curexhost;
+	newitem->pageexp = curpage;
+	newitem->expageexp = curexpage;
+	if (curtime) newitem->timespec = strdup(curtime);
+
+	return newitem;
+}
+
+
+int load_client_config(char *configfn)
+{
+	/* (Re)load the configuration file without leaking memory */
+	static time_t lastload = 0;     /* Last time the config file was loaded */
+	char fn[PATH_MAX];
+	struct stat st;
+	FILE *fd;
+	char *inbuf = NULL;
+	int inbufsz;
+	char *p, *tok;
+	exprlist_t *curhost, *curpage, *curexhost, *curexpage;
+	char *curtime;
+	c_rule_t *currule = NULL;
+
+	MEMDEFINE(fn);
+
+	if (configfn) strcpy(fn, configfn); else sprintf(fn, "%s/etc/clientrules.cfg", xgetenv("BBHOME"));
+	if (stat(fn, &st) == -1) {
+		errprintf("Cannot stat config file %s: %s\n", fn, strerror(errno)); 
+		MEMUNDEFINE(fn); 
+		return 0;
+	}
+
+	if (st.st_mtime == lastload) { MEMUNDEFINE(fn); return 0; }
+	lastload = st.st_mtime;
+
+	fd = fopen(fn, "r");
+	if (!fd) { 
+		errprintf("Cannot load config file %s: %s\n", fn, strerror(errno)); 
+		MEMUNDEFINE(fn); 
+		return 0;
+	}
+
+	/* First free the old list, if any */
+	while (rulehead) {
+		c_rule_t *tmp = rulehead;
+		rulehead = rulehead->next;
+		if (tmp->timespec) xfree(tmp->timespec);
+		xfree(tmp);
+	}
+	while (exprhead) {
+		exprlist_t *tmp = exprhead;
+		exprhead = exprhead->next;
+		if (tmp->pattern) xfree(tmp->pattern);
+		if (tmp->exp) pcre_free(tmp->exp);
+		xfree(tmp);
+	}
+
+	initfgets(fd);
+	curhost = curpage = curexhost = curexpage = NULL;
+	curtime = NULL;
+	while (unlimfgets(&inbuf, &inbufsz, fd)) {
+		exprlist_t *newhost, *newpage, *newexhost, *newexpage;
+		char *newtime;
+
+		grok_input(inbuf);
+		if (strlen(inbuf) == 0) continue;
+
+		newhost = newpage = newexhost = newexpage = NULL;
+		newtime = NULL;
+		currule = NULL;
+
+		tok = mytok(inbuf, " \t");
+		while (tok) {
+			if (strncasecmp(tok, "HOST=", 5) == 0) {
+				p = strchr(tok, '=');
+				newhost = setup_expr(p+1);
+				if (currule) currule->hostexp = newhost;
+			}
+			else if (strncasecmp(tok, "EXHOST=", 7) == 0) {
+				p = strchr(tok, '=');
+				newexhost = setup_expr(p+1);
+				if (currule) currule->exhostexp = newexhost;
+			}
+			else if (strncasecmp(tok, "PAGE=", 5) == 0) {
+				p = strchr(tok, '=');
+				newpage = setup_expr(p+1);
+				if (currule) currule->pageexp = newpage;
+			}
+			else if (strncasecmp(tok, "EXPAGE=", 7) == 0) {
+				p = strchr(tok, '=');
+				newexpage = setup_expr(p+1);
+				if (currule) currule->expageexp = newexpage;
+			}
+			else if (strncasecmp(tok, "TIME=", 5) == 0) {
+				p = strchr(tok, '=');
+				if (currule) currule->timespec = strdup(p+1);
+				else newtime = strdup(p+1);
+			}
+			else if (strcasecmp(tok, "UP") == 0) {
+				currule = setup_rule(C_UPTIME, curhost, curexhost, curpage, curexpage, curtime);
+				p = mytok(NULL, " \t"); 
+				currule->rule.uptime.recentlimit = (p ? 60*durationvalue(p): 3600);
+				p = mytok(NULL, " \t");
+				currule->rule.uptime.ancientlimit = (p ? 60*durationvalue(p): -1);
+			}
+			else if (strcasecmp(tok, "LOAD") == 0) {
+				currule = setup_rule(C_LOAD, curhost, curexhost, curpage, curexpage, curtime);
+				p = mytok(NULL, " \t"); 
+				currule->rule.load.warnlevel = (p ? atof(p) : 5.0);
+				p = mytok(NULL, " \t"); 
+				currule->rule.load.paniclevel = (p ? atof(p): 8.0);
+			}
+			else if (strcasecmp(tok, "DISK") == 0) {
+				currule = setup_rule(C_DISK, curhost, curexhost, curpage, curexpage, curtime);
+				p = mytok(NULL, " \t"); 
+				if (p) currule->rule.disk.fsexp = setup_expr(p);
+				p = mytok(NULL, " \t");
+				currule->rule.disk.warnlevel = (p ? atoi(p) : 90);
+				p = mytok(NULL, " \t");
+				currule->rule.disk.paniclevel = (p ? atoi(p): 95);
+				p = mytok(NULL, " \t");
+			}
+			else if ((strcasecmp(tok, "MEMREAL") == 0) || (strcasecmp(tok, "MEM") == 0)) {
+				currule = setup_rule(C_MEM, curhost, curexhost, curpage, curexpage, curtime);
+				currule->rule.mem.memtype = C_MEM_PHYS;
+				p = mytok(NULL, " \t");
+				currule->rule.mem.warnlevel = (p ? atoi(p) : 100);
+				p = mytok(NULL, " \t");
+				currule->rule.mem.paniclevel = (p ? atoi(p) : 101);
+			}
+			else if ((strcasecmp(tok, "MEMSWAP") == 0) || (strcasecmp(tok, "SWAP") == 0)) {
+				currule = setup_rule(C_MEM, curhost, curexhost, curpage, curexpage, curtime);
+				currule->rule.mem.memtype = C_MEM_SWAP;
+				p = mytok(NULL, " \t");
+				currule->rule.mem.warnlevel = (p ? atoi(p) : 50);
+				p = mytok(NULL, " \t");
+				currule->rule.mem.paniclevel = (p ? atoi(p) : 80);
+			}
+			else if ((strcasecmp(tok, "MEMACT") == 0) || (strcasecmp(tok, "ACTUAL") == 0)) {
+				currule = setup_rule(C_MEM, curhost, curexhost, curpage, curexpage, curtime);
+				currule->rule.mem.memtype = C_MEM_ACT;
+				p = mytok(NULL, " \t");
+				currule->rule.mem.warnlevel = (p ? atoi(p) : 90);
+				p = mytok(NULL, " \t");
+				currule->rule.mem.paniclevel = (p ? atoi(p) : 97);
+			}
+			else if (strcasecmp(tok, "PROC") == 0) {
+				currule = setup_rule(C_PROC, curhost, curexhost, curpage, curexpage, curtime);
+				p = mytok(NULL, " \t"); 
+				if (p) currule->rule.proc.procexp = setup_expr(p);
+				p = mytok(NULL, " \t");
+				currule->rule.proc.pmin = (p ? atoi(p) : 0);
+				p = mytok(NULL, " \t");
+				currule->rule.proc.pmax = (p ? atoi(p) : -1);
+				p = mytok(NULL, " \t");
+				currule->rule.proc.color = (p ? parse_color(p) : COL_RED);
+			}
+
+			tok = mytok(NULL, " \t");
+		}
+
+		if (!currule) {
+			/* No rules on this line - its the new set of defaults */
+			curhost = newhost;
+			curpage = newpage;
+			curexhost = newexhost;
+			curexpage = newexpage;
+			if (curtime) xfree(curtime); curtime = newtime;
+		}
+	}
+
+	fclose(fd);
+	if (inbuf) xfree(inbuf);
+	if (curtime) xfree(curtime);
+
+	MEMUNDEFINE(fn);
+	return 1;
+}
+
+void dump_client_config(void)
+{
+	c_rule_t *rwalk;
+
+	for (rwalk = rulehead; (rwalk); rwalk = rwalk->next) {
+		switch (rwalk->ruletype) {
+		  case C_UPTIME:
+			printf("UP %d %d", rwalk->rule.uptime.recentlimit, rwalk->rule.uptime.ancientlimit);
+			break;
+		  case C_LOAD:
+			printf("LOAD %.2f %.2f", rwalk->rule.load.warnlevel, rwalk->rule.load.paniclevel);
+			break;
+		  case C_DISK:
+			printf("DISK %s %d %d", rwalk->rule.disk.fsexp->pattern,
+			       rwalk->rule.disk.warnlevel, rwalk->rule.disk.paniclevel);
+			break;
+		  case C_MEM:
+			switch (rwalk->rule.mem.memtype) {
+			  case C_MEM_PHYS: printf("MEMREAL"); break;
+			  case C_MEM_SWAP: printf("MEMSWAP"); break;
+			  case C_MEM_ACT: printf("MEMACT"); break;
+			}
+			printf(" %d %d", rwalk->rule.mem.warnlevel, rwalk->rule.mem.paniclevel);
+			break;
+		  case C_PROC:
+			printf("PROC %s %d %d", rwalk->rule.proc.procexp->pattern,
+			       rwalk->rule.proc.pmin, rwalk->rule.proc.pmax);
+			break;
+		}
+
+		if (rwalk->timespec) printf(" TIME=%s", rwalk->timespec);
+		if (rwalk->hostexp) printf(" HOST=%s", rwalk->hostexp->pattern);
+		if (rwalk->exhostexp) printf(" EXHOST=%s", rwalk->exhostexp->pattern);
+		if (rwalk->pageexp) printf(" HOST=%s", rwalk->pageexp->pattern);
+		if (rwalk->expageexp) printf(" EXHOST=%s", rwalk->expageexp->pattern);
+		printf("\n");
+	}
 }
 
 static c_rule_t *getrule(char *hostname, char *pagename, ruletype_t ruletype)
 {
 	static c_rule_t *rwalk = NULL;
-	int found;
+	static char *ahost = NULL, *apage = NULL;
 
-	if (hostname || pagename) rwalk = rulehead; else rwalk = rwalk->next;
+	if (hostname || pagename) {
+		rwalk = rulehead; 
+		ahost = hostname;
+		apage = pagename;
+	}
+	else {
+		rwalk = rwalk->next;
+	}
 
-	for (found = 0; (rwalk && !found); rwalk = rwalk->next) {
+	for (; (rwalk); rwalk = rwalk->next) {
 		if (rwalk->ruletype != ruletype) continue;
 		if (rwalk->timespec && !timematch(rwalk->timespec)) continue;
 
-		if (rwalk->exhostpattern && namematch(hostname, rwalk->exhostpattern, rwalk->exhostpcre)) continue;
-		if (rwalk->hostpattern && !namematch(hostname, rwalk->hostpattern, rwalk->hostpcre)) continue;
+		if (rwalk->exhostexp && namematch(ahost, rwalk->exhostexp->pattern, rwalk->exhostexp->exp)) continue;
+		if (rwalk->hostexp && !namematch(ahost, rwalk->hostexp->pattern, rwalk->hostexp->exp)) continue;
 
-		if (rwalk->expagepattern && namematch(pagename, rwalk->expagepattern, rwalk->expagepcre)) continue;
-		if (rwalk->pagepattern && !namematch(pagename, rwalk->pagepattern, rwalk->pagepcre)) continue;
+		if (rwalk->expageexp && namematch(apage, rwalk->expageexp->pattern, rwalk->expageexp->exp)) continue;
+		if (rwalk->pageexp && !namematch(apage, rwalk->pageexp->pattern, rwalk->pageexp->exp)) continue;
 
 		/* If we get here, then we have something that matches */
-		found = 1;
+		return rwalk;
 	}
 
-	return rwalk;
+	return NULL;
 }
 
 void get_cpu_thresholds(namelist_t *hinfo, float *loadyellow, float *loadred, int *recentlimit, int *ancientlimit)
@@ -146,7 +413,7 @@ void get_disk_thresholds(namelist_t *hinfo, char *fsname, int *warnlevel, int *p
 	*paniclevel = 95;
 
 	rule = getrule(hostname, pagename, C_DISK);
-	while (rule && !namematch(fsname, rule->rule.disk.fspattern, rule->rule.disk.fspcre)) {
+	while (rule && !namematch(fsname, rule->rule.disk.fsexp->pattern, rule->rule.disk.fsexp->exp)) {
 		rule = getrule(NULL, NULL, C_DISK);
 	}
 
@@ -237,7 +504,7 @@ void add_process_count(char *pname)
 	mon_proc_t *pwalk;
 
 	for (pwalk = phead; (pwalk); pwalk = pwalk->next) {
-		if (namematch(pname, pwalk->rule->rule.proc.procpattern, pwalk->rule->rule.proc.procpcre)) pwalk->rule->rule.proc.pcount++;
+		if (namematch(pname, pwalk->rule->rule.proc.procexp->pattern, pwalk->rule->rule.proc.procexp->exp)) pwalk->rule->rule.proc.pcount++;
 	}
 }
 
@@ -247,7 +514,7 @@ char *check_process_count(int *pcount, int *lowlim, int *uplim, int *pcolor)
 
 	if (pmonwalk == NULL) return NULL;
 
-	result = pmonwalk->rule->rule.proc.procpattern;
+	result = pmonwalk->rule->rule.proc.procexp->pattern;
 	*pcount = pmonwalk->rule->rule.proc.pcount;
 	*lowlim = pmonwalk->rule->rule.proc.pmin;
 	*uplim = pmonwalk->rule->rule.proc.pmax;
