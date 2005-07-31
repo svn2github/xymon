2bf3e74f95c2efebc7bd69e01c24bc4ec68a80e7
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-07-31 07:47:52 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3608 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/client/hobbitclient-osf1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#----------------------------------------------------------------------------#
+# OSF1 client for Hobbit                                                     #
+#                                                                            #
+# Copyright (C) 2005 Henrik Storner <henrik@hswn.dk>                         #
+#                                                                            #
+# This program is released under the GNU General Public License (GPL),       #
+# version 2. See the file "COPYING" for details.                             #
+#                                                                            #
+#----------------------------------------------------------------------------#
+#
+# $Id: hobbitclient-osf1.sh,v 1.1 2005-07-31 07:47:52 henrik Exp $
+
+echo "[date]"
+date
+echo "[uname]"
+uname -a
+echo "[uptime]"
+uptime
+echo "[who]"
+who
+echo "[df]"
+df
+echo "[netstat]"
+netstat -s
+echo "[ps]"
+ps -ef
+echo "[top]"
+top -b -n 1 
+# vmstat
+nohup sh -c "vmstat 300 2 1>$BBTMP/hobbit_vmstat.$$ 2>&1; mv $BBTMP/hobbit_vmstat.$$ $BBTMP/hobbit_vmstat" </dev/null >/dev/null 2>&1 &
+sleep 5
+if test -f $BBTMP/hobbit_vmstat; then echo "[vmstat]"; cat $BBTMP/hobbit_vmstat; rm -f $BBTMP/hobbit_vmstat; fi
+
+exit
+
