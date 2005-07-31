0fdd812800173e946ded7ef1a1b8e1cb704d749e
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-07-31 19:52:41 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3622 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/build/genconfig.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Simpler than autoconf, but it does what we need it to do right now.
+
+echo "/* This file is auto-generated */" >include/config.h
+
+echo "Checking for socklen_t"
+$MAKE -C build test-socklent 1>/dev/null 2>&1
+if test $? -eq 0; then
+	echo "#define HAVE_SOCKLEN_T 1" >>include/config.h
+else
+	echo "#undef HAVE_SOCKLEN_T" >>include/config.h
+fi
+
+echo "Checking for snprintf"
+$MAKE -C build test-snprintf 1>/dev/null 2>&1
+if test $? -eq 0; then
+	echo "#define HAVE_SNPRINTF 1" >>include/config.h
+else
+	echo "#undef HAVE_SNPRINTF" >>include/config.h
+fi
+
+echo "Checking for vsnprintf"
+$MAKE -C build test-vsnprintf 1>/dev/null 2>&1
+if test $? -eq 0; then
+	echo "#define HAVE_VSNPRINTF 1" >>include/config.h
+else
+	echo "#undef HAVE_VSNPRINTF" >>include/config.h
+fi
+
+echo "Checking for rpc/rpcent.h"
+$MAKE -C build test-rpcenth 1>/dev/null 2>&1
+if test $? -eq 0; then
+	echo "#define HAVE_RPCENT_H 1" >>include/config.h
+else
+	echo "#undef HAVE_RPCENT_H" >>include/config.h
+fi
+
+echo "config.h created"
+exit 0
+
