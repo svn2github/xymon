eff0f27cd2964cd26c92b39bdf029aa67e44f9a9
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-07-31 12:36:20 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3613 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/hobbitd/client/osf.c
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit message daemon.                                                     */
+/*                                                                            */
+/* Client backend module for OSF                                              */
+/*                                                                            */
+/* Copyright (C) 2005 Henrik Storner <henrik@hswn.dk>                         */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char osf_rcsid[] = "$Id: osf.c,v 1.1 2005-07-31 12:36:20 henrik Exp $";
+
+void handle_osf_client(char *hostname, enum ostype_t os, namelist_t *hinfo, char *sender, time_t timestamp, char *clientdata)
+{
+	char *timestr;
+	char *uptimestr;
+	char *whostr;
+	char *psstr;
+	char *topstr;
+	char *dfstr;
+	char *msgsstr;
+	char *netstatstr;
+	char *vmstatstr;
+
+	char fromline[1024];
+
+	sprintf(fromline, "\nStatus message received from %s\n", sender);
+
+	splitmsg(clientdata);
+
+	timestr = getdata("date");
+	uptimestr = getdata("uptime");
+	whostr = getdata("who");
+	psstr = getdata("ps");
+	topstr = getdata("top");
+	dfstr = getdata("df");
+	msgsstr = getdata("msgs");
+	netstatstr = getdata("netstat");
+	vmstatstr = getdata("vmstat");
+
+	combo_start();
+
+	unix_cpu_report(hostname, hinfo, fromline, timestr, uptimestr, whostr, psstr, topstr);
+	unix_disk_report(hostname, hinfo, fromline, timestr, "Capacity", "Mounted", dfstr);
+	unix_procs_report(hostname, hinfo, fromline, timestr, "CMD", psstr);
+	msgs_report(hostname, hinfo, fromline, timestr, msgsstr);
+
+	combo_end();
+
+	unix_netstat_report(hostname, hinfo, "osf", netstatstr);
+	unix_vmstat_report(hostname, hinfo, "osf", vmstatstr);
+}
+
