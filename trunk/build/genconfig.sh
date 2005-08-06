f6840448e998d11838b22e25bd837dee1335a336
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-08-06 20:59:38 +0000

    Workaround a broken compiler on OSF/1 that has defs for snprintf without those functions actually existing.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3671 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/genconfig.sh
+++ trunk/build/genconfig.sh
@@ -17,7 +17,12 @@ fi
 echo "Checking for snprintf"
 $CC -c -o build/testfile.o $CFLAGS build/test-snprintf.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_SNPRINTF 1" >>include/config.h
+	$CC -o build/testfile $CFLAGS build/testfile.o 1>/dev/null 2>&1
+	if test $? -eq 0; then
+		echo "#define HAVE_SNPRINTF 1" >>include/config.h
+	else
+		echo "#undef HAVE_SNPRINTF" >>include/config.h
+	fi
 else
 	echo "#undef HAVE_SNPRINTF" >>include/config.h
 fi
@@ -25,7 +30,12 @@ fi
 echo "Checking for vsnprintf"
 $CC -c -o build/testfile.o $CFLAGS build/test-vsnprintf.c 1>/dev/null 2>&1
 if test $? -eq 0; then
-	echo "#define HAVE_VSNPRINTF 1" >>include/config.h
+	$CC -o build/testfile $CFLAGS build/testfile.o 1>/dev/null 2>&1
+	if test $? -eq 0; then
+		echo "#define HAVE_VSNPRINTF 1" >>include/config.h
+	else
+		echo "#undef HAVE_VSNPRINTF" >>include/config.h
+	fi
 else
 	echo "#undef HAVE_VSNPRINTF" >>include/config.h
 fi
@@ -49,5 +59,7 @@ fi
 echo "#endif" >>include/config.h
 
 echo "config.h created"
+rm -f testfile.o testfile
+
 exit 0
 
