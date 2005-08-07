2d1bf3cae28251347c4d8de4fa49adbea77bad6c
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-08-07 20:41:04 +0000

    Fix "df" command. Drop our meminfo tool, and use the native vm_stat command. Fix top to only log one sample. There's no vmstat command on Darwin.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3682 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/hobbitclient-darwin.sh
+++ trunk/client/hobbitclient-darwin.sh
@@ -10,7 +10,7 @@
 #                                                                            #
 #----------------------------------------------------------------------------#
 #
-# $Id: hobbitclient-darwin.sh,v 1.3 2005-07-24 11:32:51 henrik Exp $
+# $Id: hobbitclient-darwin.sh,v 1.4 2005-08-07 20:41:04 henrik Exp $
 
 echo "[date]"
 date
@@ -21,21 +21,15 @@ uptime
 echo "[who]"
 who
 echo "[df]"
-df -H -tnonfs,nullfs,cd9660,procfs
+df -H -T nonfs,nullfs,cd9660,procfs
 echo "[meminfo]"
-$BBHOME/bin/darwin-meminfo
-echo "[swapinfo]"
-echo "Swap data not supported on Darwin"
+vm_stat
 echo "[netstat]"
 netstat -s
 echo "[ps]"
 ps -axw
 echo "[top]"
-top -n 20
-# vmstat
-nohup sh -c "vmstat 300 2 1>$BBTMP/hobbit_vmstat.$$ 2>&1; mv $BBTMP/hobbit_vmstat.$$ $BBTMP/hobbit_vmstat" </dev/null >/dev/null 2>&1 &
-sleep 5
-if test -f $BBTMP/hobbit_vmstat; then echo "[vmstat]"; cat $BBTMP/hobbit_vmstat; rm -f $BBTMP/hobbit_vmstat; fi
+top -l 1 -n 20
 
 exit
 
