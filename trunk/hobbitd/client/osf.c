ab54c73685925cbde677e9dd2981e8852e74789d
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-08-07 20:10:32 +0000

    Memory- and swap-reports added for a memory status
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3680 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/client/osf.c
+++ trunk/hobbitd/client/osf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char osf_rcsid[] = "$Id: osf.c,v 1.2 2005-08-01 05:57:34 henrik Exp $";
+static char osf_rcsid[] = "$Id: osf.c,v 1.3 2005-08-07 20:10:32 henrik Exp $";
 
 void handle_osf_client(char *hostname, enum ostype_t os, namelist_t *hinfo, char *sender, time_t timestamp, char *clientdata)
 {
@@ -23,6 +23,8 @@ void handle_osf_client(char *hostname, enum ostype_t os, namelist_t *hinfo, char
 	char *msgsstr;
 	char *netstatstr;
 	char *vmstatstr;
+	char *memorystr;
+	char *swapstr;
 
 	char fromline[1024];
 
@@ -39,6 +41,8 @@ void handle_osf_client(char *hostname, enum ostype_t os, namelist_t *hinfo, char
 	msgsstr = getdata("msgs");
 	netstatstr = getdata("netstat");
 	vmstatstr = getdata("vmstat");
+	memorystr = getdata("memory");
+	swapstr = getdata("swap");
 
 	combo_start();
 
@@ -46,6 +50,70 @@ void handle_osf_client(char *hostname, enum ostype_t os, namelist_t *hinfo, char
 	unix_disk_report(hostname, hinfo, fromline, timestr, "Capacity", "Mounted", dfstr);
 	unix_procs_report(hostname, hinfo, fromline, timestr, "CMD", NULL, psstr);
 	msgs_report(hostname, hinfo, fromline, timestr, msgsstr);
+
+	if (memorystr && swapstr) {
+		char *p, *bol, *eoln;
+		unsigned long phystotal, physfree, swaptotal, swapfree, pagecnt, pagesize;
+
+		/*
+		 * Total Physical Memory =  5120.00 M
+		 *	              =   655360 pages
+		 *
+		 * ...
+		 *
+		 * Managed Pages Break Down:
+		 *
+		 *        free pages = 499488
+		 *
+		 */
+
+		phystotal = physfree = swaptotal = swapfree = -1;
+		pagesize = 8; /* Default - appears to be the OSF/1 standard */
+
+		bol = strstr(memorystr, "\nTotal Physical Memory =");
+		if (bol) {
+			p = strchr(bol, '=');
+			phystotal = atol(p+1);
+			bol = strchr(p, '\n');
+			if (bol) {
+				bol++;
+				bol += strspn(bol, " \t");
+				if (*bol == '=') {
+					pagecnt = atol(bol+1);
+					pagesize = (phystotal * 1024) / pagecnt;
+				}
+			}
+		}
+
+		bol = strstr(memorystr, "\nManaged Pages Break Down:");
+		if (bol) {
+			bol = strstr(bol, "free pages =");
+			if (bol) {
+				p = strchr(bol, '=');
+				physfree = atol(p+1) * pagesize / 1024;
+			}
+		}
+
+		bol = strstr(swapstr, "\nTotal swap allocation:");
+		if (bol) {
+			unsigned long swappages, freepages;
+			int n1, n2;
+
+			n1 = n2 = 0;
+			p = strstr(bol, "Allocated space:");
+			if (p) n1 = sscanf(p, "Allocated space: %ld pages", &swappages);
+			p = strstr(bol, "Available space:");
+			if (p) n2 = sscanf(p, "Available space: %ld pages", &freepages);
+			if ((n1 == 1) && (n2 == 1)) {
+				swaptotal = swappages * pagesize / 1024;
+				swapfree = freepages  * pagesize / 1024;
+			}
+		}
+
+		unix_memory_report(hostname, hinfo, fromline, timestr,
+				   phystotal, (phystotal - physfree), -1, 
+				   swaptotal, (swaptotal - swapfree));
+	}
 
 	combo_end();
 
