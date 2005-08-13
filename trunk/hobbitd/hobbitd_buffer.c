c7e020a14fac531bf646ca09f7e43c527b61adec
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-08-13 16:04:05 +0000

    The default for the STACHG and PAGE channels should be whatever the STATUS channel is. Changed CLIENT default to 512, and ENADIS to 32.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3728 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd_buffer.c
+++ trunk/hobbitd/hobbitd_buffer.c
@@ -11,7 +11,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: hobbitd_buffer.c,v 1.1 2005-08-13 15:38:43 henrik Exp $";
+static char rcsid[] = "$Id: hobbitd_buffer.c,v 1.2 2005-08-13 16:04:05 henrik Exp $";
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -27,17 +27,17 @@ unsigned int shbufsz(enum msgchannels_t chnid)
 	if (chnid != C_LAST) {
 		switch (chnid) {
 		  case C_STATUS: v = getenv("MAXMSG_STATUS"); defvalue = 256; break;
-		  case C_STACHG: v = getenv("MAXMSG_STACHG"); defvalue = 256; break;
-		  case C_PAGE:   v = getenv("MAXMSG_PAGE");   defvalue = 256; break;
+		  case C_CLIENT: v = getenv("MAXMSG_CLIENT"); defvalue = 512; break;
 		  case C_DATA:   v = getenv("MAXMSG_DATA");   defvalue = 256; break;
 		  case C_NOTES:  v = getenv("MAXMSG_NOTES");  defvalue = 256; break;
-		  case C_ENADIS: v = getenv("MAXMSG_ENADIS"); defvalue = 256; break;
-		  case C_CLIENT: v = getenv("MAXMSG_CLIENT"); defvalue = 1024; break;
+		  case C_STACHG: v = getenv("MAXMSG_STACHG"); defvalue = shbufsz(C_STATUS); break;
+		  case C_PAGE:   v = getenv("MAXMSG_PAGE");   defvalue = shbufsz(C_STATUS); break;
+		  case C_ENADIS: v = getenv("MAXMSG_ENADIS"); defvalue =  32; break;
 		  default: break;
 		}
 
 		if (v) result = atoi(v);
-		if (result < 128) result = defvalue;
+		if (result < 32) result = defvalue;
 	}
 	else {
 		enum msgchannels_t i;
