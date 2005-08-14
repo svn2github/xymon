49bef72f394c2241c2c28ed252eb3b1b1b9b62ee
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-08-14 09:52:11 +0000

    More files for the client package. Sign all RPMS
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3735 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/makerpm.sh
+++ trunk/build/makerpm.sh
@@ -26,20 +26,22 @@ EOF2
 
 cat rpm/hobbit.spec | sed -e "s/@VER@/$REL/g" >rpmbuild/SPECS/hobbit.spec
 cp rpm/hobbit-init.d rpmbuild/SOURCES/
+cp rpm/hobbitclient-init.d rpmbuild/SOURCES/
 cp rpm/hobbit.logrotate rpmbuild/SOURCES/
+cp rpm/hobbitclient-default rpmbuild/SOURCES/
 
 mkdir -p rpmbuild/hobbit-$REL
 for f in bbdisplay bbnet bbpatches bbproxy build common contrib docs hobbitd include lib client
 do
         find $f/ | grep -v RCS | cpio -pdvmu ~/hobbit/rpmbuild/hobbit-$REL/
 done
-cp -p Changes configure configure.server configure.client COPYING CREDITS README ~/hobbit/rpmbuild/hobbit-$REL/
+cp -p Changes configure configure.server configure.client COPYING CREDITS README README.CLIENT ~/hobbit/rpmbuild/hobbit-$REL/
 find ~/hobbit/rpmbuild/hobbit-$REL -type d|xargs chmod 755
 
 cd rpmbuild
 tar zcf SOURCES/hobbit-$REL.tar.gz hobbit-$REL
 rm -rf hobbit-$REL
 HOME=`pwd` rpmbuild -ba --clean SPECS/hobbit.spec
-rpm --addsign RPMS/i?86/hobbit-$REL-*.i?86.rpm
-mv RPMS/i?86/hobbit-$REL-*.i?86.rpm SRPMS/hobbit-$REL-*.src.rpm ../rpm/pkg/
+rpm --addsign RPMS/i?86/hobbit-$REL-*.i?86.rpm RPMS/i386/hobbit-client-$REL-*.i?86.rpm SRPMS/hobbit-$REL-*.src.rpm
+mv RPMS/i?86/hobbit-$REL-*.i?86.rpm RPMS/i?86/hobbit-client-$REL-*.i?86.rpm SRPMS/hobbit-$REL-*.src.rpm ../rpm/pkg/
 
