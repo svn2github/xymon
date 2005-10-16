ca50db09e80d5aa018c4913c8abebbdac4535386
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-10-16 07:32:55 +0000

    Support running a locally configured client
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3862 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/hobbitclient.sh
+++ trunk/client/hobbitclient.sh
@@ -12,7 +12,7 @@
 #                                                                            #
 #----------------------------------------------------------------------------#
 #
-# $Id: hobbitclient.sh,v 1.5 2005-09-28 21:21:17 henrik Exp $
+# $Id: hobbitclient.sh,v 1.6 2005-10-16 07:32:55 henrik Exp $
 
 # Must make sure the commands return standard (english) texts.
 LANG=C
@@ -20,13 +20,35 @@ LC_ALL=C
 LC_MESSAGES=C
 export LANG LC_ALL LC_MESSAGES
 
+LOCALMODE="no"
+if test $# -ge 1; then
+	if test "$1" = "--local"; then
+		LOCALMODE="yes"
+	fi
+	shift
+fi
+
 if test "$BBOSSCRIPT" = ""; then
 	BBOSSCRIPT="hobbitclient-`uname -s | tr '[A-Z]' '[a-z]'`.sh"
 fi
 
-echo "client $MACHINE.$BBOSTYPE"      >  $BBTMP/msg.txt
-$BBHOME/bin/$BBOSSCRIPT >> $BBTMP/msg.txt
-$BB $BBDISP "@" < $BBTMP/msg.txt
+TEMPFILE="$BBTMP/msg.txt"
+rm -f $TEMPFILE
+touch $TEMPFILE
+
+if test "$LOCALMODE" = "yes"; then
+	echo "@@client#1|0|127.0.0.1|$MACHINEDOTS|$BBOSTYPE" >> $TEMPFILE
+fi
+
+echo "client $MACHINE.$BBOSTYPE"  >>  $TEMPFILE
+$BBHOME/bin/$BBOSSCRIPT >> $TEMPFILE
+
+if test "$LOCALMODE" = "yes"; then
+	echo "@@" >> $TEMPFILE
+	$BBHOME/bin/hobbitd_client --local --config=$BBHOME/etc/localclient.cfg <$TEMPFILE
+else
+	$BB $BBDISP "@" < $TEMPFILE
+fi
 
 exit 0
 
