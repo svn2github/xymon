bede2f383b679c78cf7b4962fc4fc9bd0989ae8a
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2005-11-09 12:29:58 +0000

    Revert previous change.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3908 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/headfoot.h
+++ trunk/lib/headfoot.h
@@ -13,7 +13,6 @@
 
 #include <time.h>
 
-extern int headfoot_unknowns;
 extern void sethostenv(char *host, char *ip, char *svc, char *color, char *hikey);
 extern void sethostenv_report(time_t reportstart, time_t reportend, double repwarn, double reppanic);
 extern void sethostenv_snapshot(time_t snapshot);
