c1e1a4ab27f1dd5ded6571ecfb502b067e0caa78
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-01-13 12:04:05 +0000

    Moved to library.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@3998 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/reportlog.h
+++ trunk/lib/reportlog.h
@@ -1,15 +1,18 @@
 /*----------------------------------------------------------------------------*/
-/* Hobbit report-mode statuslog viewer.                                       */
+/* Hobbit monitor library.                                                    */
 /*                                                                            */
-/* Copyright (C) 2002-2005 Henrik Storner <henrik@storner.dk>                 */
+/* Copyright (C) 2002-2006 Henrik Storner <henrik@storner.dk>                 */
 /*                                                                            */
 /* This program is released under the GNU General Public License (GPL),       */
 /* version 2. See the file "COPYING" for details.                             */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-#ifndef __BB_REPLOG_H__
-#define __BB_REPLOG_H__
+#ifndef __REPORTLOG_H__
+#define __REPORTLOG_H__
+
+#include <stdio.h>
+#include "availability.h"
 
 #define STYLE_CRIT 0
 #define STYLE_NONGR 1
