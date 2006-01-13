2c66bad9ca5f9ad35385a2a6ab9b9ad3420b1613
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-01-13 12:51:35 +0000

    Split from bbdisplay into a pure CGI
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4010 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/web/bb-eventlog.c
+++ trunk/web/bb-eventlog.c
@@ -13,7 +13,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: bb-eventlog.c,v 1.26 2005-08-09 08:39:25 henrik Exp $";
+static char rcsid[] = "$Id: bb-eventlog.c,v 1.27 2006-01-13 12:51:35 henrik Exp $";
 
 #include <limits.h>
 #include <stdio.h>
@@ -28,314 +28,7 @@ static char rcsid[] = "$Id: bb-eventlog.c,v 1.26 2005-08-09 08:39:25 henrik Exp 
 #include <errno.h>
 #include <time.h>
 
-#include <pcre.h>
-
-#include "bbgen.h"
-#include "loadbbhosts.h"
-#include "util.h"
-#include "eventlog.h"
-
-
-char *eventignorecolumns = NULL;
-int havedoneeventlog = 0;
-
-static int wanted_eventcolumn(char *service)
-{
-	char svc[100];
-	int result;
-
-	if (!eventignorecolumns || (strlen(service) > (sizeof(svc)-3))) return 1;
-
-	sprintf(svc, ",%s,", service);
-	result = (strstr(eventignorecolumns, svc) == NULL);
-
-	return result;
-}
-
-time_t convert_time(char *timestamp)
-{
-	time_t event = 0;
-	unsigned int year,month,day,hour,min,sec,count;
-	struct tm timeinfo;
-
-	count = sscanf(timestamp, "%d/%d/%d@%d:%d:%d",
-		&year, &month, &day, &hour, &min, &sec);
-	if(count != 6) {
-		return -1;
-	}
-	if(year < 1970) {
-		return 0;
-	}
-	else {
-		memset(&timeinfo, 0, sizeof(timeinfo));
-		timeinfo.tm_year  = year - 1900;
-		timeinfo.tm_mon   = month - 1;
-		timeinfo.tm_mday  = day;
-		timeinfo.tm_hour  = hour;
-		timeinfo.tm_min   = min;
-		timeinfo.tm_sec   = sec;
-		timeinfo.tm_isdst = -1;
-		event = mktime(&timeinfo);		
-	}
-
-	return event;
-}
-
-
-void do_eventlog(FILE *output, int maxcount, int maxminutes, char *fromtime,
-		 char *totime, char *hostregex, char *testregex, char *colrregex,
-		 int ignoredialups)
-{
-	FILE *eventlog;
-	char eventlogfilename[PATH_MAX];
-	time_t firstevent = 0;
-	time_t lastevent = time(NULL);
-	event_t	*eventhead, *walk;
-	struct stat st;
-	char l[MAX_LINE_LEN];
-	char title[200];
-	namelist_t *hinfo;
-
-	/* For the PCRE matching */
-	const char *errmsg = NULL;
-	int errofs = 0;
-	pcre *hostregexp = NULL;
-	pcre *testregexp = NULL;
-	pcre *colrregexp = NULL;
-
-	havedoneeventlog = 1;
-
-	if (maxminutes && (fromtime || totime)) {
-		fprintf(output, "<B>Only one time interval type is allowed!</B>");
-		return;
-	}
-
-	if (fromtime) {
-		firstevent = convert_time(fromtime);
-		if(firstevent < 0) {
-			fprintf(output,"<B>Invalid 'from' time: %s</B>", fromtime);
-			return;
-		}
-	}
-	else if (maxminutes) {
-		firstevent = time(NULL) - maxminutes*60;
-	}
-	else {
-		firstevent = time(NULL) - 86400;
-	}
-
-	if (totime) {
-		lastevent = convert_time(totime);
-		if (lastevent < 0) {
-			fprintf(output,"<B>Invalid 'to' time: %s</B>", totime);
-			return;
-		}
-		if (lastevent < firstevent) {
-			fprintf(output,"<B>'to' time must be after 'from' time.</B>");
-			return;
-		}
-	}
-
-	if (!maxcount) maxcount = 100;
-
-	if (hostregex) hostregexp = pcre_compile(hostregex, PCRE_CASELESS, &errmsg, &errofs, NULL);
-	if (testregex) testregexp = pcre_compile(testregex, PCRE_CASELESS, &errmsg, &errofs, NULL);
-	if (colrregex) colrregexp = pcre_compile(colrregex, PCRE_CASELESS, &errmsg, &errofs, NULL);
-
-	sprintf(eventlogfilename, "%s/allevents", xgetenv("BBHIST"));
-	eventlog = fopen(eventlogfilename, "r");
-	if (!eventlog) {
-		errprintf("Cannot open eventlog\n");
-		return;
-	}
-
-	if (stat(eventlogfilename, &st) == 0) {
-		time_t curtime;
-		int done = 0;
-
-		/* Find a spot in the eventlog file close to where the firstevent time is */
-		fseek(eventlog, 0, SEEK_END);
-		do {
-			/* Go back maxcount*80 bytes - one entry is ~80 bytes */
-			if (ftell(eventlog) > maxcount*80) {
-				unsigned int uicurtime;
-				fseek(eventlog, -maxcount*80, SEEK_CUR); 
-				fgets(l, sizeof(l), eventlog); /* Skip to start of line */
-				fgets(l, sizeof(l), eventlog);
-				sscanf(l, "%*s %*s %u %*u %*u %*s %*s %*d", &uicurtime);
-				curtime = uicurtime;
-				done = (curtime < firstevent);
-			}
-			else {
-				rewind(eventlog);
-				done = 1;
-			}
-		} while (!done);
-	}
-	
-	eventhead = NULL;
-
-	while (fgets(l, sizeof(l), eventlog)) {
-
-		time_t eventtime, changetime, duration;
-		unsigned int uievt, uicht, uidur;
-		char hostname[MAX_LINE_LEN], svcname[MAX_LINE_LEN], newcol[MAX_LINE_LEN], oldcol[MAX_LINE_LEN];
-		char *newcolname, *oldcolname;
-		int state, itemsfound, hostmatch, testmatch, colrmatch;
-		event_t *newevent;
-		struct host_t *eventhost;
-		struct bbgen_col_t *eventcolumn;
-		int ovector[30];
-
-		itemsfound = sscanf(l, "%s %s %u %u %u %s %s %d",
-			hostname, svcname,
-			&uievt, &uicht, &uidur, 
-			newcol, oldcol, &state);
-		eventtime = uievt; changetime = uicht; duration = uidur;
-		oldcolname = colorname(eventcolor(oldcol));
-		newcolname = colorname(eventcolor(newcol));
-		if (eventtime > lastevent) break;
-		eventhost = find_host(hostname);
-		eventcolumn = find_or_create_column(svcname, 1);
-
-		if ( (itemsfound == 8) && 
-		     (eventtime > firstevent) && 
-		     (eventhost && !eventhost->nobb2) && 
-		     (wanted_eventcolumn(svcname)) ) {
-			if (hostregexp)
-				hostmatch = (pcre_exec(hostregexp, NULL, hostname, strlen(hostname), 0, 0, 
-						ovector, (sizeof(ovector)/sizeof(int))) >= 0);
-			else
-				hostmatch = 1;
-			if (!hostmatch) continue;
-
-			if (testregexp)
-				testmatch = (pcre_exec(testregexp, NULL, svcname, strlen(svcname), 0, 0, 
-						ovector, (sizeof(ovector)/sizeof(int))) >= 0);
-			else
-				testmatch = 1;
-			if (!testmatch) continue;
-
-			if (colrregexp) {
-				colrmatch = ( (pcre_exec(colrregexp, NULL, newcolname, strlen(newcolname), 0, 0,
-							ovector, (sizeof(ovector)/sizeof(int))) >= 0) ||
-					      (pcre_exec(colrregexp, NULL, oldcolname, strlen(oldcolname), 0, 0,
-							ovector, (sizeof(ovector)/sizeof(int))) >= 0) );
-			}
-			else
-				colrmatch = 1;
-			if (!colrmatch) continue;
-
-			if (ignoredialups) {
-				hinfo = hostinfo(hostname);
-				if (bbh_item(hinfo, BBH_FLAG_DIALUP)) continue;
-			}
-
-			newevent = (event_t *) malloc(sizeof(event_t));
-			newevent->host       = eventhost;
-			newevent->service    = eventcolumn;
-			newevent->eventtime  = eventtime;
-			newevent->changetime = changetime;
-			newevent->duration   = duration;
-			newevent->newcolor   = eventcolor(newcol);
-			newevent->oldcolor   = eventcolor(oldcol);
-			newevent->next = eventhead;
-			eventhead = newevent;
-		}
-	}
-
-	if (eventhead) {
-		char *bgcolors[2] = { "#000000", "#000033" };
-		int  bgcolor = 0;
-		int  count;
-		struct event_t *lasttoshow = eventhead;
-
-		count=0;
-		walk=eventhead; 
-		do {
-			count++;
-			lasttoshow = walk;
-			walk = walk->next;
-		} while (walk && (count<maxcount));
-
-		if (maxminutes)  { 
-			sprintf(title, "%d events received in the past %u minutes", 
-				count, (unsigned int)((time(NULL) - lasttoshow->eventtime) / 60));
-		}
-		else {
-			sprintf(title, "%d events received.", count);
-		}
-
-		fprintf(output, "<BR><BR>\n");
-		fprintf(output, "<TABLE SUMMARY=\"$EVENTSTITLE\" BORDER=0>\n");
-		fprintf(output, "<TR BGCOLOR=\"#333333\">\n");
-		fprintf(output, "<TD ALIGN=CENTER COLSPAN=6><FONT SIZE=-1 COLOR=\"#33ebf4\">%s</FONT></TD></TR>\n", title);
-
-		for (walk=eventhead; (walk != lasttoshow->next); walk=walk->next) {
-			fprintf(output, "<TR BGCOLOR=%s>\n", bgcolors[bgcolor]);
-			bgcolor = ((bgcolor + 1) % 2);
-
-			fprintf(output, "<TD ALIGN=CENTER>%s</TD>\n", ctime(&walk->eventtime));
-
-			if (walk->newcolor == COL_CLEAR) {
-				fprintf(output, "<TD ALIGN=CENTER BGCOLOR=black><FONT COLOR=white>%s</FONT></TD>\n",
-					walk->host->hostname);
-			}
-			else {
-				fprintf(output, "<TD ALIGN=CENTER BGCOLOR=%s><FONT COLOR=black>%s</FONT></TD>\n",
-					colorname(walk->newcolor), walk->host->hostname);
-			}
-
-			fprintf(output, "<TD ALIGN=LEFT>%s</TD>\n", walk->service->name);
-			fprintf(output, "<TD><A HREF=\"%s\">\n", 
-				histlogurl(walk->host->hostname, walk->service->name, walk->changetime));
-			fprintf(output, "<IMG SRC=\"%s/%s\"  HEIGHT=\"%s\" WIDTH=\"%s\" BORDER=0 ALT=\"%s\" TITLE=\"%s\"></A>\n", 
-				xgetenv("BBSKIN"), dotgiffilename(walk->oldcolor, 0, 0), 
-				xgetenv("DOTHEIGHT"), xgetenv("DOTWIDTH"), 
-				colorname(walk->oldcolor), colorname(walk->oldcolor));
-			fprintf(output, "<IMG SRC=\"%s/arrow.gif\" BORDER=0 ALT=\"From -&gt; To\">\n", 
-				xgetenv("BBSKIN"));
-			fprintf(output, "<TD><A HREF=\"%s\">\n", 
-				histlogurl(walk->host->hostname, walk->service->name, walk->eventtime));
-			fprintf(output, "<IMG SRC=\"%s/%s\"  HEIGHT=\"%s\" WIDTH=\"%s\" BORDER=0 ALT=\"%s\" TITLE=\"%s\"></A>\n", 
-				xgetenv("BBSKIN"), dotgiffilename(walk->newcolor, 0, 0), 
-				xgetenv("DOTHEIGHT"), xgetenv("DOTWIDTH"), 
-				colorname(walk->newcolor), colorname(walk->newcolor));
-		}
-
-		fprintf(output, "</TABLE>\n");
-
-		/* Clean up */
-		walk = eventhead;
-		do {
-			struct event_t *tmp = walk;
-
-			walk = walk->next;
-			xfree(tmp);
-		} while (walk);
-	}
-	else {
-		/* No events during the past maxminutes */
-		sprintf(title, "No events received in the last %d minutes", maxminutes);
-
-		fprintf(output, "<CENTER><BR>\n");
-		fprintf(output, "<TABLE SUMMARY=\"%s\" BORDER=0>\n", title);
-		fprintf(output, "<TR BGCOLOR=\"#333333\">\n");
-		fprintf(output, "<TD ALIGN=CENTER COLSPAN=6><FONT SIZE=-1 COLOR=\"#33ebf4\">%s</FONT></TD>\n", title);
-		fprintf(output, "</TR>\n");
-		fprintf(output, "</TABLE>\n");
-		fprintf(output, "</CENTER>\n");
-	}
-
-	fclose(eventlog);
-
-	if (hostregexp) pcre_free(hostregexp);
-	if (testregexp) pcre_free(testregexp);
-	if (colrregexp) pcre_free(colrregexp);
-}
-
-
-#ifdef CGI
+#include "libbbgen.h"
 
 /*
  * This program is invoked via CGI with QUERY_STRING containing:
@@ -361,11 +54,6 @@ char *reqenv[] = {
 "DOTWIDTH",
 "DOTHEIGHT",
 NULL };
-
-/* Global vars needed for load_bbhosts() */
-summary_t *sumhead = NULL;
-time_t reportstart = 0;
-double reportwarnlevel = 97.0;
 
 static void errormsg(char *msg)
 {
@@ -430,7 +118,6 @@ static void parse_query(void)
 int main(int argc, char *argv[])
 {
 	int argi;
-	bbgen_page_t *pagehead = NULL;
 	char *envarea = NULL;
 
 	for (argi=1; (argi < argc); argi++) {
@@ -478,7 +165,7 @@ int main(int argc, char *argv[])
 
 	envcheck(reqenv);
 	parse_query();
-	pagehead = load_bbhosts(NULL);
+	load_hostnames(xgetenv("BBHOSTS"), NULL, get_fqdn());
 
 	/* Now generate the webpage */
 	printf("Content-Type: text/html\n\n");
@@ -491,6 +178,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-#endif
 
