65e5155178955d160253c7b4195a1cdfcbede79a
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-02-13 22:01:55 +0000

    Use CONFIGCLASS to get the client config.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4117 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/hobbitclient.sh
+++ trunk/client/hobbitclient.sh
@@ -12,13 +12,19 @@
 #                                                                            #
 #----------------------------------------------------------------------------#
 #
-# $Id: hobbitclient.sh,v 1.7 2005-10-25 05:52:56 henrik Exp $
+# $Id: hobbitclient.sh,v 1.8 2006-02-13 22:01:55 henrik Exp $
 
 # Must make sure the commands return standard (english) texts.
 LANG=C
 LC_ALL=C
 LC_MESSAGES=C
 export LANG LC_ALL LC_MESSAGES
+
+if test "$CONFIGCLASS" = ""
+then
+	CONFIGCLASS="$MACHINEDOTS"
+	export CONFIGCLASS
+fi
 
 LOCALMODE="no"
 if test $# -ge 1; then
@@ -40,14 +46,20 @@ if test "$LOCALMODE" = "yes"; then
 	echo "@@client#1|0|127.0.0.1|$MACHINEDOTS|$BBOSTYPE" >> $TEMPFILE
 fi
 
-echo "client $MACHINE.$BBOSTYPE"  >>  $TEMPFILE
+echo "client $MACHINE.$BBOSTYPE $CONFIGCLASS"  >>  $TEMPFILE
 $BBHOME/bin/$BBOSSCRIPT >> $TEMPFILE
 
 if test "$LOCALMODE" = "yes"; then
 	echo "@@" >> $TEMPFILE
 	$BBHOME/bin/hobbitd_client --local --config=$BBHOME/etc/localclient.cfg <$TEMPFILE
 else
-	$BB $BBDISP "@" < $TEMPFILE
+	$BB $BBDISP "@" < $TEMPFILE >$BBHOME/etc/logfetch.cfg.tmp
+	if test -s $BBHOME/etc/logfetch.cfg.tmp
+	then
+		mv $BBHOME/etc/logfetch.cfg.tmp $BBHOME/etc/logfetch.cfg
+	else
+		rm $BBHOME/etc/logfetch.cfg.tmp
+	fi
 fi
 
 # Save the latest file for debugging.
