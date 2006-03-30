57b76c2aa6b728ef34651fb50142785134b208b0
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-03-30 06:59:06 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4249 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/lib/strfunc.h
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit monitor library.                                                    */
+/*                                                                            */
+/* Copyright (C) 2002-2005 Henrik Storner <henrik@storner.dk>                 */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef __STRFUNC_H__
+#define __STRFUNC_H__
+
+extern strbuffer_t *newstrbuffer(int initialsize);
+extern void addtobuffer(strbuffer_t *buf, char *newtext);
+extern void addtostrbuffer(strbuffer_t *buf, strbuffer_t *newtext);
+extern void clearstrbuffer(strbuffer_t *buf);
+extern void freestrbuffer(strbuffer_t *buf);
+extern char *grabstrbuffer(strbuffer_t *buf);
+extern strbuffer_t *dupstrbuffer(char *src);
+extern void strbufferchop(strbuffer_t *buf, int count);
+
+#endif
+
