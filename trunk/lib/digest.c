eb8b4e82460e064983512b49d996657ba520e09c
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-04-14 11:19:01 +0000

    Drop digest dependency on OpenSSL. We now have md5 and sha1 - should be enough.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4336 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/lib/digest.c
+++ trunk/lib/digest.c
@@ -10,16 +10,12 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: digest.c,v 1.11 2006-04-14 10:19:22 henrik Exp $";
+static char rcsid[] = "$Id: digest.c,v 1.12 2006-04-14 11:19:01 henrik Exp $";
 
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#ifdef BBGEN_SSL
-#include <openssl/evp.h>
-#endif
 
 #include "libbbgen.h"
 
@@ -27,34 +23,6 @@ digestctx_t *digest_init(char *digest)
 {
 	struct digestctx_t *ctx = NULL;
 
-#ifdef BBGEN_SSL
-	static int dgst_init_done = 0;
-	const EVP_MD *md;
-
-	if (!dgst_init_done) {
-		OpenSSL_add_all_digests();
-		dgst_init_done = 1;
-	}
-
-	ctx = (digestctx_t *) malloc(sizeof(digestctx_t));
-	ctx->digestname = strdup(digest);
-	ctx->digesttype = D_OPENSSL;
-	md = EVP_get_digestbyname(ctx->digestname);
-
-	if (!md) {
-		xfree(ctx);
-		return NULL;
-        }
-
-	ctx->mdctx = (void *)malloc(sizeof(EVP_MD_CTX));
-#if OPENSSL_VERSION_NUMBER >= 0x00907000L
-	EVP_MD_CTX_init(ctx->mdctx);
-	EVP_DigestInit_ex(ctx->mdctx, md, NULL);
-#else
-	EVP_DigestInit(ctx->mdctx, md);
-#endif
-
-#else
 	if (strcmp(digest, "md5") == 0) {
 		/* Use the built in MD5 routines */
 		ctx = (digestctx_t *) malloc(sizeof(digestctx_t));
@@ -72,10 +40,9 @@ digestctx_t *digest_init(char *digest)
 		mySHA1Init((mySHA1_CTX *)ctx->mdctx);
 	}
 	else {
-		errprintf("digest_init failure: bbgen was compiled without OpenSSL support\n");
+		errprintf("digest_init failure: Cannot handle digest %s\n", digest);
 		return NULL;
 	}
-#endif
 
 	return ctx;
 }
@@ -83,9 +50,6 @@ digestctx_t *digest_init(char *digest)
 
 int digest_data(digestctx_t *ctx, char *buf, int buflen)
 {
-#ifdef BBGEN_SSL
-	EVP_DigestUpdate(ctx->mdctx, buf, buflen);
-#else
 	switch (ctx->digesttype) {
 	  case D_MD5:
 		md5_append((md5_state_t *)ctx->mdctx, (const md5_byte_t *)buf, buflen);
@@ -93,35 +57,20 @@ int digest_data(digestctx_t *ctx, char *buf, int buflen)
 	  case D_SHA1:
 		mySHA1Update((mySHA1_CTX *)ctx->mdctx, buf, buflen);
 		break;
-	  case D_OPENSSL:
-		break;
 	}
-#endif
+
 	return 0;
 }
 
 
 char *digest_done(digestctx_t *ctx)
 {
-	int i;
 	unsigned int md_len = 0;
+	unsigned char *md_value = NULL;
+	char *md_string = NULL;
+	int i;
 	char *p;
-	unsigned char *md_value;
-	char *md_string;
 
-#ifdef BBGEN_SSL
-	md_value  = (unsigned char *)malloc(EVP_MAX_MD_SIZE*sizeof(unsigned char));
-	md_string = (char *)malloc(2*EVP_MAX_MD_SIZE+128);
-
-#if OPENSSL_VERSION_NUMBER >= 0x00907000L
-	EVP_DigestFinal_ex(ctx->mdctx, md_value, &md_len);
-	EVP_MD_CTX_cleanup(ctx->mdctx);
-#else
-	EVP_DigestFinal(ctx->mdctx, md_value, &md_len);
-	EVP_cleanup();
-#endif
-
-#else
 	switch (ctx->digesttype) {
 	  case D_MD5:
 		/* Built in MD5 hash */
@@ -137,10 +86,7 @@ char *digest_done(digestctx_t *ctx)
 		md_string = (char *)malloc((2*md_len + strlen(ctx->digestname) + 2)*sizeof(char));
 		mySHA1Final(md_value, (mySHA1_CTX *)ctx->mdctx);
 		break;
-	  case D_OPENSSL:
-		break;
 	}
-#endif
 
 	sprintf(md_string, "%s:", ctx->digestname);
 	for(i = 0, p = md_string + strlen(md_string); (i < md_len); i++) p += sprintf(p, "%02x", md_value[i]);
