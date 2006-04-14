1cd1a99bbc40e96da3554c8e444694103ca08ae0
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-04-14 10:17:06 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4326 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/lib/test-endianness.c
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit monitor library.                                                    */
+/*                                                                            */
+/* Utility program to define endian-ness of the target system.                */
+/*                                                                            */
+/* Copyright (C) 2006 Henrik Storner <henrik@storner.dk>                      */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: test-endianness.c,v 1.1 2006-04-14 10:17:06 henrik Exp $";
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	unsigned int c;
+	unsigned char cbuf[sizeof(c)];
+	int i;
+	int outform = 1;
+
+	if ((argc > 1) && (strcmp(argv[1], "--configh") == 0)) outform = 0;
+
+	for (i=0; (i < sizeof(c)); i++) {
+		cbuf[i] = (i % 2);
+	}
+
+	memcpy(&c, cbuf, sizeof(c));
+
+	if (c == 65537) {
+		/* Big endian */
+		if (outform == 0)
+			printf("#ifndef BIG_ENDIAN\n#define BIG_ENDIAN\n#endif\n");
+		else
+			printf(" -DBIG_ENDIAN");
+	}
+	else if (c == 16777472) {
+		/* Little endian */
+		if (outform == 0)
+			printf("#ifndef LITTLE_ENDIAN\n#define LITTLE_ENDIAN\n#endif\n");
+		else
+			printf(" -DLITTLE_ENDIAN");
+	}
+
+	fflush(stdout);
+	return 0;
+}
+
