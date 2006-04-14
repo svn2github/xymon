951442312f6c6b08126f415d9ec4b96d98c029e2
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-04-14 11:24:38 +0000

    Add lots of FILE checks, including hash/checksum validation
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4340 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/logfetch.c
+++ trunk/client/logfetch.c
@@ -12,7 +12,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: logfetch.c,v 1.9 2006-04-03 12:55:20 henrik Exp $";
+static char rcsid[] = "$Id: logfetch.c,v 1.10 2006-04-14 11:24:38 henrik Exp $";
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,27 +20,46 @@ static char rcsid[] = "$Id: logfetch.c,v 1.9 2006-04-03 12:55:20 henrik Exp $";
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 #include <limits.h>
 #include <errno.h>
 #include <regex.h>
+#include <pwd.h>
+#include <grp.h>
+
+#include "libbbgen.h"
 
 /* Is it ok for these to be hardcoded ? */
 #define MAXMINUTES 30
 #define POSCOUNT ((MAXMINUTES / 5) + 1)
 #define LINES_AFTER_TRIGGER 10
 
+typedef enum { C_NONE, C_LOG, C_FILE } checktype_t;
+
 typedef struct logdef_t {
-	char *filename;
 	off_t lastpos[POSCOUNT];
 	char *trigger;
 	char *ignore;
 	int maxbytes;
-	struct logdef_t *next;
 } logdef_t;
 
-logdef_t *loglist = NULL;
+typedef struct filedef_t {
+	int domd5, dosha1;
+} filedef_t;
 
-char *logdata(logdef_t *logdef, int *truncated)
+typedef struct checkdef_t {
+	char *filename;
+	checktype_t checktype;
+	struct checkdef_t *next;
+	union {
+		logdef_t logcheck;
+		filedef_t filecheck;
+	} check;
+} checkdef_t;
+
+checkdef_t *checklist = NULL;
+
+char *logdata(char *filename, logdef_t *logdef, int *truncated)
 {
 	static char *result = NULL;
 	char *buf = NULL;
@@ -52,10 +71,10 @@ char *logdata(logdef_t *logdef, int *truncated)
 
 	*truncated = 0;
 
-	fd = fopen(logdef->filename, "r");
+	fd = fopen(filename, "r");
 	if (fd == NULL) {
-		result = (char *)malloc(1024 + strlen(logdef->filename));
-		sprintf(result, "Cannot open logfile %s : %s\n", logdef->filename, strerror(errno));
+		result = (char *)malloc(1024 + strlen(filename));
+		sprintf(result, "Cannot open logfile %s : %s\n", filename, strerror(errno));
 		return result;
 	}
 
@@ -85,7 +104,7 @@ char *logdata(logdef_t *logdef, int *truncated)
 		*(buf + n) = '\0';
 	}
 	else {
-		sprintf("Error while reading logfile %s : %s\n", logdef->filename, strerror(errno));
+		sprintf("Error while reading logfile %s : %s\n", filename, strerror(errno));
 	}
 
 	fclose(fd);
@@ -208,38 +227,189 @@ char *logdata(logdef_t *logdef, int *truncated)
 	return result;
 }
 
+char *ftypestr(unsigned int mode, char *symlink)
+{
+	static char *result = NULL;
+	char *s = "unknown";
+
+	if (S_ISREG(mode)) s = "file";
+	if (S_ISDIR(mode)) s = "directory";
+	if (S_ISCHR(mode)) s = "char-device";
+	if (S_ISBLK(mode)) s = "block-device";
+	if (S_ISFIFO(mode)) s = "FIFO";
+	if (S_ISSOCK(mode)) s = "socket";
+
+	if (symlink == NULL) return s;
+
+	/* Special handling for symlinks */
+	if (result) free(result);
+
+	result = (char *)malloc(strlen(s) + strlen(symlink) + 100);
+	sprintf(result, "%s, symlink -> %s", s, symlink);
+	return result;
+}
+
+char *fmodestr(unsigned int mode)
+{
+	static char modestr[11];
+
+	if (S_ISREG(mode)) modestr[0] = '-';
+	else if (S_ISDIR(mode)) modestr[0] = 'd';
+	else if (S_ISCHR(mode)) modestr[0] = 'c';
+	else if (S_ISBLK(mode)) modestr[0] = 'b';
+	else if (S_ISFIFO(mode)) modestr[0] = 'p';
+	else if (S_ISLNK(mode)) modestr[0] = 'l';
+	else if (S_ISSOCK(mode)) modestr[0] = 's';
+	else modestr[0] = '?';
+
+	modestr[1] = ((mode & S_IRUSR) ? 'r' : '-');
+	modestr[2] = ((mode & S_IWUSR) ? 'w' : '-');
+	modestr[3] = ((mode & S_IXUSR) ? 'x' : '-');
+	modestr[4] = ((mode & S_IRGRP) ? 'r' : '-');
+	modestr[5] = ((mode & S_IWGRP) ? 'w' : '-');
+	modestr[6] = ((mode & S_IXGRP) ? 'x' : '-');
+	modestr[7] = ((mode & S_IROTH) ? 'r' : '-');
+	modestr[8] = ((mode & S_IWOTH) ? 'w' : '-');
+	modestr[9] = ((mode & S_IXOTH) ? 'x' : '-');
+
+	if ((mode & S_ISUID)) modestr[3] = 's';
+	if ((mode & S_ISGID)) modestr[6] = 's';
+
+	modestr[10] = '\0';
+
+	return modestr;
+}
+
+char *timestr(time_t tstamp)
+{
+	static char result[20];
+
+	strftime(result, sizeof(result), "%Y/%m/%d-%H:%M:%S", localtime(&tstamp));
+	return result;
+}
+
+char *filesum(char *fn, char *dtype)
+{
+	static char *result = NULL;
+	digestctx_t *ctx;
+	FILE *fd;
+	unsigned char buf[8192];
+	int buflen;
+
+        if ((ctx = digest_init(dtype)) == NULL) return "";
+
+	fd = fopen(fn, "r"); if (fd == NULL) return "";
+	while ((buflen = fread(buf, 1, sizeof(buf), fd)) > 0) digest_data(ctx, buf, buflen);
+	fclose(fd);
+
+	if (result) xfree(result);
+	result = strdup(digest_done(ctx));
+
+	return result;
+}
+
+void printfiledata(FILE *fd, char *fn, int domd5, int dosha1)
+{
+	struct stat st;
+	struct passwd *pw;
+	struct group *gr;
+	int staterror;
+	char linknam[PATH_MAX];
+	time_t now = time(NULL);
+
+	*linknam = '\0';
+	staterror = lstat(fn, &st);
+	if ((staterror == 0) && S_ISLNK(st.st_mode)) {
+		if (readlink(fn, linknam, sizeof(linknam)) == -1) *linknam = '\0';
+		staterror = stat(fn, &st);
+	}
+
+	if (staterror == -1) {
+		fprintf(fd, "ERROR: %s\n", strerror(errno));
+	}
+	else {
+		pw = getpwuid(st.st_uid);
+		gr = getgrgid(st.st_gid);
+
+		fprintf(fd, "type:%o (%s)\n", 
+			(st.st_mode & S_IFMT), 
+			ftypestr(st.st_mode, (*linknam ? linknam : NULL)));
+		fprintf(fd, "mode:%o (%s)\n", 
+			(st.st_mode & (S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO)), 
+			fmodestr(st.st_mode));
+		fprintf(fd, "linkcount:%d\n", st.st_nlink);
+		fprintf(fd, "owner:%u (%s)\n", st.st_uid, (pw ? pw->pw_name : ""));
+		fprintf(fd, "group:%u (%s)\n", st.st_gid, (gr ? gr->gr_name : ""));
+		fprintf(fd, "size:%lu\n", (unsigned long) st.st_size);
+		fprintf(fd, "clock:%u (%s)\n", (unsigned int)now, timestr(now));
+		fprintf(fd, "atime:%u (%s)\n", (unsigned int)st.st_atime, timestr(st.st_atime));
+		fprintf(fd, "ctime:%u (%s)\n", (unsigned int)st.st_ctime, timestr(st.st_ctime));
+		fprintf(fd, "mtime:%u (%s)\n", (unsigned int)st.st_mtime, timestr(st.st_mtime));
+		if (S_ISREG(st.st_mode)) {
+			if (domd5) fprintf(fd, "%s\n", filesum(fn, "md5"));
+			if (dosha1) fprintf(fd, "%s\n", filesum(fn, "sha1"));
+		}
+	}
+
+	fprintf(fd, "\n");
+}
+
 int loadconfig(char *cfgfn)
 {
 	FILE *fd;
 	char l[PATH_MAX + 1024];
-	logdef_t *currcfg = NULL;
-	logdef_t *firstpipeitem = NULL;
+	checkdef_t *currcfg = NULL;
+	checkdef_t *firstpipeitem = NULL;
 
 	/* Config items are in the form:
 	 *    log:filename:maxbytes
 	 *    ignore ignore-regexp (optional)
 	 *    trigger trigger-regexp (optional)
+	 *
+	 *    file:filename
 	 */
 	fd = fopen(cfgfn, "r"); if (fd == NULL) return 1;
 	while (fgets(l, sizeof(l), fd) != NULL) {
 		char *p, *filename;
-		int maxbytes;
+		int maxbytes, domd5, dosha1;
 
 		p = strchr(l, '\n'); if (p) *p = '\0';
 		p = l + strspn(l, " \t");
 		if ((*p == '\0') || (*p == '#')) continue;
 
-		if (strncmp(l, "log:", 4) == 0) {
+		if ((strncmp(l, "log:", 4) == 0) || (strncmp(l, "file:", 4) == 0)) {
+			checktype_t checktype = C_NONE;
 			char *tok;
 
-			filename = NULL; maxbytes = -1;
+			filename = NULL; maxbytes = -1; domd5 = 0; dosha1 = 0;
 			tok = strtok(l, ":");
-			if (tok) filename = strtok(NULL, ":");
-			if (filename) tok = strtok(NULL, ":");
-			if (tok) maxbytes = atoi(tok);
+			if (strcmp(tok, "log") == 0) checktype = C_LOG;
+			else if (strcmp(tok, "file") == 0) checktype = C_FILE;
+
+			filename = strtok(NULL, ":"); if (filename) tok = strtok(NULL, ":");
+			switch (checktype) {
+			  case C_LOG:
+				if (tok) maxbytes = atoi(tok);
+				break;
+			  case C_FILE:
+				maxbytes = 0;
+				if (tok) {
+					if (strcmp(tok, "md5") == 0) {
+						domd5 = 1;
+					}
+					else if (strcmp(tok, "sha1") == 0) {
+						dosha1 = 1;
+					}
+				}
+				break;
+
+			  case C_NONE:
+				break;
+			}
 
 			if ((filename != NULL) && (maxbytes != -1)) {
-				logdef_t *newitem;
+				checkdef_t *newitem;
+
 				firstpipeitem = NULL;
 
 				if (*filename == '`') {
@@ -251,18 +421,31 @@ int loadconfig(char *cfgfn)
 					p = strchr(cmd, '`'); if (p) *p = '\0';
 					fd = popen(cmd, "r");
 					if (fd) {
-						char pline[PATH_MAX];
+						char pline[PATH_MAX+1];
 						char *p;
 
 						while (fgets(pline, sizeof(pline), fd)) {
 							p = pline + strcspn(pline, "\r\n"); *p = '\0';
 
-							newitem = calloc(sizeof(logdef_t), 1);
+							newitem = calloc(sizeof(checkdef_t), 1);
 
+							newitem->checktype = checktype;
 							newitem->filename = strdup(pline);
-							newitem->maxbytes = maxbytes;
-							newitem->next = loglist;
-							loglist = newitem;
+
+							switch (checktype) {
+					  		  case C_LOG:
+								newitem->check.logcheck.maxbytes = maxbytes;
+								break;
+							  case C_FILE:
+								newitem->check.filecheck.domd5 = domd5;
+								newitem->check.filecheck.dosha1 = dosha1;
+								break;
+					  		  case C_NONE:
+								break;
+							}
+
+							newitem->next = checklist;
+							checklist = newitem;
 
 							/*
 							 * Since we insert new items at the head of the list,
@@ -280,11 +463,21 @@ int loadconfig(char *cfgfn)
 
 				}
 				else {
-					newitem = calloc(sizeof(logdef_t), 1);
+					newitem = calloc(sizeof(checkdef_t), 1);
 					newitem->filename = strdup(filename);
-					newitem->maxbytes = maxbytes;
-					newitem->next = loglist;
-					loglist = newitem;
+					newitem->checktype = checktype;
+
+					switch (checktype) {
+					  case C_LOG:
+						newitem->check.logcheck.maxbytes = maxbytes;
+						break;
+					  case C_FILE:
+					  case C_NONE:
+						break;
+					}
+
+					newitem->next = checklist;
+					checklist = newitem;
 
 					currcfg = newitem;
 				}
@@ -293,37 +486,45 @@ int loadconfig(char *cfgfn)
 				currcfg = NULL;
 			}
 		}
-		else if (currcfg && (strncmp(l, "ignore ", 7) == 0)) {
-			p = l + 7; p += strspn(p, " \t");
+		else if (currcfg && (currcfg->checktype == C_LOG)) {
+			if (strncmp(l, "ignore ", 7) == 0) {
+				p = l + 7; p += strspn(p, " \t");
 
-			if (firstpipeitem) {
-				/* Fill in this ignore expression on all items in this pipe set */
-				logdef_t *walk = currcfg;
+				if (firstpipeitem) {
+					/* Fill in this ignore expression on all items in this pipe set */
+					checkdef_t *walk = currcfg;
 
-				do {
-					walk->ignore = strdup(p);
-					walk = walk->next;
-				} while (walk && (walk != firstpipeitem->next));
+					do {
+						walk->check.logcheck.ignore = strdup(p);
+						walk = walk->next;
+					} while (walk && (walk != firstpipeitem->next));
+				}
+				else {
+					currcfg->check.logcheck.ignore = strdup(p);
+				}
 			}
-			else {
-				currcfg->ignore = strdup(p);
+			else if (strncmp(l, "trigger ", 8) == 0) {
+				p = l + 8; p += strspn(p, " \t");
+
+				if (firstpipeitem) {
+					/* Fill in this trigger expression on all items in this pipe set */
+					checkdef_t *walk = currcfg;
+
+					do {
+						walk->check.logcheck.trigger = strdup(p);
+						walk = walk->next;
+					} while (walk && (walk != firstpipeitem->next));
+				}
+				else {
+					currcfg->check.logcheck.trigger = strdup(p);
+				}
 			}
 		}
-		else if (currcfg && (strncmp(l, "trigger ", 8) == 0)) {
-			p = l + 8; p += strspn(p, " \t");
-
-			if (firstpipeitem) {
-				/* Fill in this trigger expression on all items in this pipe set */
-				logdef_t *walk = currcfg;
-
-				do {
-					walk->trigger = strdup(p);
-					walk = walk->next;
-				} while (walk && (walk != firstpipeitem->next));
-			}
-			else {
-				currcfg->trigger = strdup(p);
-			}
+		else if (currcfg && (currcfg->checktype == C_FILE)) {
+			/* Nothing */
+		}
+		else if (currcfg && (currcfg->checktype == C_NONE)) {
+			/* Nothing */
 		}
 		else currcfg = NULL;
 	}
@@ -332,7 +533,7 @@ int loadconfig(char *cfgfn)
 	return 0;
 }
 
-void loadstatus(char *statfn)
+void loadlogstatus(char *statfn)
 {
 	FILE *fd;
 	char l[PATH_MAX + 1024];
@@ -342,36 +543,38 @@ void loadstatus(char *statfn)
 
 	while (fgets(l, sizeof(l), fd)) {
 		char *fn, *tok;
-		logdef_t *lwalk;
+		checkdef_t *walk;
 		int i;
 
 		tok = strtok(l, ":"); if (!tok) continue;
 		fn = tok;
-		for (lwalk = loglist; (lwalk && strcmp(lwalk->filename, fn)); lwalk = lwalk->next) ;
-		if (!lwalk) continue;
+		for (walk = checklist; (walk && (walk->checktype == C_LOG) && (strcmp(walk->filename, fn) != 0)); walk = walk->next) ;
+		if (!walk) continue;
 
 		for (i=0; (tok && (i < POSCOUNT)); i++) {
 			tok = strtok(NULL, ":\n");
-			if (tok) lwalk->lastpos[i] = atol(tok);
+			if (tok) walk->check.logcheck.lastpos[i] = atol(tok);
 		}
 	}
 
 	fclose(fd);
 }
 
-void savestatus(char *statfn)
+void savelogstatus(char *statfn)
 {
 	FILE *fd;
-	logdef_t *lwalk;
+	checkdef_t *walk;
 
 	fd = fopen(statfn, "w");
 	if (fd == NULL) return;
 
-	for (lwalk = loglist; (lwalk); lwalk = lwalk->next) {
+	for (walk = checklist; (walk); walk = walk->next) {
 		int i;
 
-		fprintf(fd, "%s", lwalk->filename);
-		for (i = 0; (i < POSCOUNT); i++) fprintf(fd, ":%lu", lwalk->lastpos[i]);
+		if (walk->checktype != C_LOG) continue;
+
+		fprintf(fd, "%s", walk->filename);
+		for (i = 0; (i < POSCOUNT); i++) fprintf(fd, ":%lu", walk->check.logcheck.lastpos[i]);
 		fprintf(fd, "\n");
 	}
 	fclose(fd);
@@ -381,25 +584,47 @@ int main(int argc, char *argv[])
 {
 	char *cfgfn = argv[1];
 	char *statfn = argv[2];
-	logdef_t *lwalk;
+	checkdef_t *walk;
 
 	if ((cfgfn == NULL) || (statfn == NULL)) return 1;
 
 	if (loadconfig(cfgfn) != 0) return 1;
-	loadstatus(statfn);
+	loadlogstatus(statfn);
 
-	for (lwalk = loglist; (lwalk); lwalk = lwalk->next) {
+	for (walk = checklist; (walk); walk = walk->next) {
 		char *data;
 		int truncflag;
+		checkdef_t *fwalk;
 
-		data = logdata(lwalk, &truncflag);
-		fprintf(stdout, "[msgs:%s]\n", lwalk->filename);
-		if (truncflag) fprintf(stdout, "<truncated>\n");
-		fprintf(stdout, "%s\n", data);
-		free(data);
+		switch (walk->checktype) {
+		  case C_LOG:
+			data = logdata(walk->filename, &walk->check.logcheck, &truncflag);
+			fprintf(stdout, "[msgs:%s]\n", walk->filename);
+			if (truncflag) fprintf(stdout, "<truncated>\n");
+			fprintf(stdout, "%s\n", data);
+			free(data);
+
+			/* See if there's a special "file:" entry for this logfile */
+			for (fwalk = checklist; (fwalk && ((fwalk->checktype != C_FILE) || (strcmp(fwalk->filename, walk->filename) != 0))); fwalk = fwalk->next) ;
+			if (fwalk == NULL) {
+				/* No specific file: entry, so make sure the logfile metadata is available */
+				fprintf(stdout, "[file:%s]\n", walk->filename);
+				printfiledata(stdout, walk->filename, 0, 0);
+			}
+			break;
+
+		  case C_FILE:
+			fprintf(stdout, "[file:%s]\n", walk->filename);
+			printfiledata(stdout, walk->filename, 
+					walk->check.filecheck.domd5, walk->check.filecheck.dosha1);
+			break;
+
+		  case C_NONE:
+			break;
+		}
 	}
 
-	savestatus(statfn);
+	savelogstatus(statfn);
 
 	return 0;
 }
