a375e4fa52eb494ce571e9e525fa8512cc07eeb4
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-05-01 20:34:57 +0000

    Dont default the CLASS to the OS. Support for clients updating themselves from the server.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4463 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/hobbitclient.sh
+++ trunk/client/hobbitclient.sh
@@ -12,19 +12,13 @@
 #                                                                            #
 #----------------------------------------------------------------------------#
 #
-# $Id: hobbitclient.sh,v 1.10 2006-02-17 13:07:50 henrik Exp $
+# $Id: hobbitclient.sh,v 1.11 2006-05-01 20:34:57 henrik Exp $
 
 # Must make sure the commands return standard (english) texts.
 LANG=C
 LC_ALL=C
 LC_MESSAGES=C
 export LANG LC_ALL LC_MESSAGES
-
-if test "$CONFIGCLASS" = ""
-then
-	CONFIGCLASS="$BBOSTYPE"
-	export CONFIGCLASS
-fi
 
 LOCALMODE="no"
 if test $# -ge 1; then
@@ -42,12 +36,16 @@ TEMPFILE="$BBTMP/msg.txt.$$"
 rm -f $TEMPFILE
 touch $TEMPFILE
 
+CLIENTVERSION="`$BBHOME/bin/clientupdate --level`"
+
 if test "$LOCALMODE" = "yes"; then
 	echo "@@client#1|0|127.0.0.1|$MACHINEDOTS|$BBOSTYPE" >> $TEMPFILE
 fi
 
 echo "client $MACHINE.$BBOSTYPE $CONFIGCLASS"  >>  $TEMPFILE
 $BBHOME/bin/$BBOSSCRIPT >> $TEMPFILE
+echo "[clientversion]"
+echo "$CLIENTVERSION" >> $TEMPFILE
 
 if test "$LOCALMODE" = "yes"; then
 	echo "@@" >> $TEMPFILE
@@ -65,6 +63,15 @@ fi
 # Save the latest file for debugging.
 rm -f $BBTMP/msg.txt
 mv $TEMPFILE $BBTMP/msg.txt
+
+if test "$LOCALMODE" != "yes"; then
+	# Check for client updates
+	SERVERVERSION=`grep "^clientversion:" $BBTMP/logfetch.cfg | cut -d: -f2`
+	if test "$SERVERVERSION" != "" -a "$SERVERVERSION" != "$CLIENTVERSION"; then
+		cp -pf $BBHOME/bin/clientupdate $BBTMP/.update
+		exec $BBTMP/.update --update=$SERVERVERSION
+	fi
+fi
 
 exit 0
 
