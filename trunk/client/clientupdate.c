19fdb7077385e217923609a4c94523b202c2e56f
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-05-28 11:30:37 +0000

    Lots of changes to get suid-root permissions set correctly for updating is privileged utilities while keeping most things working when installed without suid-root privs.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4575 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/client/clientupdate.c
+++ trunk/client/clientupdate.c
@@ -13,16 +13,48 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: clientupdate.c,v 1.1 2006-05-01 20:34:33 henrik Exp $";
+static char rcsid[] = "$Id: clientupdate.c,v 1.2 2006-05-28 11:30:37 henrik Exp $";
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include "libbbgen.h"
 
 #define CLIENTVERSIONFILE "etc/clientversion.cfg"
+
+
+#ifdef HPUX
+
+void drop_root(uid_t myuid)
+{
+	setresuid(-1, myuid, -1);
+}
+
+void get_root(void)
+{
+	setresuid(-1, 0, -1);
+}
+
+#else
+
+void drop_root(uid_t myuid)
+{
+	seteuid(myuid);
+}
+
+void get_root(void)
+{
+	seteuid(0);
+}
+
+#endif
+
 
 int main(int argc, char *argv[])
 {
@@ -32,6 +64,13 @@ int main(int argc, char *argv[])
 	char version[10];
 	char *newversion = NULL;
 	char *newverreq;
+	char *updateparam = NULL;
+	int  removeself = 0;
+	uid_t myuid;
+
+	/* Immediately drop all root privs, we'll regain them later when needed */
+	myuid = getuid();
+	drop_root(myuid);
 
 	versionfn = (char *)malloc(strlen(xgetenv("BBHOME")) + strlen(CLIENTVERSIONFILE) + 2);
 	sprintf(versionfn, "%s/%s", xgetenv("BBHOME"), CLIENTVERSIONFILE);
@@ -44,24 +83,106 @@ int main(int argc, char *argv[])
 	}
 	else *version = '\0';
 
+	if (chdir(xgetenv("BBHOME")) != 0) {
+		printf("Cannot chdir to BBHOME\n");
+		return 1;
+	}
+
 	for (argi=1; (argi < argc); argi++) {
 		if (strcmp(argv[argi], "--level") == 0) {
+			/* For checking what version we're at */
 			printf("%s\n", version);
 			return 0;
 		}
+		else if (strcmp(argv[argi], "--reexec") == 0) {
+			/*
+			 * First step of the update procedure.
+			 *
+			 * To avoid problems with unpacking a new clientupdate
+			 * on top of the running one (some tar's will abort
+			 * if they try this), copy ourself to a temp. file and
+			 * re-exec it to carry out the update.
+			 */
+			char tmpfn[PATH_MAX];
+			char *srcfn;
+			FILE *tmpfd, *srcfd;
+			unsigned char buf[8192];
+			long n;
+			char *newcmd;
+
+			if (!updateparam) {
+				printf("clientupdate --reexec called with no update version\n");
+				return 1;
+			}
+
+			srcfn = argv[0];
+
+			sprintf(tmpfn, "%s/.update.%s.%ld.tmp", 
+				xgetenv("BBTMP"), xgetenv("MACHINEDOTS"), (long)time(NULL));
+			unlink(tmpfn);	/* To avoid symlink attacks */
+
+			srcfd = fopen(srcfn, "r");
+			if (srcfd) tmpfd = fopen(tmpfn, "w");
+			if (!srcfd || !tmpfd) {
+				printf("Cannot copy executable: %s\n", strerror(errno));
+				return 1;
+			}
+
+			while ((n = fread(buf, 1, sizeof(buf), srcfd)) > 0) fwrite(buf, 1, n, tmpfd);
+			fclose(srcfd); fclose(tmpfd);
+
+			/* Make sure the temp. binary has execute permissions set */
+			chmod(tmpfn, S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+
+			/*
+			 * Set the temp. executable suid-root, and exec() it.
+			 * If get_root() fails (because clientupdate was installed
+			 * without suid-root privs), just carry on and do what we
+			 * can without root privs. (It basically just means that
+			 * logfetch() and clientupdate() will continue to run without
+			 * root privs).
+			 */
+			get_root();
+			chown(tmpfn, 0, getgid());
+			chmod(tmpfn, S_ISUID|S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+			drop_root(myuid);
+
+			/* Run the temp. executable */
+			execl(tmpfn, tmpfn, updateparam, "--remove-self", (char *)NULL);
+
+			/* We should never go here */
+			printf("exec() failed to launch update: %s\n", strerror(errno));
+			return 1;
+		}
+
 		else if (strncmp(argv[argi], "--update=", 9) == 0) {
 			newversion = strdup(argv[argi]+9);
+			updateparam = argv[argi];
+		}
+		else if (strcmp(argv[argi], "--remove-self") == 0) {
+			removeself = 1;
+		}
+
+		else if (strcmp(argv[argi], "--suid-setup") == 0) {
+			/*
+			 * Final step of the update procedure.
+			 *
+			 * Become root to setup suid-root privs on utils that need it.
+			 * Note: If get_root() fails, we're left with normal user privileges. That is
+			 * OK, because that is how the client was installed originally, then.
+			 */
+			get_root();
+			chown("bin/logfetch", 0, getgid());
+			chmod("bin/logfetch", S_ISUID|S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+			drop_root(myuid);
+
+			return 0;
 		}
 	}
 
 	if (!newversion) return 1;
 
 	/* Update to version "newversion" */
-	if (chdir(xgetenv("BBHOME")) != 0) {
-		printf("Cannot chdir to BBHOME\n");
-		return 1;
-	}
-
 	tarpipefd = popen("tar xf -", "w");
 	if (tarpipefd == NULL) {
 		printf("Cannot launch 'tar xf -'\n");
@@ -85,6 +206,31 @@ int main(int argc, char *argv[])
 		fprintf(versionfd, "%s", newversion);
 		fclose(versionfd);
 	}
+
+	/* Make sure these have execute permissions */
+	chmod("bin/hobbitclient.sh", S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+	chmod("bin/clientupdate", S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+
+	/*
+	 * Become root to setup suid-root privs on the new clientupdate util.
+	 * Note: If get_root() fails, we're left with normal user privileges. That is
+	 * OK, because that is how the client was installed originally, then.
+	 */
+	get_root();
+	chown("bin/clientupdate", 0, getgid());
+	chmod("bin/clientupdate", S_ISUID|S_IRUSR|S_IXUSR|S_IRGRP|S_IXGRP);
+	drop_root(myuid);
+
+	if (removeself) unlink(argv[0]);
+
+	/*
+	 * Exec the new client-update utility to fix suid-root permissions on
+	 * the new files.
+	 */
+	execl("bin/clientupdate", "bin/clientupdate", "--suid-setup", (char *)NULL);
+
+	/* We should never go here */
+	printf("exec() of clientupdate --suid-setup failed: %s\n", strerror(errno));
 
 	return 0;
 }
