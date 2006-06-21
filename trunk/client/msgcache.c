3baa7aae1eef149e3ca90009d36845f6e0989a51
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-06-21 21:08:53 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4788 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/client/msgcache.c
@@ -0,0 +1,505 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit client message cache.                                               */
+/*                                                                            */
+/* This acts as a local network daemon which saves incoming messages in a     */
+/* memory cache.                                                              */
+/*                                                                            */
+/* A "pullclient" command receives the cache content in response.             */
+/* Any data provided in the "pullclient" request is saved, and passed as      */
+/* response to the first "client" command seen afterwards.                    */
+/*                                                                            */
+/* Copyright (C) 2006 Henrik Storner <henrik@hswn.dk>                         */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char rcsid[] = "$Id: msgcache.c,v 1.1 2006-06-21 21:08:53 henrik Exp $";
+
+#include "config.h"
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h>         /* Someday I'll move to GNU Autoconf for this ... */
+#endif
+#include <errno.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <signal.h>
+#include <time.h>
+
+#include "version.h"
+#include "libbbgen.h"
+
+volatile int keeprunning = 1;
+char *client_response = NULL;		/* The latest response to a "client" message */
+char *logfile = NULL;
+int maxage = 600;			/* Maximum time we will cache messages */
+
+typedef struct conn_t {
+	time_t tstamp;
+	struct sockaddr_in caddr;
+	enum { C_CLIENT_CLIENT, C_CLIENT_OTHER, C_SERVER } ctype;
+	enum { C_READING, C_WRITING, C_DONE } action;
+	int sockfd;
+	strbuffer_t *msgbuf;
+	int sentbytes;
+	struct conn_t *next;
+} conn_t;
+conn_t *chead = NULL;
+conn_t *ctail = NULL;
+
+typedef struct msgqueue_t {
+	time_t tstamp;
+	int saveresponse;
+	strbuffer_t *msgbuf;
+	struct msgqueue_t *next;
+} msgqueue_t;
+msgqueue_t *qhead = NULL;
+msgqueue_t *qtail = NULL;
+
+
+void sigmisc_handler(int signum)
+{
+	switch (signum) {
+	  case SIGTERM:
+		errprintf("Caught TERM signal, terminating\n");
+		keeprunning = 0;
+		break;
+
+	  case SIGHUP:
+		if (logfile) {
+			freopen(logfile, "a", stdout);
+			freopen(logfile, "a", stderr);
+			errprintf("Caught SIGHUP, reopening logfile\n");
+		}
+		break;
+	}
+}
+
+void grabdata(conn_t *conn)
+{
+	/* Get data from the connection socket - we know there is some */
+
+	int n;
+	char buf[8192];
+
+	n = read(conn->sockfd, buf, sizeof(buf));
+	if (n == -1) {
+		/* Read failure */
+		errprintf("Connection lost during read\n");
+		conn->action = C_DONE;
+	}
+	else if (n == 0) {
+		/* Done reading */
+		if (STRBUFLEN(conn->msgbuf) == 0) {
+			conn->action = C_DONE;
+			return;
+		}
+
+		/* 
+		 * See what kind of message this is. If it's a "pullclient" message,
+		 * start handing off the queued messages.
+		 * If it's a local "client" message, respond with the queued response
+		 * from the Hobbit server.
+		 * Anything else, just close the connection.
+		 */
+		if (strncmp(STRBUF(conn->msgbuf), "pullclient", 10) == 0) {
+			char *clientcfg;
+
+			conn->ctype = C_SERVER;
+			conn->action = C_WRITING;
+
+			/* Save any client config sent to us */
+			clientcfg = strchr(STRBUF(conn->msgbuf), '\n');
+			if (clientcfg) {
+				clientcfg++;
+				if (client_response) xfree(client_response);
+				client_response = strdup(clientcfg);
+			}
+		}
+		else if (strncmp(STRBUF(conn->msgbuf), "client ", 7) == 0) {
+			conn->ctype = C_CLIENT_CLIENT;
+			conn->action = (client_response ? C_WRITING : C_DONE);
+		}
+		else {
+			conn->ctype = C_CLIENT_OTHER;
+			conn->action = C_DONE;
+		}
+
+		if (conn->ctype != C_SERVER) {
+			/* Put it on the outbound queue */
+			msgqueue_t *newq = calloc(1, sizeof(msgqueue_t));
+
+			newq->tstamp = conn->tstamp;
+			newq->saveresponse = (conn->ctype == C_CLIENT_CLIENT);
+			newq->msgbuf = conn->msgbuf;
+			conn->msgbuf = NULL;
+			if (qtail) {
+				qtail->next = newq;
+				qtail = newq;
+			}
+			else {
+				qhead = qtail = newq;
+			}
+
+			if ((conn->ctype == C_CLIENT_CLIENT) && (conn->action == C_WRITING)) {
+				/* Send the response back to the client */
+				conn->msgbuf = newstrbuffer(0);
+				addtobuffer(conn->msgbuf, client_response);
+				xfree(client_response); client_response = NULL;
+			}
+		}
+		else {
+			msgqueue_t *q, *rec;
+
+			/* Clear the outbound message queue */
+			q = qhead; qhead = qtail = NULL;
+
+			if (!q) {
+				/* No queued messages */
+				conn->action = C_DONE;
+			}
+			else {
+				/* Build a message of all the queued data */
+				clearstrbuffer(conn->msgbuf);
+
+				/* Index line first */
+				for (rec = q; (rec); rec = rec->next) {
+					char idx[20];
+					sprintf(idx, "%d ", STRBUFLEN(rec->msgbuf));
+					addtobuffer(conn->msgbuf, idx);
+				}
+				addtobuffer(conn->msgbuf, "\n");
+
+				/* Then a stream of messages */
+				while (q) {
+					rec = q; q = q->next;
+					addtostrbuffer(conn->msgbuf, rec->msgbuf);
+					freestrbuffer(rec->msgbuf);
+					xfree(rec);
+				}
+			}
+		}
+	}
+	else {
+		buf[n] = '\0';
+		addtobuffer(conn->msgbuf, buf);
+	}
+}
+
+void senddata(conn_t *conn)
+{
+	/* Send data on the connection socket */
+	int n, togo;
+	char *startp;
+
+	togo = STRBUFLEN(conn->msgbuf) - conn->sentbytes;
+	startp = STRBUF(conn->msgbuf) + conn->sentbytes;
+	n = write(conn->sockfd, startp, togo);
+
+	if (n == -1) {
+		/* Write failure */
+		errprintf("Connection lost during write\n");
+		conn->action = C_DONE;
+	}
+	else if (n >= 0) {
+		conn->sentbytes += n;
+		if (conn->sentbytes == STRBUFLEN(conn->msgbuf)) {
+			conn->action = C_DONE;
+		}
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+	int daemonize = 1;
+	int timeout = 10;
+	int listenq = 10;
+	char *pidfile = "msgcache.pid";
+	int sockcount = 0;
+	int lsocket;
+	struct sockaddr_in laddr;
+	struct sigaction sa;
+	int opt;
+
+	/* Dont save the output from errprintf() */
+	save_errbuf = 0;
+
+	memset(&laddr, 0, sizeof(laddr));
+	inet_aton("0.0.0.0", (struct in_addr *) &laddr.sin_addr.s_addr);
+	laddr.sin_port = htons(1984);
+	laddr.sin_family = AF_INET;
+
+	for (opt=1; (opt < argc); opt++) {
+		if (argnmatch(argv[opt], "--listen=")) {
+			char *locaddr, *p;
+			int locport;
+
+			locaddr = strchr(argv[opt], '=')+1;
+			p = strchr(locaddr, ':');
+			if (p) { locport = atoi(p+1); *p = '\0'; } else locport = 1984;
+
+			memset(&laddr, 0, sizeof(laddr));
+			laddr.sin_port = htons(locport);
+			laddr.sin_family = AF_INET;
+			if (inet_aton(locaddr, (struct in_addr *) &laddr.sin_addr.s_addr) == 0) {
+				errprintf("Invalid listen address %s\n", locaddr);
+				return 1;
+			}
+		}
+		else if (argnmatch(argv[opt], "--timeout=")) {
+			char *p = strchr(argv[opt], '=');
+			timeout = atoi(p+1);
+		}
+		else if (argnmatch(argv[opt], "--lqueue=")) {
+			char *p = strchr(argv[opt], '=');
+			listenq = atoi(p+1);
+		}
+		else if (strcmp(argv[opt], "--daemon") == 0) {
+			daemonize = 1;
+		}
+		else if (strcmp(argv[opt], "--no-daemon") == 0) {
+			daemonize = 0;
+		}
+		else if (argnmatch(argv[opt], "--pidfile=")) {
+			char *p = strchr(argv[opt], '=');
+			pidfile = strdup(p+1);
+		}
+		else if (argnmatch(argv[opt], "--logfile=")) {
+			char *p = strchr(argv[opt], '=');
+			logfile = strdup(p+1);
+		}
+		else if (strcmp(argv[opt], "--debug") == 0) {
+			debug = 1;
+		}
+		else if (strcmp(argv[opt], "--version") == 0) {
+			printf("bbproxy version %s\n", VERSION);
+			return 0;
+		}
+	}
+
+	/* Set up a socket to listen for new connections */
+	lsocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (lsocket == -1) {
+		errprintf("Cannot create listen socket (%s)\n", strerror(errno));
+		return 1;
+	}
+	opt = 1;
+	setsockopt(lsocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	fcntl(lsocket, F_SETFL, O_NONBLOCK);
+	if (bind(lsocket, (struct sockaddr *)&laddr, sizeof(laddr)) == -1) {
+		errprintf("Cannot bind to listen socket (%s)\n", strerror(errno));
+		return 1;
+	}
+
+	if (listen(lsocket, listenq) == -1) {
+		errprintf("Cannot listen (%s)\n", strerror(errno));
+		return 1;
+	}
+
+	/* Redirect logging to the logfile, if requested */
+	if (logfile) {
+		freopen(logfile, "a", stdout);
+		freopen(logfile, "a", stderr);
+	}
+
+	errprintf("Hobbit msgcache version %s starting\n", VERSION);
+	errprintf("Listening on %s:%d\n", inet_ntoa(laddr.sin_addr), ntohs(laddr.sin_port));
+
+	if (daemonize) {
+		pid_t childpid;
+
+		freopen("/dev/null", "a", stdin);
+
+		/* Become a daemon */
+		childpid = fork();
+		if (childpid < 0) {
+			/* Fork failed */
+			errprintf("Could not fork\n");
+			exit(1);
+		}
+		else if (childpid > 0) {
+			/* Parent - save PID and exit */
+			FILE *fd = fopen(pidfile, "w");
+			if (fd) {
+				fprintf(fd, "%d\n", (int)childpid);
+				fclose(fd);
+			}
+			exit(0);
+		}
+		/* Child (daemon) continues here */
+		setsid();
+	}
+
+	setup_signalhandler("msgcache");
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = sigmisc_handler;
+	sigaction(SIGHUP, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+
+	do {
+		fd_set fdread, fdwrite;
+		int maxfd;
+		int n;
+		conn_t *cwalk, *cprev;
+		msgqueue_t *qwalk, *qprev;
+		time_t mintstamp;
+
+		FD_ZERO(&fdread);
+		FD_ZERO(&fdwrite);
+
+		/* Add the listen socket */
+		FD_SET(lsocket, &fdread); 
+		maxfd = lsocket;
+
+		for (cwalk = chead; (cwalk); cwalk = cwalk->next) {
+			switch (cwalk->action) {
+			  case C_READING:
+				FD_SET(cwalk->sockfd, &fdread); 
+				if (cwalk->sockfd > maxfd) maxfd = cwalk->sockfd; 
+				break;
+
+			  case C_WRITING:
+				FD_SET(cwalk->sockfd, &fdwrite);
+				if (cwalk->sockfd > maxfd) maxfd = cwalk->sockfd; 
+				break;
+
+			  case C_DONE:
+				break;
+			}
+		}
+
+		n = select(maxfd+1, &fdread, &fdwrite, NULL, NULL);
+
+		if (n < 0) {
+			if (errno == EINTR) continue;
+			errprintf("select failed: %s\n", strerror(errno));
+			return 0;
+		}
+
+		if (n == 0) continue; /* Timeout */
+
+		for (cwalk = chead; (cwalk); cwalk = cwalk->next) {
+			switch (cwalk->action) {
+			  case C_READING:
+				if (FD_ISSET(cwalk->sockfd, &fdread)) grabdata(cwalk);
+				break;
+
+			  case C_WRITING:
+				if (FD_ISSET(cwalk->sockfd, &fdwrite)) senddata(cwalk);
+				break;
+
+			  case C_DONE:
+				break;
+			}
+		}
+
+		if (FD_ISSET(lsocket, &fdread)) {
+			/* New incoming connection */
+			conn_t *newconn;
+			int caddrsize;
+
+			dprintf("New connection\n");
+			newconn = calloc(1, sizeof(conn_t));
+
+			caddrsize = sizeof(newconn->caddr);
+			newconn->sockfd = accept(lsocket, (struct sockaddr *)&newconn->caddr, &caddrsize);
+			if (newconn->sockfd == -1) {
+				/* accept() failure. Yes, it does happen! */
+				dprintf("accept failure, ignoring connection (%s), sockcount=%d\n", 
+					strerror(errno), sockcount);
+				xfree(newconn);
+				newconn = NULL;
+			}
+			else {
+				fcntl(newconn->sockfd, F_SETFL, O_NONBLOCK);
+				newconn->action = C_READING;
+				newconn->msgbuf = newstrbuffer(0);
+				newconn->tstamp = time(NULL);
+			}
+
+			if (newconn) {
+				if (ctail) { ctail->next = newconn; ctail = newconn; }
+				else chead = ctail = newconn;
+			}
+		}
+
+		/* Remove the finished connections */
+		cwalk = chead; cprev = NULL;
+		while (cwalk) {
+			conn_t *zombie;
+
+			if (cwalk->action != C_DONE) {
+				cprev = cwalk;
+				cwalk = cwalk->next;
+				continue;
+			}
+
+			/* Close the socket */
+			close(cwalk->sockfd);
+
+			zombie = cwalk;
+			if (cprev == NULL) {
+				chead = zombie->next;
+				cwalk = chead;
+				cprev = NULL;
+			}
+			else {
+				cprev->next = zombie->next;
+				cwalk = zombie->next;
+			}
+
+			freestrbuffer(zombie->msgbuf);
+			xfree(zombie);
+		}
+		if (!chead) ctail = NULL;
+
+		/* Remove expired messages */
+		qwalk = qhead; qprev = NULL;
+		mintstamp = time(NULL) - maxage;
+		while (qwalk) {
+			msgqueue_t *zombie;
+
+			if (qwalk->tstamp > mintstamp) {
+				/* Hasn't expired yet */
+				qprev = qwalk;
+				qwalk = qwalk->next;
+				continue;
+			}
+
+			zombie = qwalk;
+			if (qprev == NULL) {
+				qhead = zombie->next;
+				qwalk = qhead;
+				qprev = NULL;
+			}
+			else {
+				qprev->next = zombie->next;
+				qwalk = zombie->next;
+			}
+
+			freestrbuffer(zombie->msgbuf);
+			xfree(zombie);
+		}
+		if (!qhead) qtail = NULL;
+
+	} while (keeprunning);
+
+	if (pidfile) unlink(pidfile);
+	return 0;
+}
+
