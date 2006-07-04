86671db5798bafc50005ebe8398d57e2fd01a8aa
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-07-04 11:31:34 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4818 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/lib/suid.h
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit monitor library.                                                    */
+/*                                                                            */
+/* Copyright (C) 2006 Henrik Storner <henrik@hswn.dk>                         */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#ifndef __SUID_H__
+#define __SUID_H__
+
+extern void drop_root(void);
+extern void get_root(void);
+extern void drop_root_and_removesuid(char *fn);
+
+#endif
+
