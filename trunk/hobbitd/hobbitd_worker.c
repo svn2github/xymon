8ed30403957bdcd1f04b3ace74e08b846d26ed33
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-07-11 14:01:16 +0000

    Did some serious debugging of how we handled oversize messages and buffer wraps. This version has been tested with a lot of different scenarios.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@4895 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd_worker.c
+++ trunk/hobbitd/hobbitd_worker.c
@@ -12,7 +12,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: hobbitd_worker.c,v 1.25 2006-05-11 20:16:58 henrik Exp $";
+static char rcsid[] = "$Id: hobbitd_worker.c,v 1.26 2006-07-11 14:01:16 henrik Exp $";
 
 #include "config.h"
 
@@ -22,6 +22,7 @@ static char rcsid[] = "$Id: hobbitd_worker.c,v 1.25 2006-05-11 20:16:58 henrik E
 #include <ctype.h>
 
 #include <unistd.h>
+#include <fcntl.h>
 
 #ifdef HAVE_SYS_SELECT_H
 #include <sys/select.h>         /* Someday I'll move to GNU Autoconf for this ..  . */
@@ -37,21 +38,24 @@ static char rcsid[] = "$Id: hobbitd_worker.c,v 1.25 2006-05-11 20:16:58 henrik E
 #include "hobbitd_worker.h"
 
 
+#define EXTRABUFSPACE 4095
+
 unsigned char *get_hobbitd_message(enum msgchannels_t chnid, char *id, int *seq, struct timeval *timeout)
 {
 	static unsigned int seqnum = 0;
 	static char *idlemsg = NULL;
 	static char *buf = NULL;
 	static size_t bufsz = 0;
+	static size_t maxmsgsize = 0;
+	static int ioerror = 0;
+
 	static char *startpos;	/* Where our unused data starts */
 	static char *endpos;	/* Where the first message ends */
 	static char *fillpos;	/* Where our unused data ends (the \0 byte) */
-	static char *movebuf = NULL; /* Result buffer - used only when we need to shuffle data around */
-	static int ioerror = 0;
 
 	struct timeval cutoff;
 	struct timezone tz;
-	int needmoredata;
+	int maymove, needmoredata;
 	char *endsrch;		/* Where in the buffer do we start looking for the end-message marker */
 	char *result;
 
@@ -77,33 +81,18 @@ unsigned char *get_hobbitd_message(enum msgchannels_t chnid, char *id, int *seq,
 	 * a pointer to our input buffer. The only time we 
 	 * need to shuffle data around is if the buffer
 	 * does not have room left to hold a complete message.
-	 * In that case, we use the "movebuf" to hold the
-	 * current result message, and move the remainder of
-	 * data (the incomplete next message) to the start of
-	 * the buffer.
 	 */
-
-startagain:
-	if (ioerror) return NULL;
-
-	if (movebuf) {
-		/* 
-		 * movebuf is used to return a message sometimes. So
-		 * we free it when it has been used.
-		 */
-		xfree(movebuf);
-		movebuf = NULL;
-	}
 
 	if (buf == NULL) {
 		/*
 		 * Initial setup of the buffers.
 		 * We allocate a buffer large enough for the largest message
 		 * that can arrive on this channel, and add 4KB extra room.
-		 * The extra 4KB is to allow the memmove() that will be
+		 * The EXTRABUFSPACE is to allow the memmove() that will be
 		 * needed occasionally some room to work optimally.
 		 */
-		bufsz = 1024*(shbufsz(chnid) + 4);
+		maxmsgsize = 1024*shbufsz(chnid);
+		bufsz = maxmsgsize + EXTRABUFSPACE;
 		buf = (char *)malloc(bufsz+1);
 		*buf = '\0';
 		startpos = fillpos = buf;
@@ -111,6 +100,38 @@ startagain:
 
 		/* idlemsg is used to return the idle message in case of timeouts. */
 		idlemsg = strdup("@@idle\n");
+
+		/* We dont want to block when reading data. */
+		fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+	}
+
+	/*
+	 * If the start of the next message doesn't begin with "@@" then 
+	 * there's something rotten.
+	 * It might be some data left-over from an oversized message.
+	 */
+	if (*startpos && strncmp(startpos, "@@", 2) != 0) {
+		errprintf("Bad data in channel, skipping it\n");
+		startpos = strstr(startpos, "\n@@");
+		endpos = (startpos ? strstr(startpos, "\n@@\n") : NULL);
+		if (startpos && (startpos == endpos)) {
+			startpos = endpos + 4;
+			endpos = strstr(startpos, "\n@@\n");
+		}
+
+		if (!startpos) {
+			startpos = buf;
+			fillpos = buf;
+			endpos = NULL;
+		}
+
+		seqnum = 0; /* After skipping, we dont know what to expect */
+	}
+
+startagain:
+	if (ioerror) {
+		errprintf("get_hobbitd_message: Returning NULL due to previous i/o error\n");
+		return NULL;
 	}
 
 	if (timeout) {
@@ -131,6 +152,12 @@ startagain:
 	 */
 	endsrch = startpos;
 
+	/*
+	 * See if the current available buffer space is enough to hold a full message.
+	 * If not, then flag that we may do a memmove() of the buffer data.
+	 */
+	maymove = ((startpos + maxmsgsize) >= (buf + bufsz));
+
 	/* We only need to read data, if we do not have an end-of-message marker */
 	needmoredata = (endpos == NULL);
 	while (needmoredata) {
@@ -138,6 +165,32 @@ startagain:
 		struct timeval now, tmo;
 		fd_set fdread;
 		int res;
+		size_t bufleft = bufsz - (fillpos - buf);
+		size_t usedbytes = (fillpos - startpos);
+
+		dprintf("Want msg %d, startpos %ld, fillpos %ld, endpos %ld, usedbytes=%ld, bufleft=%ld\n",
+			  (seqnum+1), (startpos-buf), (fillpos-buf), (endpos ? (endpos-buf) : -1), usedbytes, bufleft);
+
+		if (usedbytes >= maxmsgsize) {
+			/* Over-size message. Truncate it. */
+			errprintf("Got over-size message, truncating at %d bytes (max: %d)\n", usedbytes, maxmsgsize);
+			endpos = startpos + maxmsgsize;
+			memcpy(endpos, "\n@@\n", 4);	/* Simulate end-of-message and flush data */
+			needmoredata = 0;
+			continue;
+		}
+
+		if (maymove && (bufleft < EXTRABUFSPACE)) {
+			/* Buffer is almost full - move data to accomodate a large message. */
+			dprintf("Moving %d bytes to start of buffer\n", usedbytes);
+			memmove(buf, startpos, usedbytes);
+			startpos = buf;
+			fillpos = startpos + usedbytes;
+			*fillpos = '\0';
+			endsrch = (usedbytes >= 4) ? (fillpos - 4) : startpos;
+			maymove = 0;
+			bufleft = bufsz - (fillpos - buf);
+		}
 
 		if (timeout) {
 			/* How long time until the timeout ? */
@@ -156,11 +209,13 @@ startagain:
 		res = select(STDIN_FILENO+1, &fdread, NULL, NULL, (timeout ? &tmo : NULL));
 
 		if (res < 0) {
+			if ((errno == EAGAIN) || (errno == EINTR)) continue;
+
 			/* Some error happened */
-			if (errno != EINTR) {
-				ioerror = 1;
-				return NULL;
-			}
+			ioerror = 1;
+			dprintf("get_hobbitd_message: Returning NULL due to select error %s\n",
+				  strerror(errno));
+			return NULL;
 		}
 		else if (res == 0) {
 			/* 
@@ -171,9 +226,16 @@ startagain:
 			return idlemsg;
 		}
 		else if (FD_ISSET(STDIN_FILENO, &fdread)) {
-			size_t bufleft = bufsz - (fillpos - buf);
 			res = read(STDIN_FILENO, fillpos, bufleft);
-			if (res <= 0) {
+			if (res < 0) {
+				if ((errno == EAGAIN) || (errno == EINTR)) continue;
+
+				ioerror = 1;
+				dprintf("get_hobbitd_message: Returning NULL due to read error %s\n",
+					  strerror(errno));
+				return NULL;
+			}
+			else if (res == 0) {
 				/* read() returns 0 --> End-of-file */
 				ioerror = 1;
 				return NULL;
@@ -202,18 +264,14 @@ startagain:
 	/* We have a complete message between startpos and endpos */
 	result = startpos;
 	*endpos = '\0';
-	startpos = endpos+4;
-	endpos = strstr(startpos, "\n@@\n");
+	startpos = endpos+4; /* +4 because we skip the "\n@@\n" end-marker from the previous message */
+	endpos = strstr(startpos, "\n@@\n");	/* To see if we already have a full message loaded */
+	/* fillpos stays where it is */
 
-	if ((endpos == NULL) && ((bufsz - (startpos - buf)) < (bufsz/2))) {
-		size_t usedbytes = (fillpos - startpos);
-
-		movebuf = strdup(result);
-		result = movebuf;
-		memmove(buf, startpos, usedbytes);
-		startpos = buf;
-		fillpos = startpos + usedbytes;
-		*fillpos = '\0';
+	/* Check that it really is a message, and not just some garbled data */
+	if (strncmp(result, "@@", 2) != 0) {
+		errprintf("Dropping (more) garbled data\n");
+		goto startagain;
 	}
 
 	/* Get and check the message sequence number */
@@ -246,6 +304,9 @@ startagain:
 			if (seqnum == 999999) seqnum = 0;
 		}
 	}
+
+	dprintf("startpos %ld, fillpos %ld, endpos %ld\n",
+		  (startpos-buf), (fillpos-buf), (endpos ? (endpos-buf) : -1));
 
 	return result;
 }
