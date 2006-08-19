02c7ba03abfb1d43559f3fdbdd238403ca7355d9
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2006-08-19 08:18:56 +0000

    Fix broken "top" handling with Solaris. TOP always refers to ONLY the "top" executable, not any options.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5054 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/bb-commands.sh
+++ trunk/build/bb-commands.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: bb-commands.sh,v 1.6 2006-06-21 05:50:38 henrik Exp $
+# $Id: bb-commands.sh,v 1.7 2006-08-19 08:18:56 henrik Exp $
 
 # Script to pick up most of the commands used by BB extension scripts.
 # This is used during installation, to build a hobbitserver.cfg that
@@ -23,22 +23,12 @@ echo "# The following defines a bunch of commands that BB extensions expect to b
 echo "# Hobbit does not use them, but they are provided here so if you use BB extension"
 echo "# scripts, then they will hopefully run without having to do a lot of tweaking."
 echo ""
-for CMD in uptime awk cat cp cut date egrep expr find grep head id ln ls mv rm sed sort tail touch tr uniq who
+for CMD in uptime awk cat cp cut date egrep expr find grep head id ln ls mv rm sed sort tail top touch tr uniq who
 do
 	ENVNAME=`echo $CMD | tr "[a-z]" "[A-Z]"`
 	PGM=`findbin $CMD | head -n 1`
 	echo "${ENVNAME}=\"${PGM}\""
 done
-
-# TOP can either be "top", or on Solaris the "prstat" command.
-PRSTAT=`findbin prstat | head -n 1`
-if test "$PRSTAT" != ""
-then
-	PGM="$PRSTAT -can 20 1 1"
-else
-	PGM=`findbin top | head -n 1`
-fi
-echo "TOP=\"${PGM}\""
 
 # WC is special
 PGM=`findbin wc | head -n 1`
