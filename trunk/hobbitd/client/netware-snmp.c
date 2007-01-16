4128d35a44ef5bfda281c1a2503a6c3b754f132a
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2007-01-16 10:25:19 +0000

    Generate "memory" status. Fix user/process counting
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5143 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/client/netware-snmp.c
+++ trunk/hobbitd/client/netware-snmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char netware_snmp__rcsid[] = "$Id: netware-snmp.c,v 1.1 2007-01-15 14:22:30 henrik Exp $";
+static char netware_snmp__rcsid[] = "$Id: netware-snmp.c,v 1.2 2007-01-16 10:25:19 henrik Exp $";
 
 void handle_netware_snmp_client(char *hostname, char *clienttype, enum ostype_t os, 
 				namelist_t *hinfo, char *sender, time_t timestamp,
@@ -23,6 +23,9 @@ void handle_netware_snmp_client(char *hostname, char *clienttype, enum ostype_t 
 	char *memorystr;
 	char *netstatstr;
 	char *portsstr;
+
+	static pcre *countexp = NULL;
+	int  pscount = 0, usercount = 0;
 
 	char fromline[1024];
 
@@ -46,43 +49,56 @@ void handle_netware_snmp_client(char *hostname, char *clienttype, enum ostype_t 
 		p = strchr(datestr, ','); if (p) *p = ' ';
 	}
 
-	unix_cpu_report(hostname, clienttype, os, hinfo, fromline, datestr, uptimestr, NULL, NULL, NULL, NULL, NULL);
+	/* Since we only have the counts for number of processes and users, we must get these ourselves */
+	if (!countexp) countexp = compileregex(", (\\d+) users, (\\d+) procs,");
+	{
+		char *buf = strdup(uptimestr);
+		char *c1, *c2;
+
+		c1 = c2 = NULL;
+		if (pickdata(buf, countexp, 0, &c1, &c2)) {
+			if (c1) { usercount = atoi(c1); xfree(c1); }
+			if (c2) { pscount = atoi(c2); xfree(c2); }
+		}
+		xfree(buf);
+	}
+
+	unix_cpu_report(hostname, clienttype, os, hinfo, fromline, datestr, uptimestr, NULL, NULL, 
+			NULL, pscount, NULL, usercount, NULL);
 	unix_disk_report(hostname, clienttype, os, hinfo, fromline, datestr, "Available", "Use%", "Mounted", dfstr);
 	unix_procs_report(hostname, clienttype, os, hinfo, fromline, datestr, "CMD", NULL, nlmstr);
 	unix_ports_report(hostname, clienttype, os, hinfo, fromline, datestr, 1, 2, 3, portsstr);
 
 	unix_netstat_report(hostname, clienttype, os, hinfo, fromline, datestr, netstatstr);
 
-#if 0
 	if (memorystr) {
 		char *p;
-		long memphystotal, memphysused, memphysfree,
-		     memactused, memactfree,
-		     memswaptotal, memswapused, memswapfree;
+		long memphystotal, memphysused, memactused,
+		     memallocated, memcodedata, memdos, memcachebufs;
 
-		memphystotal = memswaptotal = memphysused = memswapused = memactused = memactfree = -1;
-		p = strstr(freestr, "\nMem:");
-		if (p && (sscanf(p, "\nMem: %ld %ld %ld", &memphystotal, &memphysused, &memphysfree) == 3)) {
-			memphystotal /= 1024;
-			memphysused /= 1024;
-			memphysfree /= 1024;
-		}
-		p = strstr(freestr, "\nSwap:");
-		if (p && (sscanf(p, "\nSwap: %ld %ld %ld", &memswaptotal, &memswapused, &memswapfree) == 3)) {
-			memswaptotal /= 1024;
-			memswapused /= 1024;
-			memswapfree /= 1024;
-		}
-		p = strstr(freestr, "\n-/+ buffers/cache:");
-		if (p && (sscanf(p, "\n-/+ buffers/cache: %ld %ld", &memactused, &memactfree) == 2)) {
-			memactused /= 1024;
-			memactfree /= 1024;
+		memphystotal = memphysused = memactused = -1;
+		memallocated = memcodedata = memdos = memcachebufs = -1;
+
+		/* The "Total" is at the beginning of the message */
+		if (strncmp(memorystr, "Total ", 6) == 0) p = memorystr; else p = strstr(memorystr, "\nTotal ");
+		if (p && ((p = strchr(p, ':')) != NULL)) memphystotal = atol(p+1);
+		p = strstr(memorystr, "\nAllocated ");
+		if (p && ((p = strchr(p, ':')) != NULL)) memallocated = atol(p+1);
+		p = strstr(memorystr, "\nCode/Data ");
+		if (p && ((p = strchr(p, ':')) != NULL)) memcodedata = atol(p+1);
+		p = strstr(memorystr, "\nDOS memory ");
+		if (p && ((p = strchr(p, ':')) != NULL)) memdos = atol(p+1);
+		p = strstr(memorystr, "\nCache buffers ");
+		if (p && ((p = strchr(p, ':')) != NULL)) memcachebufs = atol(p+1);
+		if ((memallocated >= 0) && (memcodedata >= 0) && (memdos >= 0) && (memcachebufs >= 0)) {
+			memphysused = (memallocated + memcodedata + memdos + memcachebufs);
+			memactused = (memallocated + memcodedata + memdos);
 		}
 
-		unix_memory_report(hostname, clienttype, os, hinfo, fromline, timestr,
-				   memphystotal, memphysused, memactused, memswaptotal, memswapused);
+		if ((memphysused >= 0) && (memactused >= 0) && (memphystotal >= 0)) {
+			unix_memory_report(hostname, clienttype, os, hinfo, fromline, datestr,
+					   (memphystotal / 1024), (memphysused / 1024), (memactused / 1024), -1, -1);
+		}
 	}
-#endif
-
 }
 
