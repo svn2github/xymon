a90af0a78979ccddaeb6709f0980626facca9919
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2007-01-26 09:53:59 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5163 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/hobbitd/client/bbwin.c
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit message daemon.                                                     */
+/*                                                                            */
+/* Client backend module for BBWin/Windoes client                             */
+/*                                                                            */
+/* Copyright (C) 2006-2007 Henrik Storner <henrik@hswn.dk>                    */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char bbwin_rcsid[] = "$Id: bbwin.c,v 1.1 2007-01-26 09:53:59 henrik Exp $";
+
+void handle_win32_bbwin_client(char *hostname, char *clienttype, enum ostype_t os, 
+				namelist_t *hinfo, char *sender, time_t timestamp,
+				char *clientdata)
+{
+	/* Stub routine for the BBWin backend parser. Waiting for Etienne to provide this. */
+}
+
