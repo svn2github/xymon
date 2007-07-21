f0943181cc9a0579c6c85310d02ccbbd64e6e2fc
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2007-07-21 15:18:41 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5287 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/hobbitd/rrd/do_paging.c
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit RRD handler module.                                                 */
+/*                                                                            */
+/* This module handles "paging" messages.                                     */
+/*                                                                            */
+/* Copyright (C) 2006 Henrik Storner <henrik@hswn.dk>                         */
+/* Copyright (C) 2007 Rich Smrcina                                            */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char paging_rcsid[] = "$Id: do_paging.c,v 1.1 2007-07-21 15:18:41 henrik Exp $";
+
+static char *paging_params[] = { "rrdcreate", rrdfn, "DS:paging:GAUGE:600:0:U", NULL };
+static char *paging_tpl      = NULL;
+
+int do_paging_rrd(char *hostname, char *testname, char *msg, time_t tstamp) 
+{ 
+	char *p1, *p2, *p3, *pr;
+	char *fn = NULL;
+	int pagerate;
+
+	p1=(strstr(msg, "z/VM"));
+	p2=(strstr(msg, "z/VSE"));
+
+	if (p1 || p2) {
+		pr=(strstr(msg, "Rate"));
+		if (pr) {
+			pr += 5;
+			sscanf(pr, "%d per", &pagerate);
+			setupfn("paging.rrd", fn);
+
+			sprintf(rrdvalues, "%d:%d", (int)tstamp, pagerate);
+			create_and_update_rrd(hostname, testname, rrdfn, paging_params, paging_tpl);
+		}
+
+	}
+	return 0;
+}
+
