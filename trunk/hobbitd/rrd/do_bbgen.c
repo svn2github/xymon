e0870bd40fa40de7bc34cc3954e667d54cdee7bb
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2007-07-22 12:54:16 +0000

    Support the per-color status breakdown tracking and graphing
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5310 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/rrd/do_bbgen.c
+++ trunk/hobbitd/rrd/do_bbgen.c
@@ -1,14 +1,14 @@
 /*----------------------------------------------------------------------------*/
 /* Hobbit RRD handler module.                                                 */
 /*                                                                            */
-/* Copyright (C) 2004-2006 Henrik Storner <henrik@hswn.dk>                    */
+/* Copyright (C) 2004-2007 Henrik Storner <henrik@hswn.dk>                    */
 /*                                                                            */
 /* This program is released under the GNU General Public License (GPL),       */
 /* version 2. See the file "COPYING" for details.                             */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char bbgen_rcsid[] = "$Id: do_bbgen.c,v 1.15 2007-07-21 10:19:16 henrik Exp $";
+static char bbgen_rcsid[] = "$Id: do_bbgen.c,v 1.16 2007-07-22 12:54:16 henrik Exp $";
 
 int do_bbgen_rrd(char *hostname, char *testname, char *msg, time_t tstamp) 
 { 
@@ -18,42 +18,135 @@ int do_bbgen_rrd(char *hostname, char *testname, char *msg, time_t tstamp)
 					"DS:hostcount:GAUGE:600:0:U", "DS:statuscount:GAUGE:600:0:U", 
 					NULL };
 	static char *bbgen2_tpl      = NULL;
+	static char *bbgen3_params[] = { "rrdcreate", rrdfn,
+					 "DS:redcount:GAUGE:600:0:U", "DS:rednopropcount:GAUGE:600:0:U",
+					 "DS:yellowcount:GAUGE:600:0:U", "DS:yellownopropcount:GAUGE:600:0:U",
+					 "DS:greencount:GAUGE:600:0:U",
+					 "DS:purplecount:GAUGE:600:0:U",
+					 "DS:clearcount:GAUGE:600:0:U",
+					 "DS:bluecount:GAUGE:600:0:U",
+					 "DS:redpct:GAUGE:600:0:100", "DS:rednoproppct:GAUGE:600:0:100",
+					 "DS:yellowpct:GAUGE:600:0:100", "DS:yellownoproppct:GAUGE:600:0:100",
+					 "DS:greenpct:GAUGE:600:0:100",
+					 "DS:purplepct:GAUGE:600:0:100",
+					 "DS:clearpct:GAUGE:600:0:100",
+					 "DS:bluepct:GAUGE:600:0:100",
+					NULL };
+	static char *bbgen3_tpl      = NULL;
 
-	char	*p;
+	char	*p, *bol, *eoln;
 	float	runtime;
 	int	hostcount, statuscount;
+	int	redcount, rednopropcount, yellowcount, yellownopropcount,
+		greencount, purplecount, clearcount, bluecount;
+	double	pctredcount, pctrednopropcount, pctyellowcount, pctyellownopropcount,
+		pctgreencount, pctpurplecount, pctclearcount, pctbluecount;
 
 	if (bbgen_tpl == NULL) bbgen_tpl = setup_template(bbgen_params);
 	if (bbgen2_tpl == NULL) bbgen2_tpl = setup_template(bbgen2_params);
+	if (bbgen3_tpl == NULL) bbgen3_tpl = setup_template(bbgen3_params);
 
-	p = strstr(msg, "TIME TOTAL");
-	if (p && (sscanf(p, "TIME TOTAL %f", &runtime) == 1)) {
-		if (strcmp("bbgen", testname) != 0) {
-			setupfn("bbgen.%s.rrd", testname);
+	runtime = 0.0;
+	hostcount = statuscount = 0;
+	redcount = rednopropcount = yellowcount = yellownopropcount = 0;
+	greencount = purplecount = clearcount = bluecount = 0;
+	pctredcount = pctrednopropcount = pctyellowcount = pctyellownopropcount = 0.0;
+	pctgreencount = pctpurplecount = pctclearcount = pctbluecount = 0.0;
+
+	bol = msg;
+	do {
+		int *valptr = NULL;
+		double *pctvalptr = NULL;
+
+		eoln = strchr(bol, '\n'); if (eoln) *eoln = '\0';
+
+		p = bol + strspn(bol, " \t");
+		if (strncmp(p, "TIME TOTAL", 10) == 0) sscanf(p, "TIME TOTAL %f", &runtime);
+		else if (strncmp(p, "Hosts", 5) == 0) valptr = &hostcount;
+		else if (strncmp(p, "Status messages", 15) == 0) valptr = &statuscount;
+		else if (strncmp(p, "- Red (non-propagating)", 23) == 0) {
+			valptr = &rednopropcount;
+			pctvalptr = &pctrednopropcount;
 		}
-		else {
-			strcpy(rrdfn, "bbgen.rrd");
+		else if (strncmp(p, "- Red", 5) == 0) {
+			valptr = &redcount;
+			pctvalptr = &pctredcount;
 		}
-		sprintf(rrdvalues, "%d:%.2f", (int)tstamp, runtime);
-		create_and_update_rrd(hostname, testname, rrdfn, bbgen_params, bbgen_tpl);
+		else if (strncmp(p, "- Yellow (non-propagating)", 26) == 0) {
+			valptr = &yellownopropcount;
+			pctvalptr = &pctyellownopropcount;
+		}
+		else if (strncmp(p, "- Yellow", 8) == 0) {
+			valptr = &yellowcount;
+			pctvalptr = &pctyellowcount;
+		}
+		else if (strncmp(p, "- Green", 7) == 0) {
+			valptr = &greencount;
+			pctvalptr = &pctgreencount;
+		}
+		else if (strncmp(p, "- Purple", 8) == 0) {
+			valptr = &purplecount;
+			pctvalptr = &pctpurplecount;
+		}
+		else if (strncmp(p, "- Clear", 7) == 0) {
+			valptr = &clearcount;
+			pctvalptr = &pctclearcount;
+		}
+		else if (strncmp(p, "- Blue", 6) == 0) {
+			valptr = &bluecount;
+			pctvalptr = &pctbluecount;
+		}
+
+		if (valptr) {
+			p = strchr(bol, ':');
+			if (p) {
+				*valptr = atoi(p+1);
+
+				if (pctvalptr) {
+					p = strchr(p, '(');
+					if (p) *pctvalptr = atof(p+1);
+				}
+			}
+		}
+
+		bol = (eoln ? eoln+1 : NULL);
+	} while (bol);
+
+
+	if (strcmp("bbgen", testname) != 0) {
+		setupfn("bbgen.%s.rrd", testname);
 	}
-
-	hostcount = statuscount = -1;
-	p = strstr(msg, "\n Hosts");
-	if (!p || (sscanf(p+1, " Hosts : %d", &hostcount) != 1)) hostcount = -1;
-	p = strstr(msg, "\n Status messages");
-	if (!p || (sscanf(p+1, " Status messages : %d", &statuscount) != 1)) statuscount = -1;
-
-	if ((hostcount != -1) && (statuscount != -1)) {
-		if (strcmp("bbgen", testname) != 0) {
-			setupfn("hobbit.%s.rrd", testname);
-		}
-		else {
-			strcpy(rrdfn, "hobbit.rrd");
-		}
-		sprintf(rrdvalues, "%d:%d:%d", (int)tstamp, hostcount, statuscount);
-		create_and_update_rrd(hostname, testname, rrdfn, bbgen2_params, bbgen2_tpl);
+	else {
+		strcpy(rrdfn, "bbgen.rrd");
 	}
+	sprintf(rrdvalues, "%d:%.2f", (int)tstamp, runtime);
+	create_and_update_rrd(hostname, testname, rrdfn, bbgen_params, bbgen_tpl);
+
+
+	if (strcmp("bbgen", testname) != 0) {
+		setupfn("hobbit.%s.rrd", testname);
+	}
+	else {
+		strcpy(rrdfn, "hobbit.rrd");
+	}
+	sprintf(rrdvalues, "%d:%d:%d", (int)tstamp, hostcount, statuscount);
+	create_and_update_rrd(hostname, testname, rrdfn, bbgen2_params, bbgen2_tpl);
+
+
+	if (strcmp("bbgen", testname) != 0) {
+		setupfn("hobbit2.%s.rrd", testname);
+	}
+	else {
+		strcpy(rrdfn, "hobbit2.rrd");
+	}
+	sprintf(rrdvalues, "%d:%d:%d:%d:%d:%d:%d:%d:%d:%5.2f:%5.2f:%5.2f:%5.2f:%5.2f:%5.2f:%5.2f:%5.2f", 
+		(int)tstamp, 
+		redcount, rednopropcount, yellowcount, yellownopropcount,
+		greencount, purplecount, clearcount, bluecount,
+		pctredcount, pctrednopropcount, pctyellowcount, pctyellownopropcount,
+		pctgreencount, pctpurplecount, pctclearcount, pctbluecount);
+	create_and_update_rrd(hostname, testname, rrdfn, bbgen3_params, bbgen3_tpl);
+
 
 	return 0;
 }
