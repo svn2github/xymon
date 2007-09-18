fa7f9bdd663b62aaaf42d8f1af8202436ff57a90
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2007-09-18 08:30:09 +0000

    Revert use of msort() - it is broken.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5378 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/bbdisplay/pagegen.c
+++ trunk/bbdisplay/pagegen.c
@@ -11,7 +11,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char rcsid[] = "$Id: pagegen.c,v 1.190 2007-09-12 19:37:51 henrik Exp $";
+static char rcsid[] = "$Id: pagegen.c,v 1.191 2007-09-18 08:30:09 henrik Exp $";
 
 #include <limits.h>
 #include <stdio.h>
@@ -307,21 +307,13 @@ void setup_htaccess(const char *pagepath)
 	}
 }
 
-static int host_t_compare(void *a, void *b)
+static int host_t_compare(const void *v1, const void *v2)
 {
-	return (strcmp(((host_t *)a)->hostname, ((host_t *)b)->hostname) < 0);
-}
+	host_t **n1 = (host_t **)v1;
+	host_t **n2 = (host_t **)v2;
 
-static void * host_t_getnext(void *a)
-{
-	return ((host_t *)a)->next;
+	return strcmp((*n1)->hostname, (*n2)->hostname);
 }
-
-static void host_t_setnext(void *a, void *newval)
-{
-	((host_t *)a)->next = (host_t *)newval;
-}
-
 
 void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FILE *output, FILE *rssoutput, char *grouptitle, int pagetype, char *pagepath)
 {
@@ -372,7 +364,22 @@ void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FIL
 		fprintf(output, "<CENTER><TABLE SUMMARY=\"Group Block\" BORDER=0 CELLPADDING=2>\n");
 
 		/* Generate the host rows */
-		if (sorthosts) msort(head, host_t_compare, host_t_getnext, host_t_setnext);
+		if (sorthosts) {
+			int i, hcount = 0;
+			host_t **hlist;
+
+			for (h=head; (h); h=h->next) hcount++;
+			hlist = (host_t **) calloc((hcount+1), sizeof(host_t *));
+			for (h=head, i=0; (h); h=h->next, i++) hlist[i] = h;
+			qsort(hlist, hcount, sizeof(host_t *), host_t_compare);
+
+			for (h=head=hlist[0], i=1; (i <= hcount); i++) { 
+				h->next = hlist[i];
+				h = h->next;
+			}
+			xfree(hlist);
+		}
+
 		for (h = head; (h); h = h->next) {
 			/* If there is a host pretitle, show it. */
 			dbgprintf("Host:%s, pretitle:%s\n", h->hostname, textornull(h->pretitle));
