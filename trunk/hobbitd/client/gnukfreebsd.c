4021dd01043ae48bb933697d66d662b6a4c0f01c
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-01-28 11:18:44 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5523 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/hobbitd/client/gnukfreebsd.c
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*/
+/* Hobbit message daemon.                                                     */
+/*                                                                            */
+/* Client backend module for GNU/kFreeBSD                                     */
+/*                                                                            */
+/* Copyright (C) 2008 Henrik Storner <henrik@hswn.dk>                         */
+/*                                                                            */
+/* This program is released under the GNU General Public License (GPL),       */
+/* version 2. See the file "COPYING" for details.                             */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+static char gnukfreebsd_rcsid[] = "$Id: gnukfreebsd.c,v 1.1 2008-01-28 11:18:44 henrik Exp $";
+
+void handle_gnukfreebsd_client(char *hostname, char *clienttype, enum ostype_t os, 
+				void *hinfo, char *sender, time_t timestamp,
+				char *clientdata)
+{
+	char *timestr;
+	char *uptimestr;
+	char *clockstr;
+	char *msgcachestr;
+	char *whostr;
+	char *psstr;
+	char *topstr;
+	char *dfstr;
+	char *msgsstr;
+	char *netstatstr;
+	char *vmstatstr;
+	char *ifstatstr;
+	char *portsstr;
+
+	char fromline[1024];
+
+	sprintf(fromline, "\nStatus message received from %s\n", sender);
+
+	splitmsg(clientdata);
+
+	timestr = getdata("date");
+	uptimestr = getdata("uptime");
+	clockstr = getdata("clock");
+	msgcachestr = getdata("msgcache");
+	whostr = getdata("who");
+	psstr = getdata("ps");
+	topstr = getdata("top");
+	dfstr = getdata("df");
+	msgsstr = getdata("msgs");
+	netstatstr = getdata("netstat");
+	ifstatstr = getdata("ifstat");
+	vmstatstr = getdata("vmstat");
+	portsstr = getdata("ports");
+
+	unix_cpu_report(hostname, clienttype, os, hinfo, fromline, timestr, uptimestr, clockstr, msgcachestr, 
+			whostr, 0, psstr, 0, topstr);
+	unix_disk_report(hostname, clienttype, os, hinfo, fromline, timestr, "Available", "Capacity", "Mounted", dfstr);
+	unix_procs_report(hostname, clienttype, os, hinfo, fromline, timestr, "CMD", NULL, psstr);
+	unix_ports_report(hostname, clienttype, os, hinfo, fromline, timestr, 3, 4, 5, portsstr);
+
+	msgs_report(hostname, clienttype, os, hinfo, fromline, timestr, msgsstr);
+	file_report(hostname, clienttype, os, hinfo, fromline, timestr);
+	linecount_report(hostname, clienttype, os, hinfo, fromline, timestr);
+
+	unix_netstat_report(hostname, clienttype, os, hinfo, fromline, timestr, netstatstr);
+	unix_ifstat_report(hostname, clienttype, os, hinfo, fromline, timestr, ifstatstr);
+	unix_vmstat_report(hostname, clienttype, os, hinfo, fromline, timestr, vmstatstr);
+}
+
