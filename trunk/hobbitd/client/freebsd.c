b47c5d4aef125c97b01e384d4dde150d02738f0c
henrik <henrik@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-03-27 12:24:12 +0000

    Use data from "sysctl vm.vmtotal" for free memory instead of the home-grown meminfo utility
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5611 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/client/freebsd.c
+++ trunk/hobbitd/client/freebsd.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-static char freebsd_rcsid[] = "$Id: freebsd.c,v 1.22 2008-01-03 10:10:39 henrik Exp $";
+static char freebsd_rcsid[] = "$Id: freebsd.c,v 1.23 2008-03-27 12:24:12 henrik Exp $";
 
 void handle_freebsd_client(char *hostname, char *clienttype, enum ostype_t os, 
 			   void *hinfo, char *sender, time_t timestamp,
@@ -26,6 +26,7 @@ void handle_freebsd_client(char *hostname, char *clienttype, enum ostype_t os,
 	char *dfstr;
 	char *meminfostr;
 	char *swapinfostr;
+	char *vmtotalstr;
 	char *msgsstr;
 	char *netstatstr;
 	char *ifstatstr;
@@ -34,6 +35,10 @@ void handle_freebsd_client(char *hostname, char *clienttype, enum ostype_t os,
 
 	char *p;
 	char fromline[1024];
+
+	unsigned long memphystotal = 0, memphysfree = 0, memphysused = 0;
+	unsigned long memswaptotal = 0, memswapfree = 0, memswapused = 0;
+	int found = 0;
 
 	sprintf(fromline, "\nStatus message received from %s\n", sender);
 
@@ -69,45 +74,48 @@ void handle_freebsd_client(char *hostname, char *clienttype, enum ostype_t os,
 	unix_ifstat_report(hostname, clienttype, os, hinfo, fromline, timestr, ifstatstr);
 	unix_vmstat_report(hostname, clienttype, os, hinfo, fromline, timestr, vmstatstr);
 
-	if (meminfostr && swapinfostr) {
-		unsigned long memphystotal, memphysfree, memphysused;
-		unsigned long memswaptotal, memswapfree, memswapused;
-		int found = 0;
-
-		memphystotal = memphysfree = memphysused = 0;
-		memswaptotal = memswapfree = memswapused = 0;
-
+	if (meminfostr) {
 		p = strstr(meminfostr, "Total:"); if (p) { memphystotal = atol(p+6); found++; }
+	}
+
+	if (vmtotalstr) {
+		p = strstr(vmtotalstr, "\nFree Memory Pages:");
+		if (p) {
+			memphysfree = atol(p + 18);
+			found++;
+		}
+	}
+
+	if ((found == 1) && meminfostr) {
 		p = strstr(meminfostr, "Free:");  if (p) { memphysfree  = atol(p+5); found++; }
 		memphysused = memphystotal - memphysfree;
+	}
 
-		memswaptotal = memswapfree = memswapused = 0;
-		if (swapinfostr) {
-			found++;
-			p = strchr(swapinfostr, '\n'); /* Skip the header line */
-			while (p) {
-				long stot, sused, sfree;
-				char *bol;
+	if (swapinfostr) {
+		found++;
+		p = strchr(swapinfostr, '\n'); /* Skip the header line */
+		while (p) {
+			long stot, sused, sfree;
+			char *bol;
 				
-				bol = p+1;
-				p = strchr(bol, '\n'); if (p) *p = '\0';
+			bol = p+1;
+			p = strchr(bol, '\n'); if (p) *p = '\0';
 
-				if (sscanf(bol, "%*s %ld %ld %ld", &stot, &sused, &sfree) == 3) {
-					memswaptotal += stot;
-					memswapused += sused;
-					memswapfree += sfree;
-				}
-
-				if (p) *p = '\n';
+			if (sscanf(bol, "%*s %ld %ld %ld", &stot, &sused, &sfree) == 3) {
+				memswaptotal += stot;
+				memswapused += sused;
+				memswapfree += sfree;
 			}
 
-			memswaptotal /= 1024; memswapused /= 1024; memswapfree /= 1024;
+			if (p) *p = '\n';
 		}
 
-		if (found >= 2) {
-			unix_memory_report(hostname, clienttype, os, hinfo, fromline, timestr,
-				   memphystotal, memphysused, -1, memswaptotal, memswapused);
-		}
+		memswaptotal /= 1024; memswapused /= 1024; memswapfree /= 1024;
+	}
+
+	if (found >= 2) {
+		unix_memory_report(hostname, clienttype, os, hinfo, fromline, timestr,
+			   memphystotal, memphysused, -1, memswaptotal, memswapused);
 	}
 }
 
