eac78d35db96218ee881b4062a21683598c2eb8e
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-08-05 20:59:20 +0000

    Work around C-ARES 1.5.2 broken handling of librt as a required runtime library.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5687 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/build/clock-gettime-librt.sh
@@ -0,0 +1,21 @@
+	echo "Checking for clock_gettime() requiring librt ..."
+
+	LIBRTDEF=""
+
+	cd build
+	OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt clean
+	OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt test-link
+	if [ $? -ne 0 ]; then
+		OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt test-link-rt
+		if [ $? -eq 0 ]; then
+			echo "clock_gettime() requires librt"
+			LIBRTDEF="-lrt"
+		else
+			echo "clock_gettime() not present, but this should be OK"
+		fi
+
+		OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt clean
+	fi
+
+	cd ..
+
