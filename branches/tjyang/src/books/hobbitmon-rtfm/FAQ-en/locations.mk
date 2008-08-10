544859165a14a82853465e7fa3f0ba92a3018fe9
tjyang <tjyang@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-08-10 14:06:54 +0000

    file/dirs clean up
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5722 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/tjyang/src/books/hobbitmon-rtfm/FAQ-en/locations.mk
+++
@@ -1,8 +0,0 @@
-# the locations of files that are targets of the Makefile rules
-
-WEB	= "web site main source directory"
-WEB_BETA = "web site beta source directory"
-CGI_BIN	= "web site CGI directory"
-HOME_DIR = "where sources live"
-CTAN_ROOT = "position of CTAN root on archive machine"
-FTPDIR = "directory for off-archive FTP distribution"
