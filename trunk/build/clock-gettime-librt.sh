96b4ccea26e8d753af97edae0bd427fcc578d659
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-10-01 12:39:07 +0000

    Fix typo
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5821 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/clock-gettime-librt.sh
+++ trunk/build/clock-gettime-librt.sh
@@ -6,7 +6,7 @@
 	OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt clean
 	OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt test-link 1>/dev/null 2>&1
 	if [ $? -ne 0 ]; then
-		OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt test-link-rt 1>&dev/null 2>&1
+		OS=`uname -s | tr '[/]' '[_]'` $MAKE -f Makefile.test-clockgettime-librt test-link-rt 1>/dev/null 2>&1
 		if [ $? -eq 0 ]; then
 			echo "clock_gettime() requires librt"
 			LIBRTDEF="-lrt"
