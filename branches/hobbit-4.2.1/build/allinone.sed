d8a26f309cd6dc3c942c90a7b4902adca690da94
tjyang <tjyang@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-11-06 22:19:27 +0000

    delete README.4.2.1 hobbit-4.2.1
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5899 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/hobbit-4.2.1/build/allinone.sed
+++
@@ -1,1 +0,0 @@
-s!^Last updated:.*$!Last updated: 2006-08-08 21:13 UTC!
--- branches/hobbit-4.2.1/build/pedanticcheck.sh
+++
@@ -1,3 +0,0 @@
-make -s -f Makefile.home clean
-make -s -f Makefile.home 2>&1|egrep -v "unused|not used|In function|Ved..verste niveau|In file included|^ *from |misc.h:38|^Checking for|^config.h |misc.h:.*long long|At top level|signedness"
-
