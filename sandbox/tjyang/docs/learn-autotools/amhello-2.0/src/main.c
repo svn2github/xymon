6bd2fd8a2df93550cf395f8c3b912f023149c664
tjyang <tjyang@44351d6e-118b-4698-b696-ce33095ecaa4>
2008-11-11 01:05:07 +0000

    add docs directory
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@5904 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ sandbox/tjyang/docs/learn-autotools/amhello-2.0/src/main.c
@@ -0,0 +1,11 @@
+/* Copyright (C) 2006 Free Software Foundation, Inc.
+ *    This program is free software; the Free Software Foundation
+ *       gives unlimited permission to copy, distribute and modify it.  */
+
+#include "say.h"
+int main (void)
+{
+  say_hello ();
+  return 0;
+}
+
