b9a7d51b5479c10d2caea46c8384040a02f7c92b
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2009-02-08 18:10:31 +0000

    Annual copyright update
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6093 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.0/bbnet/bbtest-net.h
+++ branches/4.3.0/bbnet/bbtest-net.h
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /* Hobbit monitor network test tool.                                          */
 /*                                                                            */
-/* Copyright (C) 2003-2006 Henrik Storner <henrik@hswn.dk>                    */
+/* Copyright (C) 2003-2009 Henrik Storner <henrik@hswn.dk>                    */
 /*                                                                            */
 /* This program is released under the GNU General Public License (GPL),       */
 /* version 2. See the file "COPYING" for details.                             */
