f68b3f167034deb004d4fff11f01d92092aeaa2e
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2009-02-19 10:36:00 +0000

    Fix broken msort() function, and change caller accordingly.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6143 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.4.0/lib/msort.h
+++ branches/4.4.0/lib/msort.h
@@ -11,7 +11,7 @@
 #ifndef __MSORT_H__
 #define __MSORT_H__
 
-typedef int (msortcompare_fn_t)(void *, void *);
+typedef int (msortcompare_fn_t)(void **, void **);
 typedef void * (msortgetnext_fn_t)(void *);
 typedef void (msortsetnext_fn_t)(void *, void *);
 extern void *msort(void *head, msortcompare_fn_t comparefn, msortgetnext_fn_t getnext, msortsetnext_fn_t setnext);
