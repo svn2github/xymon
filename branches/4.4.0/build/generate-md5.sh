16a65b2adf8939806828a0c46fea1fd930545358
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2009-02-19 12:28:54 +0000

    Drop premature 4.4.0 tree - makes merging from 4.3.0 easier.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6148 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.4.0/build/generate-md5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# cd ~/hobbit
-
-WEBLIST=`(cd hobbitd; find webfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
-WWWLIST=`(cd hobbitd; find wwwfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
-
-(cat build/md5.dat; for F in $WEBLIST $WWWLIST; do echo "`./common/bbdigest md5 hobbitd/$F` $F"; done) | sort -k2 | uniq
-
