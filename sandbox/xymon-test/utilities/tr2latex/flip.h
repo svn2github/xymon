31c192615f5a932e44037278fb5c9838ca08f42e
tjyang <tjyang@44351d6e-118b-4698-b696-ce33095ecaa4>
2010-03-13 15:17:46 +0000

    create utilities directory to hold directory like tr2latex and latex2man ...
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6280 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ sandbox/xymon-test/utilities/tr2latex/flip.h
@@ -0,0 +1,19 @@
+#ifndef FLIP__H
+#define FLIP__H
+
+/*
+** tr2latex - troff to LaTeX converter
+** COPYRIGHT (C) 1987 Kamal Al-Yahya, 1991,1992 Christian Engel
+**
+** Module: flip.h
+**
+** This file contains the words that are placed the opposite way
+** in troff and TeX. Is there any more?
+*/
+
+const char *flip_list[] =
+{
+"bar",  "dot",   "dotdot",   "hat",    "tilde",   "under",     "vec"
+};
+
+#endif
---
+++ sandbox/xymon-test/utilities/tr2latex/greek.h
@@ -0,0 +1,22 @@
+#ifndef GREEK__H
+#define GREEK__H
+
+/*
+** tr2latex - troff to LaTeX converter
+** COPYRIGHT (C) 1987 Kamal Al-Yahya, 1991,1992 Christian Engel
+**
+** Module: greek.h
+**
+** This file contains the list of the upper-case Greek letters.
+** In case I overlooked any, it can be added here.
+*/
+
+const char *GRK_list[] =
+{
+"ALPHA",    "BETA",    "CHI",    "DELTA",   "EPSILON",
+"ETA",      "GAMMA",   "KAPPA",  "LAMDA",   "MU",
+"NU",       "OMEGA",   "PHI",    "PI",      "PSI",
+"RHO",      "SIGMA",   "TAU",    "THETA",   "XI",    "ZETA"
+};
+
+#endif
