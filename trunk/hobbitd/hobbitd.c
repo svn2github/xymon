0486237ed6cc9e6ecee57fd044ff9ce254b25658
tjyang <tjyang@44351d6e-118b-4698-b696-ce33095ecaa4>
2010-06-27 14:10:28 +0000

    a simple typo correction, errormessages -> error messages
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6312 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/hobbitd/hobbitd.c
+++ trunk/hobbitd/hobbitd.c
@@ -680,7 +680,7 @@ char *generate_stats(void)
 	}
 
 	if (errbuf) {
-		addtobuffer(statsbuf, "\n\nLatest errormessages:\n");
+		addtobuffer(statsbuf, "\n\nLatest error messages:\n");
 		addtobuffer(statsbuf, errbuf);
 		addtobuffer(statsbuf, "\n");
 	}
