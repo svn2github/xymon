931b242981da607174cad5dffa8c12e673392c01
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2010-10-04 21:04:17 +0000

    Linux client: lsb_release may be in /usr/bin . From Japheth Cleaver.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6396 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.0/client/hobbitclient-linux.sh
+++ branches/4.3.0/client/hobbitclient-linux.sh
@@ -19,6 +19,9 @@ echo "[osversion]"
 if [ -x /bin/lsb_release ]; then
 	/bin/lsb_release -r -i -s | xargs echo
 	/bin/lsb_release -a
+elif [ -x /usr/bin/lsb_release ]; then
+	/usr/bin/lsb_release -r -i -s | xargs echo
+	/usr/bin/lsb_release -a
 elif [ -f /etc/redhat-release ]; then
 	cat /etc/redhat-release
 elif [ -f /etc/gentoo-release ]; then
