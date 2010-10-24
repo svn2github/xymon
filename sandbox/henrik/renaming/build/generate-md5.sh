d3f7c6f74bcb73938d65b158dc15ab074ce97aed
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2010-10-24 16:07:26 +0000

    Renaming: bbdigest -> xymondigest
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6510 44351d6e-118b-4698-b696-ce33095ecaa4

--- sandbox/henrik/renaming/build/generate-md5.sh
+++ sandbox/henrik/renaming/build/generate-md5.sh
@@ -5,5 +5,5 @@
 WEBLIST=`(cd hobbitd; find webfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
 WWWLIST=`(cd hobbitd; find wwwfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
 
-(cat build/md5.dat; for F in $WEBLIST $WWWLIST; do echo "`./common/bbdigest md5 hobbitd/$F` $F"; done) | sort -k2 | uniq
+(cat build/md5.dat; for F in $WEBLIST $WWWLIST; do echo "`./common/xymondigest md5 hobbitd/$F` $F"; done) | sort -k2 | uniq
 
