f736e4fdc8d1d385bb68e95dd576f813ecd54cc0
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2010-10-29 09:49:54 +0000

    Environment settings: Allow for easy append-to variables, so we can easily add extra definitions to the long variables (TEST2RRD, GRAPHS etc).
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6546 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.0/lib/environ.c
+++ branches/4.3.0/lib/environ.c
@@ -220,35 +220,61 @@ void loadenv(char *envfile, char *area)
 	fd = stackfopen(envfile, "r", NULL);
 	if (fd) {
 		while (stackfgets(inbuf, NULL)) {
+			char *equalpos;
+			int appendto = 0;
+
 			sanitize_input(inbuf, 1, 1);
 
-			if (STRBUFLEN(inbuf) && strchr(STRBUF(inbuf), '=')) {
-				/*
-				 * Do the environment "area" stuff: If the input
-				 * is of the form AREA/NAME=VALUE, then setup the variable
-				 * only if we're called with the correct AREA setting.
-				 */
-				oneenv = NULL;
+			if ((STRBUFLEN(inbuf) == 0) || ((equalpos = strchr(STRBUF(inbuf), '=')) == NULL)) continue;
 
-				p = STRBUF(inbuf) + strcspn(STRBUF(inbuf), "=/");
-				if (*p == '/') {
-					if (area) {
-						*p = '\0';
-						if (strcasecmp(STRBUF(inbuf), area) == 0) oneenv = strdup(expand_env(p+1));
+			appendto = ((equalpos > STRBUF(inbuf)) && (*(equalpos-1) == '+'));
+
+			/*
+			 * Do the environment "area" stuff: If the input
+			 * is of the form AREA/NAME=VALUE, then setup the variable
+			 * only if we're called with the correct AREA setting.
+			 */
+			oneenv = NULL;
+
+			p = STRBUF(inbuf) + strcspn(STRBUF(inbuf), "=/");
+			if (*p == '/') {
+				if (area) {
+					*p = '\0';
+					if (strcasecmp(STRBUF(inbuf), area) == 0) oneenv = strdup(expand_env(p+1));
+				}
+			}
+			else oneenv = strdup(expand_env(STRBUF(inbuf)));
+
+			if (oneenv) {
+				p = strchr(oneenv, '=');
+				if (*(p+1) == '"') {
+					/* Move string over the first '"' */
+					memmove(p+1, p+2, strlen(p+2)+1);
+					/* Kill a trailing '"' */
+					if (*(oneenv + strlen(oneenv) - 1) == '"') *(oneenv + strlen(oneenv) - 1) = '\0';
+				}
+
+				if (appendto) {
+					char *oldval, *addstring, *p;
+
+					addstring = strchr(oneenv, '='); if (addstring) { *addstring = '\0'; addstring++; }
+					p = strchr(oneenv, '+'); if (p) *p = '\0';
+
+					oldval = getenv(oneenv);
+					if (oldval) {
+						char *combinedenv = (char *)malloc(strlen(oneenv) + strlen(oldval) + strlen(addstring) + 2);
+						sprintf(combinedenv, "%s=%s%s", oneenv, oldval, (addstring));
+						xfree(oneenv);
+						oneenv = combinedenv;
+					}
+					else {
+						/* oneenv is now VARxxVALUE, so fix it to be a normal env. variable format */
+						strcat(oneenv, "=");
+						memmove(oneenv+strlen(oneenv), addstring, strlen(addstring) + 1);
 					}
 				}
-				else oneenv = strdup(expand_env(STRBUF(inbuf)));
 
-				if (oneenv) {
-					p = strchr(oneenv, '=');
-					if (*(p+1) == '"') {
-						/* Move string over the first '"' */
-						memmove(p+1, p+2, strlen(p+2)+1);
-						/* Kill a trailing '"' */
-						if (*(oneenv + strlen(oneenv) - 1) == '"') *(oneenv + strlen(oneenv) - 1) = '\0';
-					}
-					n = putenv(oneenv);
-				}
+				n = putenv(oneenv);
 			}
 		}
 		stackfclose(fd);
