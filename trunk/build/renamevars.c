713445297dd719897247e6cda4e97c81e786abea
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2011-03-08 13:05:32 +0000

    trunk: Merge 4.3.0 back into trunk, part 3: Add new files
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6648 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ trunk/build/renamevars.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv)
+{
+	char buf[1024];
+	char *newnam, *oldnam, *oldval, *p;
+
+	while (fgets(buf, sizeof(buf), stdin)) {
+		p = strchr(buf, '\n'); if (p) *p = '\0';
+		newnam = buf;
+		oldnam = strchr(buf, '='); if (!oldnam) continue;
+		*oldnam = '\0'; oldnam++;
+		oldval = getenv(oldnam);
+		if (oldval) {
+			printf("%s=\"", newnam);
+			for (p = oldval; (*p); p++) {
+				if (*p == '"')
+					printf("\\\"");
+				else
+					printf("%c", *p);
+			}
+			printf("\"\n");
+		}
+	}
+
+	return 0;
+}
+
