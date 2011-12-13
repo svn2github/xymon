4d347c7f36b8e4d38fa06097e88f2fb2b9b94d6b
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2011-12-13 11:00:41 +0000

    Installation: Avoid deleting include/directory lines in config files. Also improve handling of unknown lines in the files so they are copied to the new file.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6802 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/merge-lines.c
+++ trunk/build/merge-lines.c
@@ -14,6 +14,8 @@ typedef struct entry_t {
 	char *val;
 	int copied;
 	struct entry_t *next;
+	int extracount;
+	char **extralines;
 } entry_t;
 
 typedef struct newname_t {
@@ -24,12 +26,14 @@ typedef struct newname_t {
 entry_t *head = NULL;
 entry_t *tail = NULL;
 newname_t *newnames = NULL;
+char *lastblankandcomment = NULL;
 
 int main(int argc, char *argv[])
 {
 	char *curfn, *curbckfn, *srcfn;
 	FILE *curfd, *curbckfd, *srcfd;
 	char delim = '=';
+	char alldelims[10];
 	char l[32768];
 	entry_t *ewalk;
 	struct stat st;
@@ -57,7 +61,13 @@ int main(int argc, char *argv[])
 	curbckfn = (char *)malloc(strlen(curfn) + 5);
 	sprintf(curbckfn, "%s.bak", curfn);
 
-	if (strstr(srcfn, ".csv")) delim = ';';
+	if (strstr(srcfn, ".csv")) {
+		delim = ';';
+		strcpy(alldelims, ";");
+	}
+	else {
+		sprintf(alldelims, "%c+-", delim);
+	}
 
 	if (stat(curfn, &st) == -1) { showit = 0; goto nooriginal; }
 
@@ -72,31 +82,108 @@ int main(int argc, char *argv[])
 		fprintf(curbckfd, "%s", l);
 
 		bol = l + strspn(l, " \t\r\n");
-		if ((*bol == '#') || (*bol == '\0')) continue;
+		if ((*bol == '#') || (*bol == '\0')) {
+			if (!lastblankandcomment)
+				lastblankandcomment = strdup(bol);
+			else {
+				lastblankandcomment = (char *)realloc(lastblankandcomment, strlen(lastblankandcomment) + strlen(bol) + 1);
+				strcat(lastblankandcomment, bol);
+			}
+			continue;
+		}
 
-		p = strchr(bol, delim);
-		if (p) {
-			entry_t *newent;
-
-			*p = '\0';
-			newent = (entry_t *)malloc(sizeof(entry_t));
-			newent->name = strdup(bol);
-			*p = delim;
-			newent->val = strdup(l);
-			newent->copied = 0;
-			newent->next = NULL;
-
-			if (tail == NULL) {
-				tail = head = newent;
+		if ((strncmp(bol, "include ", 8) == 0) || (strncmp(bol, "directory ", 10) == 0)) {
+			if (!tail->extralines) {
+				tail->extracount = 1;
+				tail->extralines = (char **)malloc(sizeof(char *));
 			}
 			else {
-				tail->next = newent;
-				tail = newent;
+				tail->extracount++;
+				tail->extralines = (char **)realloc(tail->extralines, (tail->extracount*sizeof(char *)));
+			}
+
+			if (!lastblankandcomment)
+				tail->extralines[tail->extracount-1] = strdup(bol);
+			else {
+				tail->extralines[tail->extracount-1] = (char *)malloc(1 + strlen(bol) + strlen(lastblankandcomment));
+				sprintf(tail->extralines[tail->extracount-1], "%s%s", lastblankandcomment, bol);
+			}
+
+			if (lastblankandcomment) {
+				free(lastblankandcomment);
+				lastblankandcomment = NULL;
+			}
+
+			continue;
+		}
+
+		p = bol + strcspn(bol, alldelims);
+		if (*p) {
+			entry_t *newent;
+
+			if (*p == delim) {
+				*p = '\0';
+				newent = (entry_t *)calloc(1, sizeof(entry_t));
+				newent->name = strdup(bol);
+				*p = delim;
+				newent->val = strdup(l);
+
+				if (tail == NULL) {
+					tail = head = newent;
+				}
+				else {
+					tail->next = newent;
+					tail = newent;
+				}
+			}
+			else if (*(p+1) == delim) {
+				char sav = *p;
+				entry_t *walk;
+
+				*p = '\0';
+				for (walk = head; (walk && (strcmp(walk->name, bol) != 0)); walk = walk->next) ;
+				*p = sav;
+				if (walk) {
+					if (!walk->extralines) {
+						walk->extracount = 1;
+						walk->extralines = (char **)malloc(sizeof(char *));
+					}
+					else {
+						walk->extracount++;
+						walk->extralines = (char **)realloc(walk->extralines, (walk->extracount*sizeof(char *)));
+					}
+
+					if (!lastblankandcomment)
+						walk->extralines[walk->extracount-1] = strdup(bol);
+					else {
+						walk->extralines[walk->extracount-1] = (char *)malloc(1 + strlen(bol) + strlen(lastblankandcomment));
+						sprintf(walk->extralines[walk->extracount-1], "%s%s", lastblankandcomment, bol);
+					}
+				}
+			}
+
+			if (lastblankandcomment) {
+				free(lastblankandcomment);
+				lastblankandcomment = NULL;
 			}
 		}
 	}
 	fclose(curfd);
 	fclose(curbckfd);
+
+	if (lastblankandcomment) {
+		/* Add this to the last entry */
+		if (!tail->extralines) {
+			tail->extracount = 1;
+			tail->extralines = (char **)malloc(sizeof(char *));
+		}
+		else {
+			tail->extracount++;
+			tail->extralines = (char **)realloc(tail->extralines, (tail->extracount*sizeof(char *)));
+		}
+
+		tail->extralines[tail->extracount-1] = strdup(lastblankandcomment);
+	}
 
 nooriginal:
 	srcfd = fopen(srcfn, "r");
@@ -107,7 +194,7 @@ nooriginal:
 		char *bol, *p;
 
 		bol = l + strspn(l, " \t\r\n");
-		if ((*bol == '#') || (*bol == '\0')) {
+		if ((*bol == '#') || (*bol == '\0') || (strncmp(bol, "include ", 8) == 0) || (strncmp(bol, "directory ", 10) == 0)) {
 			fprintf(curfd, "%s", l);
 			continue;
 		}
@@ -138,6 +225,12 @@ nooriginal:
 
 			if (ewalk) {
 				fprintf(curfd, "%s", ewalk->val);
+				if (ewalk->extralines) {
+					int i;
+
+					for (i = 0; (i < ewalk->extracount); i++) 
+						fprintf(curfd, "%s", ewalk->extralines[i]);
+				}
 				ewalk->copied = 1;
 			}
 			else {
