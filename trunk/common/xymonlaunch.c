52fb9ba8d6a89d734a715d39926b3df6c84de440
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2012-01-23 09:14:44 +0000

    Missing newlines in some error messages
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@6895 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/common/xymonlaunch.c
+++ trunk/common/xymonlaunch.c
@@ -262,7 +262,7 @@ void load_config(char *conffn)
 			xfreedup(curtask->cronstr,p);
 			if (curtask->crondate) { crondatefree(curtask->crondate);curtask->crondate=parse_cron_time(curtask->cronstr); }
 			if (!curtask->crondate) {
-				errprintf("Can't parse cron date: %s->%s", curtask->key, curtask->cronstr);
+				errprintf("Can't parse cron date: %s->%s\n", curtask->key, curtask->cronstr);
 				curtask->disabled = 1;
 			}
 			curtask->interval = -1; /* disable interval */
@@ -291,7 +291,7 @@ void load_config(char *conffn)
 				curtask->depends = twalk;
 			}
 			else {
-				errprintf("Configuration error, unknown dependency %s->%s", curtask->key, p);
+				errprintf("Configuration error, unknown dependency %s->%s\n", curtask->key, p);
 			}
 		}
 		else if (curtask && (strncasecmp(p, "ENVFILE ", 8) == 0)) {
