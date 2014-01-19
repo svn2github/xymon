41daceb4f26c08260f25bc9d2c480229827e2936
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2014-01-19 21:49:42 +0000

    xymongen: Icons used for the "info" and "trends" columns can be configured explicitly, and no longer have to be one of the color-icons.
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7357 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.14/xymongen/pagegen.c
+++ branches/4.3.14/xymongen/pagegen.c
@@ -441,7 +441,7 @@ void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FIL
 	col_list_t *groupcols, *gc;
 	int	genstatic;
 	int	columncount;
-	char	*xymonskin;
+	char	*xymonskin, *infocolumngif, *trendscolumngif;
 	int	rowcount = 0;
 	int     usetooltip = 0;
 
@@ -449,6 +449,8 @@ void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FIL
 		return;
 
 	xymonskin = strdup(xgetenv("XYMONSKIN"));
+	infocolumngif = strdup(getenv("INFOCOLUMNGIF") ?  getenv("INFOCOLUMNGIF") : dotgiffilename(COL_GREEN, 0, 1));
+	trendscolumngif = strdup(getenv("TRENDSCOLUMNGIF") ?  getenv("TRENDSCOLUMNGIF") : dotgiffilename(COL_GREEN, 0, 1));
 
 	switch (tooltipuse) {
 	  case TT_STDONLY: usetooltip = (pagetype == PAGE_NORMAL); break;
@@ -558,7 +560,11 @@ void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FIL
 					if (strcmp(e->column->name, xgetenv("INFOCOLUMN")) == 0) {
 						/* Show the host IP on the hint display of the "info" column */
 						htmlalttag = alttag(e->column->name, COL_GREEN, 0, 1, h->ip);
-						img = dotgiffilename((getenv("INFOCOLUMNCOLOR") ? parse_color(getenv("INFOCOLUMNCOLOR")) : COL_GREEN), 0, 0);
+						img = infocolumngif;
+					}
+					else if (strcmp(e->column->name, xgetenv("TRENDSCOLUMN")) == 0) {
+						htmlalttag = alttag(e->column->name, COL_GREEN, 0, 1, h->ip);
+						img = trendscolumngif;
 					}
 					else {
 						htmlalttag = alttag(e->column->name, e->color, e->acked, e->propagate, e->age);
@@ -681,6 +687,8 @@ void do_hosts(host_t *head, int sorthosts, char *onlycols, char *exceptcols, FIL
 	}
 
 	xfree(xymonskin);
+	xfree(infocolumngif);
+	xfree(trendscolumngif);
 }
 
 void do_groups(group_t *head, FILE *output, FILE *rssoutput, char *pagepath)
