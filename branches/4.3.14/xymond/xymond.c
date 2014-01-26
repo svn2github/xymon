ced4e5dda5478a155746602bf3cf9dcdf2885c92
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2014-01-26 11:15:54 +0000

    xymond: New --ack-each-color option changes ack behaviour so a yellow ack does not apply when status changed to red, but a red ack applies if status goes yellow
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7372 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.14/xymond/xymond.c
+++ branches/4.3.14/xymond/xymond.c
@@ -131,6 +131,7 @@ typedef struct xymond_log_t {
 	time_t enabletime;	/* time when test auto-enables after a disable */
 	time_t acktime;		/* time when test acknowledgement expires */
 	time_t redstart, yellowstart;
+	int maxackedcolor;	/* The most severe color that has been acked */
 	unsigned char *message;
 	int msgsz;
 	unsigned char *dismsg, *ackmsg;
@@ -190,7 +191,7 @@ int      clientsavemem = 1;	/* In memory */
 int      clientsavedisk = 0;	/* On disk via the CLICHG channel */
 int      allow_downloads = 1;
 int	 defaultvalidity = 30;	/* Minutes */
-
+int	 ackeachcolor = 0;
 
 #define NOTALK 0
 #define RECEIVING 1
@@ -1471,6 +1472,12 @@ void handle_status(unsigned char *msg, char *sender, char *hostname, char *testn
 		if (decide_alertstate(newcolor) == A_OK) {
 			/* The test recovered. Clear the ack. */
 			log->acktime = 0;
+			log->maxackedcolor = 0;
+		}
+
+		if (ackeachcolor && (log->maxackedcolor < newcolor)) {
+			/* Severity has increased above the one that was acked. Clear the current ack */
+			log->acktime = 0;
 		}
 
 		if (log->acktime > now) {
@@ -1479,6 +1486,7 @@ void handle_status(unsigned char *msg, char *sender, char *hostname, char *testn
 		else {
 			/* The acknowledge has expired. Clear the timestamp and the message buffer */
 			log->acktime = 0;
+			log->maxackedcolor = 0;
 			if (log->ackmsg) { xfree(log->ackmsg); log->ackmsg = NULL; }
 		}
 	}
@@ -2075,6 +2083,7 @@ void handle_ack(char *msg, char *sender, xymond_log_t *log, int duration)
 	dbgprintf("->handle_ack\n");
 
 	log->acktime = getcurrenttime(NULL)+duration*60;
+	if (log->color > log->maxackedcolor) log->maxackedcolor = log->color;
 	if (log->validtime < log->acktime) log->validtime = log->acktime;
 
 	p = msg;
@@ -5067,6 +5076,9 @@ int main(int argc, char *argv[])
 			char *p = strchr(argv[argi], '=');
 			flapcount = atoi(p+1);
 			if (flapcount < 0) flapcount = 0;
+		}
+		else if (strcmp(argv[argi], "--ack-each-color") == 0) {
+			ackeachcolor = 1;
 		}
 		else if (argnmatch(argv[argi], "--env=")) {
 			char *p = strchr(argv[argi], '=');
