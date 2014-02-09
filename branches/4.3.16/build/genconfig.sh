cb6dd9351a8e68e693009cb6a93cb85ebed0174e
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2014-02-09 07:06:40 +0000

    genconfig: Disable the native binary-tree code for the 4.3.x series
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7393 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.16/build/genconfig.sh
+++ branches/4.3.16/build/genconfig.sh
@@ -80,13 +80,14 @@ else
 	echo "#undef HAVE_STRTOLL_H" >>include/config.h
 fi
 
-echo "Checking for POSIX binary tree functions"
-$CC -c -o build/testfile.o $CFLAGS build/test-bintree.c 1>/dev/null 2>&1
-if test $? -eq 0; then
-	echo "#define HAVE_BINARY_TREE 1" >>include/config.h
-else
+# This is experimental for 4.3.x
+#echo "Checking for POSIX binary tree functions"
+#$CC -c -o build/testfile.o $CFLAGS build/test-bintree.c 1>/dev/null 2>&1
+#if test $? -eq 0; then
+#	echo "#define HAVE_BINARY_TREE 1" >>include/config.h
+#else
 	echo "#undef HAVE_BINARY_TREE" >>include/config.h
-fi
+#fi
 
 
 echo "#endif" >>include/config.h
