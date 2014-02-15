6ab13a343577b2762c59f747acc23957dbb49389
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2014-02-15 12:21:19 +0000

    Build: Dont require xymondigest to exist, use openssl
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7438 44351d6e-118b-4698-b696-ce33095ecaa4

--- trunk/build/generate-md5.sh
+++ trunk/build/generate-md5.sh
@@ -5,5 +5,5 @@
 WEBLIST=`(cd xymond; find webfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
 WWWLIST=`(cd xymond; find wwwfiles -type f) | egrep -v "RCS|\.svn" | xargs echo`
 
-(cat build/md5.dat; for F in $WEBLIST $WWWLIST; do echo "`./common/xymondigest md5 xymond/$F` $F"; done) | sort -k2 | uniq
+(cat build/md5.dat; for F in $WEBLIST $WWWLIST; do echo "md5:`openssl dgst -md5 xymond/${F} | awk '{print $2}'` ${F}"; done) | sort -k2 | uniq
 
