acdca1f450963785bd7dce4cb4634d78f61f4048
storner <storner@44351d6e-118b-4698-b696-ce33095ecaa4>
2014-09-28 09:30:32 +0000

    AIX client: vmstat behaves differently than other Unixes. JC Cleaver
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7472 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.18/client/xymonclient-aix.sh
+++ branches/4.3.18/client/xymonclient-aix.sh
@@ -65,7 +65,7 @@ then
 fi
 
 # vmstat
-nohup sh -c "vmstat 300 2 1>$XYMONTMP/xymon_vmstat.$MACHINEDOTS.$$ 2>&1; mv $XYMONTMP/xymon_vmstat.$MACHINEDOTS.$$ $XYMONTMP/xymon_vmstat.$MACHINEDOTS" </dev/null >/dev/null 2>&1 &
+nohup sh -c "vmstat 300 1 1>$XYMONTMP/xymon_vmstat.$MACHINEDOTS.$$ 2>&1; mv $XYMONTMP/xymon_vmstat.$MACHINEDOTS.$$ $XYMONTMP/xymon_vmstat.$MACHINEDOTS" </dev/null >/dev/null 2>&1 &
 sleep 5
 if test -f $XYMONTMP/xymon_vmstat.$MACHINEDOTS; then echo "[vmstat]"; cat $XYMONTMP/xymon_vmstat.$MACHINEDOTS; rm -f $XYMONTMP/xymon_vmstat.$MACHINEDOTS; fi
 
