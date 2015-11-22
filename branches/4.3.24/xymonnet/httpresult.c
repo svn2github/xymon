f93a59d710e7f5c678a5d56445eb3684a0720066
jccleaver <jccleaver@44351d6e-118b-4698-b696-ce33095ecaa4>
2015-11-22 02:19:43 +0000

    Fix non-special HTTP 1xx/3xx status codes red instead of yellow (backport r7772)
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7773 44351d6e-118b-4698-b696-ce33095ecaa4

--- branches/4.3.24/xymonnet/httpresult.c
+++ branches/4.3.24/xymonnet/httpresult.c
@@ -64,10 +64,10 @@ static int statuscolor(testedhost_t *h, int status)
 		break;
 	  default:
 		/* Unknown or custom status */
-		result = (result < 100) ? (h->dialup ? COL_CLEAR : COL_RED) :
-			 (result < 200) ? COL_YELLOW :
-			 (result < 300) ? COL_GREEN  :
-			 (result < 400) ? COL_YELLOW :
+		result = (status < 100) ? (h->dialup ? COL_CLEAR : COL_RED) :
+			 (status < 200) ? COL_YELLOW :
+			 (status < 300) ? COL_GREEN  :
+			 (status < 400) ? COL_YELLOW :
 			 COL_RED;
 		break;
 	}
