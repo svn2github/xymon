3210af48ede2cada2ec65e8a7adb33cddef499ff
jccleaver <jccleaver@44351d6e-118b-4698-b696-ce33095ecaa4>
2016-03-22 02:33:29 +0000

    Add ssl3 test files from r7954
    git-svn-id: svn://svn.code.sf.net/p/xymon/code@7956 44351d6e-118b-4698-b696-ce33095ecaa4

---
+++ branches/4.x-master/build/test-ssl3.c
@@ -0,0 +1,19 @@
+#include <openssl/opensslv.h>
+#include <openssl/ssl.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
+
+#if !defined(OPENSSL_VERSION_NUMBER) || (OPENSSL_VERSION_NUMBER < 0x00905000L)
+#error SSL-protocol testing requires OpenSSL version 0.9.5 or later
+#endif
+
+int main(int argc, char *argv[])
+{
+	SSL_CTX *ctx;
+
+	SSL_library_init();
+	ctx = SSL_CTX_new(SSLv3_client_method());
+	return 0;
+}
+
